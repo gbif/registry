7f11a09b0a31042c1d606dcb44d9ea9a651d6e82
Markus Döring <m.doering@mac.com>
2014-08-21 11:57:18 +0200

    Update uat-editor_rights.sql

--- uat-editor_rights.sql
+++ uat-editor_rights.sql
@@ -1,3 +1,6 @@
 -- user rights for UAT in addition to production setup
 insert into editor_rights values ('idigbio', '132fe0d8-aa85-40a0-af20-52f48fae7bd6');
+-- WARNING! make sure the idigbio organization & installation is existing in UAT
+-- http://api.gbif-uat.org/v1/organization/132fe0d8-aa85-40a0-af20-52f48fae7bd6
+-- http://api.gbif-uat.org/v1/installation/219655b7-1226-4a3b-8319-1ea249c44ec5
 insert into editor_rights values ('idigbio', 'e002f531-be93-427c-adc4-811cde0e9205');
