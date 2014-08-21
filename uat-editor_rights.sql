daa49e7f47ba7a3158cede2b39a04c9f84c5aa59
Markus Döring <m.doering@mac.com>
2014-08-21 11:54:18 +0200

    Adding idigbio rights to manage idigbio organization

--- uat-editor_rights.sql
+++ uat-editor_rights.sql
@@ -1,2 +1,3 @@
 -- user rights for UAT in addition to production setup
 insert into editor_rights values ('idigbio', '132fe0d8-aa85-40a0-af20-52f48fae7bd6');
+insert into editor_rights values ('idigbio', 'e002f531-be93-427c-adc4-811cde0e9205');
