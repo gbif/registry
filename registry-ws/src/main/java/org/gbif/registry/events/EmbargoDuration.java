0ef9f02641244cdeae1932d7390903ae70fd9675
timrobertson100 <timrobertson100@gmail.com>
2015-02-13 15:28:30 +0100

    Adds missing binding annotation POR-2646

--- registry-ws/src/main/java/org/gbif/registry/events/EmbargoDuration.java
+++ registry-ws/src/main/java/org/gbif/registry/events/EmbargoDuration.java
@@ -5,9 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.google.inject.BindingAnnotation;
+
 /**
  * An annotation to indicate a parameter provides an embargo duration.
  */
+@BindingAnnotation
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface EmbargoDuration {
