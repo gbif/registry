e7e1a52d73d2ed0b72ed9ea8f5c3cdfddc639905
Markus Döring <m.doering@mac.com>
2015-02-24 19:02:36 +0100

    Adding more eml validation tests

--- registry-metadata/src/test/java/org/gbif/registry/metadata/EmlValidatorTest.java
+++ registry-metadata/src/test/java/org/gbif/registry/metadata/EmlValidatorTest.java
@@ -11,6 +11,18 @@ public class EmlValidatorTest {
     EmlValidator.validate(getClass().getClassLoader().getResourceAsStream("eml/sample-strict.xml"));
   }
 
+  /**
+   * Validate the xml files copied from the eml-metadata-project at
+   * https://code.google.com/p/gbif-common-resources/source/browse/gbif-metadata-profile/trunk/src/test/resources/eml/
+   */
+  @Test
+  public void testValidateEmlMetadataProfileSample() throws Exception {
+    EmlValidator.validate(getClass().getClassLoader().getResourceAsStream("eml-metadata-profile/sample.xml"));
+    EmlValidator.validate(getClass().getClassLoader().getResourceAsStream("eml-metadata-profile/sample2.xml"));
+    // currently invalid
+    // EmlValidator.validate(getClass().getClassLoader().getResourceAsStream("eml-metadata-profile/sample3.xml"));
+  }
+
   @Test
   public void testValidateIptEml() throws Exception {
     EmlValidator.validate(new URL("https://gbif-providertoolkit.googlecode.com/svn/trunk/gbif-ipt/src/test/resources/resources/res1/eml-1.1.xml").openStream());
