6211f833c2e2113dbafb72092d64f9975380db38
Markus Döring <m.doering@mac.com>
2015-07-22 11:40:07 +0200

    Truncate all constituent relations from datacite metadata as last resort

--- registry-cli/src/main/java/org/gbif/registry/cli/DoiUpdateListener.java
+++ registry-cli/src/main/java/org/gbif/registry/cli/DoiUpdateListener.java
@@ -26,140 +26,149 @@ import org.slf4j.MarkerFactory;
  */
 public class DoiUpdateListener extends AbstractMessageCallback<ChangeDoiMessage> {
 
-  private static final Logger LOG = LoggerFactory.getLogger(DoiUpdateListener.class);
-  private static final Marker DOI_SMTP = MarkerFactory.getMarker("DOI_SMTP");
-  private final long timeToRetryInMs;
+    private static final Logger LOG = LoggerFactory.getLogger(DoiUpdateListener.class);
+    private static final Marker DOI_SMTP = MarkerFactory.getMarker("DOI_SMTP");
+    private final long timeToRetryInMs;
 
-  private final DoiService doiService;
-  private final DoiMapper doiMapper;
+    private final DoiService doiService;
+    private final DoiMapper doiMapper;
 
-  public DoiUpdateListener(DoiService doiService, DoiMapper doiMapper, long timeToRetryInMs) {
-    this.doiService = doiService;
-    this.doiMapper = doiMapper;
-    this.timeToRetryInMs = timeToRetryInMs;
-  }
-
-  @Override
-  public void handleMessage(ChangeDoiMessage msg) {
-    LOG.debug("Handling change DOI to {} message for {}", msg.getStatus(), msg.getDoi());
-    final DoiData currState = doiMapper.get(msg.getDoi());
-    if (currState == null) {
-      // this is bad, we should have an entry for the DOI in our registry table!
-      LOG.warn("Skipping unknown GBIF DOI {}", msg.getDoi());
-      return;
+    public DoiUpdateListener(DoiService doiService, DoiMapper doiMapper, long timeToRetryInMs) {
+        this.doiService = doiService;
+        this.doiMapper = doiMapper;
+        this.timeToRetryInMs = timeToRetryInMs;
     }
 
-    for (int retry = 1; retry < 4; retry++){
-      try {
-        switch (msg.getStatus()) {
-          case REGISTERED:
-            registerOrUpdate(msg.getDoi(), msg.getTarget(), msg.getMetadata(), currState);
-            break;
-          case RESERVED:
-            reserve(msg.getDoi(), msg.getMetadata(), currState);
-            break;
-          case DELETED:
-            delete(msg.getDoi(), currState);
-            break;
-          default:
-            LOG.warn("Cannot update {} to illegal state {}.", msg.getDoi(), msg.getStatus());
-            break;
+    @Override
+    public void handleMessage(ChangeDoiMessage msg) {
+        LOG.debug("Handling change DOI to {} message for {}", msg.getStatus(), msg.getDoi());
+        final DoiData currState = doiMapper.get(msg.getDoi());
+        if (currState == null) {
+            // this is bad, we should have an entry for the DOI in our registry table!
+            LOG.warn("Skipping unknown GBIF DOI {}", msg.getDoi());
+            return;
         }
-        break;
 
-      } catch (DoiExistsException e) {
-        writeFailedStatus(msg.getDoi(), msg.getTarget(), msg.getMetadata());
-        LOG.warn(DOI_SMTP, "DOI {} existed already when trying to change status to {}. Ignore", msg.getDoi(), msg.getStatus(), e);
-        break;
+        boolean descriptionTruncated = false;
+        for (int retry = 1; retry < 4; retry++) {
+            try {
+                switch (msg.getStatus()) {
+                    case REGISTERED:
+                        registerOrUpdate(msg.getDoi(), msg.getTarget(), msg.getMetadata(), currState);
+                        break;
+                    case RESERVED:
+                        reserve(msg.getDoi(), msg.getMetadata(), currState);
+                        break;
+                    case DELETED:
+                        delete(msg.getDoi(), currState);
+                        break;
+                    default:
+                        LOG.warn("Cannot update {} to illegal state {}.", msg.getDoi(), msg.getStatus());
+                        break;
+                }
+                break;
 
-      } catch (DoiHttpException e) {
-        writeFailedStatus(msg.getDoi(), msg.getTarget(), msg.getMetadata());
-        if (e.getStatus() == 413) {
-          LOG.warn(DOI_SMTP, "Metadata of length {} is exceeding max datacite limit in attempt #{} "
-                             + "while updating {} to {} with target {}. "
-                             + "Trying again with truncated metadata", msg.getMetadata().length(), retry,
-            msg.getDoi(), msg.getStatus(), msg.getTarget(), e);
-          try {
-            LOG.debug("Original metadata for DOI {}:\n\n{}", msg.getDoi(), msg.getMetadata());
-            String truncatedXml = DataCiteConverter.truncateDescription(msg.getDoi(), msg.getMetadata(), msg.getTarget());
-            msg = new ChangeDoiMessage(msg.getStatus(), msg.getDoi(), truncatedXml, msg.getTarget());
-          } catch (InvalidMetadataException e1) {
-            LOG.warn("Failed to deserialize xml metadata for DOI {}", msg.getDoi(), e1);
-          }
+            } catch (DoiExistsException e) {
+                writeFailedStatus(msg.getDoi(), msg.getTarget(), msg.getMetadata());
+                LOG.warn(DOI_SMTP, "DOI {} existed already when trying to change status to {}. Ignore", msg.getDoi(), msg.getStatus(), e);
+                break;
+
+            } catch (DoiHttpException e) {
+                writeFailedStatus(msg.getDoi(), msg.getTarget(), msg.getMetadata());
+                if (e.getStatus() == 413) {
+                    LOG.warn(DOI_SMTP, "Metadata of length {} is exceeding max datacite limit in attempt #{} "
+                                    + "while updating {} to {} with target {}. "
+                                    + "Trying again {}",
+                            msg.getMetadata().length(), retry, msg.getDoi(), msg.getStatus(), msg.getTarget(),
+                            descriptionTruncated ? "without constituent information" : "with truncated description", e);
+                    try {
+                        String truncatedXml;
+                        if (descriptionTruncated) {
+                            LOG.warn("Truncating all constituent relations as last resort from metadata for DOI {}", msg.getDoi());
+                            truncatedXml = DataCiteConverter.truncateConstituents(msg.getDoi(), msg.getMetadata(), msg.getTarget());
+                        } else {
+                            LOG.debug("Original metadata for DOI {}:\n\n{}", msg.getDoi(), msg.getMetadata());
+                            truncatedXml = DataCiteConverter.truncateDescription(msg.getDoi(), msg.getMetadata(), msg.getTarget());
+                            descriptionTruncated = true;
+                        }
+                        msg = new ChangeDoiMessage(msg.getStatus(), msg.getDoi(), truncatedXml, msg.getTarget());
+                    } catch (InvalidMetadataException e1) {
+                        LOG.warn("Failed to deserialize xml metadata for DOI {}", msg.getDoi(), e1);
+                    }
+                } else {
+                    LOG.warn(DOI_SMTP, "DOI http {} exception updating {} to {} with target {}. Attempt #{}",
+                            e.getStatus(), msg.getDoi(), msg.getStatus(), msg.getTarget(), retry, e);
+                    sleep();
+                }
+
+            } catch (DoiException e) {
+                writeFailedStatus(msg.getDoi(), msg.getTarget(), msg.getMetadata());
+                LOG.warn(DOI_SMTP, "DOI exception updating {} to {} with target {}. Attempt #{}",
+                        msg.getDoi(), msg.getStatus(), msg.getTarget(), retry, e);
+                sleep();
+            }
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(timeToRetryInMs);
+        } catch (InterruptedException e1) {
+            LOG.info("Interrupted retries");
+        }
+    }
+
+    private void writeFailedStatus(DOI doi, URI target, String xml) {
+        doiMapper.update(doi, new DoiData(DoiStatus.FAILED, target), xml);
+    }
+
+    private void reserve(DOI doi, String xml, DoiData currState) throws DoiException {
+        doiService.reserve(doi, xml);
+        LOG.info("Reserved doi {}", doi);
+        DoiData newState = new DoiData(DoiStatus.RESERVED, currState.getTarget());
+        doiMapper.update(doi, newState, xml);
+    }
+
+    private void delete(DOI doi, DoiData currState) throws DoiException {
+        if (currState.getStatus() == null) {
+            doiMapper.delete(doi);
         } else {
-          LOG.warn(DOI_SMTP, "DOI http {} exception updating {} to {} with target {}. Attempt #{}",
-            e.getStatus(), msg.getDoi(), msg.getStatus(), msg.getTarget(), retry, e);
-          sleep();
+            try {
+                boolean fullDeleted = doiService.delete(doi);
+                if (fullDeleted) {
+                    doiMapper.delete(doi);
+                    LOG.info("Deleted doi {}", doi);
+                } else {
+                    DoiData newState = new DoiData(DoiStatus.DELETED, currState.getTarget());
+                    doiMapper.update(doi, newState, null);
+                    LOG.info("Marked registered doi {} as deleted", doi);
+                }
+            } catch (DoiHttpException e) {
+                // in case of a 404 swallow
+                if (e.getStatus() == 404) {
+                    LOG.warn(DOI_SMTP, "Trying to delete DOI {} failed because it doesn't exist in DataCite; deleting locally",
+                            doi, e);
+                    doiMapper.delete(doi);
+                } else {
+                    throw e;
+                }
+            }
         }
-
-      } catch (DoiException e) {
-        writeFailedStatus(msg.getDoi(), msg.getTarget(), msg.getMetadata());
-        LOG.warn(DOI_SMTP, "DOI exception updating {} to {} with target {}. Attempt #{}",
-          msg.getDoi(), msg.getStatus(), msg.getTarget(), retry, e);
-        sleep();
-      }
     }
-  }
 
-  private void sleep() {
-    try {
-      Thread.sleep(timeToRetryInMs);
-    } catch (InterruptedException e1) {
-      LOG.info("Interrupted retries");
-    }
-  }
-
-  private void writeFailedStatus(DOI doi, URI target, String xml) {
-    doiMapper.update(doi, new DoiData(DoiStatus.FAILED, target), xml);
-  }
-
-  private void reserve(DOI doi, String xml, DoiData currState) throws DoiException {
-    doiService.reserve(doi, xml);
-    LOG.info("Reserved doi {}", doi);
-    DoiData newState = new DoiData(DoiStatus.RESERVED, currState.getTarget());
-    doiMapper.update(doi, newState, xml);
-  }
-
-  private void delete(DOI doi, DoiData currState) throws DoiException {
-    if (currState.getStatus() == null) {
-      doiMapper.delete(doi);
-    } else {
-      try {
-        boolean fullDeleted = doiService.delete(doi);
-        if (fullDeleted) {
-          doiMapper.delete(doi);
-          LOG.info("Deleted doi {}", doi);
+    private void registerOrUpdate(DOI doi, URI target, String xml, DoiData currState) throws DoiException {
+        if (DoiStatus.REGISTERED == currState.getStatus()) {
+            // the DOI was already registered, so we only need to update the target url if changed and the metadata
+            if (!target.equals(currState.getTarget())) {
+                doiService.update(doi, target);
+            }
+            doiService.update(doi, xml);
+            LOG.info("Updated doi {} with target {}", doi, target);
         } else {
-          DoiData newState = new DoiData(DoiStatus.DELETED, currState.getTarget());
-          doiMapper.update(doi, newState, null);
-          LOG.info("Marked registered doi {} as deleted", doi);
+            doiService.register(doi, target, xml);
+            LOG.info("Registered doi {} with target {}", doi, target);
         }
-      } catch (DoiHttpException e) {
-        // in case of a 404 swallow
-        if (e.getStatus() == 404) {
-          LOG.warn(DOI_SMTP, "Trying to delete DOI {} failed because it doesn't exist in DataCite; deleting locally",
-            doi, e);
-          doiMapper.delete(doi);
-        } else {
-          throw e;
-        }
-      }
+        // store the new state in our registry
+        doiMapper.update(doi, new DoiData(DoiStatus.REGISTERED, target), xml);
     }
-  }
-
-  private void registerOrUpdate(DOI doi, URI target, String xml, DoiData currState) throws DoiException {
-    if (DoiStatus.REGISTERED == currState.getStatus()) {
-      // the DOI was already registered, so we only need to update the target url if changed and the metadata
-      if (!target.equals(currState.getTarget())) {
-        doiService.update(doi, target);
-      }
-      doiService.update(doi, xml);
-      LOG.info("Updated doi {} with target {}", doi, target);
-    } else {
-      doiService.register(doi, target, xml);
-      LOG.info("Registered doi {} with target {}", doi, target);
-    }
-    // store the new state in our registry
-    doiMapper.update(doi, new DoiData(DoiStatus.REGISTERED, target), xml);
-  }
 }
--- registry-ws/src/test/java/org/gbif/registry/ws/util/DataCiteConverterTest.java
+++ registry-ws/src/test/java/org/gbif/registry/ws/util/DataCiteConverterTest.java
@@ -27,6 +27,7 @@ import com.google.common.io.Resources;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
@@ -35,113 +36,128 @@ import static org.mockito.Mockito.when;
 
 public class DataCiteConverterTest {
 
-  @Test
-  public void testGetYear() throws Exception {
-    Date d = new Date(1418340702253l);
-    assertEquals("2014", DataCiteConverter.getYear(d));
-    assertNull(DataCiteConverter.getYear(null));
-  }
+    @Test
+    public void testGetYear() throws Exception {
+        Date d = new Date(1418340702253l);
+        assertEquals("2014", DataCiteConverter.getYear(d));
+        assertNull(DataCiteConverter.getYear(null));
+    }
 
-  @Test
-  public void testConvert() throws Exception {
-    Organization publisher = new Organization();
-    publisher.setTitle("X-Publisher");
-    publisher.setKey(UUID.randomUUID());
+    @Test
+    public void testConvert() throws Exception {
+        Organization publisher = new Organization();
+        publisher.setTitle("X-Publisher");
+        publisher.setKey(UUID.randomUUID());
 
-    final DOI doi = new DOI("10.1234/5678");
-    Dataset d = new Dataset();
-    d.setKey(UUID.randomUUID());
-    d.setType(DatasetType.OCCURRENCE);
-    d.setTitle("my title");
-    d.setCreated(new Date());
-    d.setModified(new Date());
-    d.setCreatedBy("Markus");
-    d.setLanguage(Language.NORWEGIAN);
-    d.setDataLanguage(Language.NORWEGIAN);
+        final DOI doi = new DOI("10.1234/5678");
+        Dataset d = new Dataset();
+        d.setKey(UUID.randomUUID());
+        d.setType(DatasetType.OCCURRENCE);
+        d.setTitle("my title");
+        d.setCreated(new Date());
+        d.setModified(new Date());
+        d.setCreatedBy("Markus");
+        d.setLanguage(Language.NORWEGIAN);
+        d.setDataLanguage(Language.NORWEGIAN);
 
-    DataCiteMetadata m = convertAndValidate(doi, d, publisher);
-    assertEquals("my title", m.getTitles().getTitle().get(0).getValue());
-    assertEquals("Markus", m.getCreators().getCreator().get(0).getCreatorName());
-    assertEquals(doi.getDoiName(), m.getIdentifier().getValue());
+        DataCiteMetadata m = convertAndValidate(doi, d, publisher);
+        assertEquals("my title", m.getTitles().getTitle().get(0).getValue());
+        assertEquals("Markus", m.getCreators().getCreator().get(0).getCreatorName());
+        assertEquals(doi.getDoiName(), m.getIdentifier().getValue());
 
-    d.setDoi(doi);
-    d.setDescription("bla bla bla bla bla bla bla bla - I talk too much");
-    List<GeospatialCoverage> geos = Lists.newArrayList();
-    d.setGeographicCoverages(geos);
-    GeospatialCoverage g = new GeospatialCoverage();
-    geos.add(g);
-    g.setBoundingBox(new BoundingBox(1,2,3,4));
+        d.setDoi(doi);
+        d.setDescription("bla bla bla bla bla bla bla bla - I talk too much");
+        List<GeospatialCoverage> geos = Lists.newArrayList();
+        d.setGeographicCoverages(geos);
+        GeospatialCoverage g = new GeospatialCoverage();
+        geos.add(g);
+        g.setBoundingBox(new BoundingBox(1, 2, 3, 4));
 
-    m = convertAndValidate(doi, d, publisher);
-    assertEquals("my title", m.getTitles().getTitle().get(0).getValue());
-    assertEquals("Markus", m.getCreators().getCreator().get(0).getCreatorName());
-    assertEquals("10.1234/5678", m.getIdentifier().getValue());
-    assertEquals(Lists.<Double>newArrayList(1d,3d,2d,4d), m.getGeoLocations().getGeoLocation().get(0).getGeoLocationBox());
-    assertEquals(d.getDescription(), m.getDescriptions().getDescription().get(0).getContent().get(0));
-  }
+        m = convertAndValidate(doi, d, publisher);
+        assertEquals("my title", m.getTitles().getTitle().get(0).getValue());
+        assertEquals("Markus", m.getCreators().getCreator().get(0).getCreatorName());
+        assertEquals("10.1234/5678", m.getIdentifier().getValue());
+        assertEquals(Lists.<Double>newArrayList(1d, 3d, 2d, 4d), m.getGeoLocations().getGeoLocation().get(0).getGeoLocationBox());
+        assertEquals(d.getDescription(), m.getDescriptions().getDescription().get(0).getContent().get(0));
+    }
 
 
-  private DataCiteMetadata convertAndValidate(DOI doi, Dataset d, Organization publisher) throws InvalidMetadataException {
-    DataCiteMetadata m = DataCiteConverter.convert(d, publisher);
-    DataCiteValidator.toXml(doi, m);
-    return m;
-  }
+    private DataCiteMetadata convertAndValidate(DOI doi, Dataset d, Organization publisher) throws InvalidMetadataException {
+        DataCiteMetadata m = DataCiteConverter.convert(d, publisher);
+        DataCiteValidator.toXml(doi, m);
+        return m;
+    }
 
-  @Test
-  public void testConvertDownload() throws Exception{
-    DatasetOccurrenceDownloadUsage du1 = new DatasetOccurrenceDownloadUsage();
-    du1.setDatasetKey(UUID.randomUUID());
-    du1.setDatasetTitle("my title");
-    du1.setDatasetDOI(new DOI("10.1234/5679"));
-    du1.setNumberRecords(101);
+    @Test
+    public void testConvertDownload() throws Exception {
+        DatasetOccurrenceDownloadUsage du1 = new DatasetOccurrenceDownloadUsage();
+        du1.setDatasetKey(UUID.randomUUID());
+        du1.setDatasetTitle("my title");
+        du1.setDatasetDOI(new DOI("10.1234/5679"));
+        du1.setNumberRecords(101);
 
-    DatasetOccurrenceDownloadUsage du2 = new DatasetOccurrenceDownloadUsage();
-    du2.setDatasetKey(UUID.randomUUID());
-    du2.setDatasetTitle("my title #2");
-    du2.setDatasetDOI(new DOI("10.1234/klimbim"));
-    du2.setNumberRecords(2002);
+        DatasetOccurrenceDownloadUsage du2 = new DatasetOccurrenceDownloadUsage();
+        du2.setDatasetKey(UUID.randomUUID());
+        du2.setDatasetTitle("my title #2");
+        du2.setDatasetDOI(new DOI("10.1234/klimbim"));
+        du2.setNumberRecords(2002);
 
-    Download download = new Download();
-    download.setCreated(new Date());
-    download.setDoi(new DOI("10.1234/5678"));
-    download.setKey("1");
-    download.setModified(new Date());
-    download.setNumberDatasets(1l);
-    download.setSize(100);
-    download.setStatus(Download.Status.SUCCEEDED);
-    download.setTotalRecords(10);
-    DownloadRequest downloadRequest = new DownloadRequest();
-    downloadRequest.setCreator("dev@gbif.org");
-    download.setRequest(downloadRequest);
+        Download download = new Download();
+        download.setCreated(new Date());
+        download.setDoi(new DOI("10.1234/5678"));
+        download.setKey("1");
+        download.setModified(new Date());
+        download.setNumberDatasets(1l);
+        download.setSize(100);
+        download.setStatus(Download.Status.SUCCEEDED);
+        download.setTotalRecords(10);
+        DownloadRequest downloadRequest = new DownloadRequest();
+        downloadRequest.setCreator("dev@gbif.org");
+        download.setRequest(downloadRequest);
 
-    User user = new User();
-    user.setUserName("peta");
-    user.setEmail("aha@music.com");
-    user.setFirstName("Pete");
-    user.setEmail("Doherty");
+        User user = new User();
+        user.setUserName("peta");
+        user.setEmail("aha@music.com");
+        user.setFirstName("Pete");
+        user.setEmail("Doherty");
 
-    // mock title lookup API
-    TitleLookup tl = mock(TitleLookup.class);
-    when(tl.getDatasetTitle(anyString())).thenReturn("PonTaurus");
-    when(tl.getSpeciesName(anyString())).thenReturn("Abies alba Mill.");
+        // mock title lookup API
+        TitleLookup tl = mock(TitleLookup.class);
+        when(tl.getDatasetTitle(anyString())).thenReturn("PonTaurus");
+        when(tl.getSpeciesName(anyString())).thenReturn("Abies alba Mill.");
 
-    DataCiteMetadata metadata = DataCiteConverter.convert(download, user, Lists.newArrayList(du1, du2), tl);
-    String xml = DataCiteValidator.toXml(download.getDoi(), metadata);
-    System.out.println(xml);
-    assertTrue(xml.contains(du1.getDatasetDOI().getDoiName()));
-    assertTrue(xml.contains(du2.getDatasetDOI().getDoiName()));
-    assertTrue(xml.contains(String.valueOf(du1.getNumberRecords())));
-    assertTrue(xml.contains(String.valueOf(du2.getNumberRecords())));
-  }
+        DataCiteMetadata metadata = DataCiteConverter.convert(download, user, Lists.newArrayList(du1, du2), tl);
+        String xml = DataCiteValidator.toXml(download.getDoi(), metadata);
+        System.out.println(xml);
+        assertTrue(xml.contains(du1.getDatasetDOI().getDoiName()));
+        assertTrue(xml.contains(du2.getDatasetDOI().getDoiName()));
+        assertTrue(xml.contains(String.valueOf(du1.getNumberRecords())));
+        assertTrue(xml.contains(String.valueOf(du2.getNumberRecords())));
+    }
 
-  @Test
-  public void testTruncateDesription() throws Exception{
-    DOI doi = new DOI("10.15468/dl.v8zc57");
-    String xml = Resources.toString(Resources.getResource("metadata/datacite-large.xml"), Charsets.UTF_8);
-    String xml2 = DataCiteConverter.truncateDescription(doi, xml, URI.create("http://gbif.org"));
-    //System.out.println(xml2);
-    DataCiteValidator.validateMetadata(xml2);
-    assertEquals(2316, xml2.length());
-  }
+    @Test
+    public void testTruncateDesription() throws Exception {
+        DOI doi = new DOI("10.15468/dl.v8zc57");
+        String xml = Resources.toString(Resources.getResource("metadata/datacite-large.xml"), Charsets.UTF_8);
+        String xml2 = DataCiteConverter.truncateDescription(doi, xml, URI.create("http://gbif.org"));
+        //System.out.println(xml2);
+        DataCiteValidator.validateMetadata(xml2);
+        assertTrue(xml2.contains("for full list of all constituents"));
+        assertFalse(xml2.contains("University of Ghent"));
+        assertTrue(xml2.contains("10.15468/siye1z"));
+        assertEquals(3648, xml2.length());
+    }
 
+    @Test
+    public void testTruncateConstituents() throws Exception {
+        DOI doi = new DOI("10.15468/dl.v8zc57");
+        String xml = Resources.toString(Resources.getResource("metadata/datacite-large.xml"), Charsets.UTF_8);
+        String xml2 = DataCiteConverter.truncateConstituents(doi, xml, URI.create("http://gbif.org"));
+        System.out.println(xml2);
+        DataCiteValidator.validateMetadata(xml2);
+        assertTrue(xml2.contains("for full list of all constituents"));
+        assertFalse(xml2.contains("University of Ghent"));
+        assertFalse(xml2.contains("10.15468/siye1z"));
+        assertEquals(2310, xml2.length());
+    }
 }
