d56b46aceaa9a1c73a7d9da4bd2d08d4a10dd48f
Christian Gendreau <cgendreau@gbif.org>
2015-10-30 11:20:55 +0100

    slowly move EMLWriter to a non-static class

--- registry-metadata/src/main/java/org/gbif/registry/metadata/EMLWriter.java
+++ registry-metadata/src/main/java/org/gbif/registry/metadata/EMLWriter.java
@@ -26,11 +26,51 @@ public class EMLWriter {
 
   private static final String TEMPLATE_PATH = "/gbif-eml-profile-template";
   private static final String EML_TEMPLATE = String.format("eml-dataset-%s.ftl", EMLProfileVersion.GBIF_1_0_2.getVersion());
+  private final Configuration freemarkerConfig;
+  private final boolean useDoiAsIdentifier;
+
   private static final Configuration FTL = DatasetXMLWriterConfigurationProvider.provideFreemarker(TEMPLATE_PATH);
 
-  private EMLWriter() {
-    // static utils class
+  /**
+   * Private constructor, use {@link #newInstance()}
+   * @param cfg
+   */
+  private EMLWriter(Configuration cfg, boolean useDoiAsIdentifier) {
+    this.freemarkerConfig = cfg;
+    this.useDoiAsIdentifier = useDoiAsIdentifier;
   }
+
+  /**
+   * Get a new instance of EMLWriter with default Freemarker configuration.
+   * Same as calling {@link #newInstance(boolean)} method with useDoiAsIdentifier = false
+   *
+   * @return new instance
+   */
+  public static EMLWriter newInstance(){
+    return new EMLWriter(DatasetXMLWriterConfigurationProvider.provideFreemarker(), false);
+  }
+
+  /**
+   * Get a new instance of EMLWriter with default Freemarker configuration.
+   *
+   * @param useDoiAsIdentifier should the packageId be the dataset.doi? If true, the dataset.doi won't be included in the list of alternate identifiers
+   * @return
+   */
+  public static EMLWriter newInstance(boolean useDoiAsIdentifier){
+    return new EMLWriter(DatasetXMLWriterConfigurationProvider.provideFreemarker(), useDoiAsIdentifier);
+  }
+
+  /**
+   * Write a document from a Dataset object.
+   *
+   * @param dataset non null dataset object
+   * @param writer where the output document will go. The writer is not closed by this method.
+   * @throws IOException if an error occurs while processing the template
+   */
+  public void writeTo(Dataset dataset, Writer writer) throws IOException {
+    write(dataset, writer, useDoiAsIdentifier);
+  }
+
 
   /**
    * Wrapper for a dataset instance that exposes some very EML specific methods.
@@ -87,6 +127,8 @@ public class EMLWriter {
   }
 
   /**
+   * This method will be deprecated soon, please use an instance {@link #newInstance()}
+   *
    * Same as calling {@link #write(Dataset, Writer, boolean) write} method with useDoiAsIdentifier = false.
    *
    * @param dataset
@@ -98,18 +140,20 @@ public class EMLWriter {
   }
 
   /**
+   * This method will be deprecated soon, please use an instance {@link #newInstance()}
+   *
    * Write an EML document from a Dataset object.
    *
    * @param dataset non null dataset object
    * @param writer where the output document will go. The writer is not closed by this method.
-   * @param useDoiAsIdentifier should the packageId be the dataset.doi? If true, the dataset.doi won't be included in the list of alternate identifiers.
+   * @param _useDoiAsIdentifier should the packageId be the dataset.doi? If true, the dataset.doi won't be included in the list of alternate identifiers.
    * @throws IOException if an error occurs while processing the template
    */
-  public static void write(Dataset dataset, Writer writer, boolean useDoiAsIdentifier) throws IOException {
+  public static void write(Dataset dataset, Writer writer, boolean _useDoiAsIdentifier) throws IOException {
     Preconditions.checkNotNull(dataset, "Dataset can't be null");
 
     Map<String, Object> map = ImmutableMap.of
-            ("dataset", dataset, "eml", new EmlDatasetWrapper(dataset), "useDoiAsIdentifier", useDoiAsIdentifier);
+            ("dataset", dataset, "eml", new EmlDatasetWrapper(dataset), "useDoiAsIdentifier", _useDoiAsIdentifier);
 
     try {
       FTL.getTemplate(EML_TEMPLATE).process(map, writer);
--- registry-metadata/src/test/java/org/gbif/registry/metadata/EMLWriterTest.java
+++ registry-metadata/src/test/java/org/gbif/registry/metadata/EMLWriterTest.java
@@ -5,55 +5,62 @@ import org.gbif.api.model.registry.Contact;
 import org.gbif.api.model.registry.Dataset;
 import org.gbif.registry.metadata.parse.DatasetParser;
 import org.gbif.utils.file.FileUtils;
+
 import org.junit.Test;
 
 import java.io.StringWriter;
 import java.util.UUID;
 
+import static org.junit.Assert.assertTrue;
+
 public class EMLWriterTest {
 
-    @Test
-    public void testWrite() throws Exception {
-        Dataset d = DatasetParser.build(FileUtils.classpathStream("eml/sample.xml"));
-        d.setKey(UUID.randomUUID());
-        StringWriter writer = new StringWriter();
-        EMLWriter.write(d, writer);
-    }
+  EMLWriter emlWriter = EMLWriter.newInstance();
+  EMLWriter emlWriterDOI = EMLWriter.newInstance(true);
 
-    @Test
-    public void testWriteNullContact() throws Exception {
-        Dataset d = DatasetParser.build(FileUtils.classpathStream("eml/sample.xml"));
-        d.setKey(UUID.randomUUID());
-        d.getContacts().clear();
-        StringWriter writer = new StringWriter();
-        EMLWriter.write(d, writer);
-    }
+  @Test
+  public void testWrite() throws Exception {
+    Dataset d = DatasetParser.build(FileUtils.classpathStream("eml/sample.xml"));
+    d.setKey(UUID.randomUUID());
+    StringWriter writer = new StringWriter();
+    emlWriter.writeTo(d, writer);
+  }
 
-    @Test
-    public void testNullAddress() throws Exception {
-        Dataset d = DatasetParser.build(FileUtils.classpathStream("eml/sample.xml"));
-        d.setKey(UUID.randomUUID());
-        Contact c = d.getContacts().get(0);
-        c.getAddress().add(null);
-        c.getAddress().add(null);
-        StringWriter writer = new StringWriter();
-        EMLWriter.write(d, writer);
-    }
+  @Test
+  public void testWriteNullContact() throws Exception {
+    Dataset d = DatasetParser.build(FileUtils.classpathStream("eml/sample.xml"));
+    d.setKey(UUID.randomUUID());
+    d.getContacts().clear();
+    StringWriter writer = new StringWriter();
+    emlWriter.writeTo(d, writer);
+  }
 
-    @Test
-    public void testWriteDC() throws Exception {
-        Dataset d = DatasetParser.build(FileUtils.classpathStream("dc/worms_dc.xml"));
-        d.setKey(UUID.randomUUID());
-        StringWriter writer = new StringWriter();
-        EMLWriter.write(d, writer);
-    }
+  @Test
+  public void testNullAddress() throws Exception {
+    Dataset d = DatasetParser.build(FileUtils.classpathStream("eml/sample.xml"));
+    d.setKey(UUID.randomUUID());
+    Contact c = d.getContacts().get(0);
+    c.getAddress().add(null);
+    c.getAddress().add(null);
+    StringWriter writer = new StringWriter();
+    emlWriter.writeTo(d, writer);
+  }
 
-    @Test
-    public void testWriteDoiAsPrimaryId() throws Exception {
-      Dataset d = DatasetParser.build(FileUtils.classpathStream("dc/worms_dc.xml"));
-      d.setKey(UUID.randomUUID());
-      d.setDoi(new DOI("10.1234/5679"));
-      StringWriter writer = new StringWriter();
-      EMLWriter.write(d, writer,true);
-    }
+  @Test
+  public void testWriteDC() throws Exception {
+    Dataset d = DatasetParser.build(FileUtils.classpathStream("dc/worms_dc.xml"));
+    d.setKey(UUID.randomUUID());
+    StringWriter writer = new StringWriter();
+    emlWriter.writeTo(d, writer);
+  }
+
+  @Test
+  public void testWriteDoiAsPrimaryId() throws Exception {
+    Dataset d = DatasetParser.build(FileUtils.classpathStream("dc/worms_dc.xml"));
+    d.setKey(UUID.randomUUID());
+    d.setDoi(new DOI("10.1234/5679"));
+    StringWriter writer = new StringWriter();
+    emlWriterDOI.writeTo(d, writer);
+    assertTrue(writer.toString().contains("packageId=\"10.1234/5679\""));
+  }
 }
