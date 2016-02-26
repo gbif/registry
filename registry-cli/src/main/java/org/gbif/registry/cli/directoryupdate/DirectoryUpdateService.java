e702cbfefc1be2672d0c80b1a3e25f59df08db28
Christian Gendreau <cgendreau@gbif.org>
2016-02-26 10:24:11 +0100

    Directory update cli: fixed initial delay apply update

--- registry-cli/src/main/java/org/gbif/registry/cli/directoryupdate/DirectoryUpdateService.java
+++ registry-cli/src/main/java/org/gbif/registry/cli/directoryupdate/DirectoryUpdateService.java
@@ -16,6 +16,7 @@ import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
 
 import com.google.common.collect.Maps;
 import com.google.common.util.concurrent.AbstractIdleService;
@@ -28,14 +29,17 @@ import org.threeten.bp.LocalTime;
 import org.threeten.bp.temporal.ChronoUnit;
 
 /**
- * WORK-IN-PROGRESS
- * Service that will periodically get the list of participants and nodes from the Directory and update the registry
- * data is required.
  *
+ * Service that will periodically get the list of participants and nodes from the Directory and update the registry
+ * data if required.
+ * This service will NOT remove nodes that are not available in the Directory.
+ *
+ * Only a limited set of fields are updated see {@link #shouldUpdateNode} and {@link #updateRegistryNode}
  */
 public class DirectoryUpdateService extends AbstractIdleService {
 
   private static final Logger LOG = LoggerFactory.getLogger(DirectoryUpdateService.class);
+  private static final String DIRECTORY_UPDATE_USER = "directory-update-robot";
   //avoid using 0
   private static final int DEFAULT_START_HOUR = 5;
   private static final int DEFAULT_START_MINUTE = 34;
@@ -52,7 +56,7 @@ public class DirectoryUpdateService extends AbstractIdleService {
 
   private NodeMapper nodeMapper;
 
-  public DirectoryUpdateService(DirectoryUpdateConfiguration cfg){
+  public DirectoryUpdateService(DirectoryUpdateConfiguration cfg) {
     Injector directoryUpdaterInj = cfg.createInjector();
     directoryParticipantService = directoryUpdaterInj.getInstance(ParticipantService.class);
     directoryNodeService = directoryUpdaterInj.getInstance(org.gbif.api.service.directory.NodeService.class);
@@ -73,14 +77,19 @@ public class DirectoryUpdateService extends AbstractIdleService {
     LocalTime t = LocalTime.of(startHour, startMinute);
     long initialDelay = LocalTime.now().until(t, ChronoUnit.MINUTES);
 
-    LOG.info("DirectoryUpdateService Starting in " + initialDelay + " minutes");
+    //if the delay is passed,
+    if (initialDelay < 0) {
+      initialDelay = initialDelay + ChronoUnit.DAYS.getDuration().toMinutes();
+    }
+
+    LOG.info("DirectoryUpdateService Starting in " + initialDelay + " minute(s)");
 
     scheduler.scheduleAtFixedRate(new Runnable() {
       @Override
       public void run() {
         applyUpdates();
       }
-    }, initialDelay, frequencyInHour*(ChronoUnit.MINUTES.getDuration().getSeconds()), TimeUnit.MINUTES);
+    }, initialDelay, frequencyInHour * (ChronoUnit.MINUTES.getDuration().getSeconds()), TimeUnit.MINUTES);
   }
 
   @Override
@@ -89,25 +98,25 @@ public class DirectoryUpdateService extends AbstractIdleService {
   }
 
   /**
-   * Apply the updates from the Directory to the Registry
+   * Apply the updates from the Directory to the Registry (if required)
+   *
    */
-  public void applyUpdates(){
+  public void applyUpdates() {
     //get all participant from the Directory
     PagingResponse<Participant> participants = directoryParticipantService.list(new PagingRequest(0, DEFAULT_MAX_LIMIT));
     PagingResponse<Node> nodes = directoryNodeService.list(new PagingRequest(0, DEFAULT_MAX_LIMIT));
 
-    Map<Integer,Participant> participantsById = Maps.newHashMap();
-    Map<Integer,Node> nodeByParticipantsId = Maps.newHashMap();
+    Map<Integer, Participant> participantsById = Maps.newHashMap();
+    Map<Integer, Node> nodeByParticipantsId = Maps.newHashMap();
 
-    for(Participant participant : participants.getResults()){
+    for (Participant participant : participants.getResults()) {
       participantsById.put(participant.getId(), participant);
     }
 
-    for(Node node : nodes.getResults()){
-      if(node.getParticipantId() != null){
+    for (Node node : nodes.getResults()) {
+      if (node.getParticipantId() != null) {
         nodeByParticipantsId.put(node.getParticipantId(), node);
-      }
-      else{
+      } else {
         LOG.warn("Directory Node {} without link to a Participant skipped.", node.getId());
       }
     }
@@ -116,19 +125,17 @@ public class DirectoryUpdateService extends AbstractIdleService {
     Integer participantId;
     Participant participant;
     Node directoryNode;
-    for(org.gbif.api.model.registry.Node registryNode : registryNodes){
+    for (org.gbif.api.model.registry.Node registryNode : registryNodes) {
       participantId = findParticipantId(registryNode);
-      if(participantId != null){
+      if (participantId != null) {
         participant = participantsById.get(participantId);
         directoryNode = nodeByParticipantsId.get(participantId);
-        // should we handle participant without node ?
-        if(participant != null && directoryNode != null) {
+        if (participant != null) {
           if (shouldUpdateNode(registryNode, participant, directoryNode)) {
-            LOG.info("Requires update: " + registryNode.getTitle() + " should be update to " + directoryNode.getName());
+            updateRegistryNode(registryNode, participant, directoryNode);
           }
-        }
-        else{
-          LOG.info("Can't find participantId " + participantId);
+        } else {
+          LOG.warn("Can't find participantId {} in the Directory", participantId);
         }
       }
     }
@@ -138,37 +145,40 @@ public class DirectoryUpdateService extends AbstractIdleService {
   /**
    * Check if a registry Node requires an update.
    *
-   * @param registryNode
-   * @param participant
-   * @param directoryNode
-   * @return
    */
-  private boolean shouldUpdateNode(org.gbif.api.model.registry.Node registryNode, Participant participant, Node directoryNode) {
+  private boolean shouldUpdateNode(org.gbif.api.model.registry.Node registryNode, Participant participant,
+                                   @Nullable Node directoryNode) {
+
+    String titleFromDirectory = directoryNode != null ? directoryNode.getName() : participant.getName();
     return !Objects.equals(
-            registryNode.getTitle(), directoryNode.getName())
+            registryNode.getTitle(), titleFromDirectory)
             || !Objects.equals(registryNode.getCountry(), participant.getCountryCode())
             || !Objects.equals(DirectoryRegistryConstantsMapping.PARTICIPATION_STATUS.get(participant.getParticipationStatus()),
-              registryNode.getParticipationStatus())
+            registryNode.getParticipationStatus())
             || !Objects.equals(DirectoryRegistryConstantsMapping.PARTICIPATION_TYPE.get(participant.getType()),
-              registryNode.getType())
+            registryNode.getType())
             || !Objects.equals(registryNode.getGbifRegion(), participant.getGbifRegion());
   }
 
   /**
    * Update a Registry Node from a Directory Participant and Node.
+   * registryNode title is set to the name of the Node from the Directory with a fallback on participant name is
+   * no node is available in the Directory.
    *
-   * @param registryNode
-   * @param participant
-   * @param directoryNode
    */
-  private void updateRegistryNode(org.gbif.api.model.registry.Node registryNode, Participant participant, Node directoryNode){
-    registryNode.setTitle(directoryNode.getName());
+  private void updateRegistryNode(org.gbif.api.model.registry.Node registryNode, Participant participant,
+                                  @Nullable Node directoryNode) {
+    String titleFromDirectory = directoryNode != null ? directoryNode.getName() : participant.getName();
+    registryNode.setTitle(titleFromDirectory);
     registryNode.setCountry(participant.getCountryCode());
     registryNode.setParticipationStatus(DirectoryRegistryConstantsMapping.PARTICIPATION_STATUS.get(participant.getParticipationStatus()));
     registryNode.setType(DirectoryRegistryConstantsMapping.PARTICIPATION_TYPE.get(participant.getType()));
     registryNode.setGbifRegion(participant.getGbifRegion());
 
-   // participant.getContinent()
+    registryNode.setModifiedBy(DIRECTORY_UPDATE_USER);
+
+    nodeMapper.update(registryNode);
+    LOG.info("Node {} ({}) updated by {}", registryNode.getTitle(), registryNode.getKey(), DIRECTORY_UPDATE_USER);
   }
 
   private static Integer findParticipantId(org.gbif.api.model.registry.Node node) {
