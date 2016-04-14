5353bed8754e1d050961c72b8c2385c98d7b31c7
Christian Gendreau <cgendreau@gbif.org>
2016-04-14 10:44:43 +0200

    update versions, tiny fix in MetricsClient

--- registry-ws/src/main/java/org/gbif/registry/metrics/OccurenceMetricsWsClient.java
+++ registry-ws/src/main/java/org/gbif/registry/metrics/OccurenceMetricsWsClient.java
@@ -10,7 +10,7 @@ import com.sun.jersey.core.util.MultivaluedMapImpl;
 /**
  *
  * HTTP based WebService client for Occurrence metrics.
- * This class uses the HTTP API and includes no dependencies to the metrics project.
+ * This class uses the HTTP API and includes no dependencies of the metrics project.
  *
  */
 public class OccurenceMetricsWsClient implements OccurrenceMetricsClient {
@@ -19,15 +19,19 @@ public class OccurenceMetricsWsClient implements OccurrenceMetricsClient {
   private static final String OCCURRENCE_COUNT_PATH = "occurrence/count";
   private WebResource resource;
 
+  /**
+   *
+   * @param resource WebResource pointing to the API
+   */
   @Inject
   public OccurenceMetricsWsClient(WebResource resource) {
-    this.resource = resource;
+    this.resource = resource.path(OCCURRENCE_COUNT_PATH);
   }
 
   @Override
   public Long getCountForDataset(UUID datasetKey) {
     MultivaluedMap<String, String> params = new MultivaluedMapImpl();
     params.putSingle(DATASET_KEY, datasetKey.toString());
-    return resource.path(OCCURRENCE_COUNT_PATH).queryParams(params).get(Long.class);
+    return resource.queryParams(params).get(Long.class);
   }
 }
