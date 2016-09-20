eab01131ab25ebce2041cc19b9ec5a35279d8b03
Christian Gendreau <cgendreau@gbif.org>
2016-09-20 13:44:27 +0200

    added all creators + contributors for Datacite metadata (dataset)

--- registry-doi/src/main/java/org/gbif/registry/doi/DataCiteConverter.java
+++ registry-doi/src/main/java/org/gbif/registry/doi/DataCiteConverter.java
@@ -9,9 +9,11 @@ import org.gbif.api.model.registry.DatasetOccurrenceDownloadUsage;
 import org.gbif.api.model.registry.Organization;
 import org.gbif.api.model.registry.eml.KeywordCollection;
 import org.gbif.api.model.registry.eml.geospatial.GeospatialCoverage;
+import org.gbif.api.vocabulary.ContactType;
 import org.gbif.api.vocabulary.IdentifierType;
 import org.gbif.api.vocabulary.Language;
 import org.gbif.api.vocabulary.License;
+import org.gbif.doi.metadata.datacite.ContributorType;
 import org.gbif.doi.metadata.datacite.DataCiteMetadata;
 import org.gbif.doi.metadata.datacite.DateType;
 import org.gbif.doi.metadata.datacite.DescriptionType;
@@ -41,6 +43,7 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
@@ -53,9 +56,25 @@ public class DataCiteConverter {
 
   private static final ObjectFactory FACTORY = new ObjectFactory();
 
-  //http://orcid.org/0000-0002-7101-9767
   public static final String ORCID_NAME_IDENTIFIER_SCHEME = "ORCID";
   public static final String RESEARCHERID_NAME_IDENTIFIER_SCHEME = "ResearcherID";
+
+  private static Map<ContactType, ContributorType> REGISTRY_DATACITE_ROLE_MAPPING =
+          ImmutableMap.<ContactType, ContributorType>builder().
+                  put(ContactType.EDITOR, ContributorType.EDITOR).
+                  put(ContactType.PUBLISHER, ContributorType.EDITOR).
+                  put(ContactType.CONTENT_PROVIDER, ContributorType.DATA_COLLECTOR).
+                  put(ContactType.CUSTODIAN_STEWARD, ContributorType.DATA_MANAGER).
+                  put(ContactType.CURATOR, ContributorType.DATA_CURATOR).
+                  put(ContactType.METADATA_AUTHOR, ContributorType.DATA_CURATOR).
+                  put(ContactType.DISTRIBUTOR, ContributorType.DISTRIBUTOR).
+                  put(ContactType.OWNER, ContributorType.RIGHTS_HOLDER).
+                  put(ContactType.POINT_OF_CONTACT, ContributorType.CONTACT_PERSON).
+                  put(ContactType.PRINCIPAL_INVESTIGATOR, ContributorType.PROJECT_LEADER).
+                  put(ContactType.ORIGINATOR, ContributorType.DATA_COLLECTOR).
+                  put(ContactType.PROCESSOR, ContributorType.PRODUCER).
+                  put(ContactType.PROGRAMMER, ContributorType.PRODUCER).
+          build();
 
   // Patterns must return 2 groups: scheme and id
   public static final Map<Pattern, String> SUPPORTED_SCHEMES = ImmutableMap.of(
@@ -93,18 +112,7 @@ public class DataCiteConverter {
             .withPublisher(publisher.getTitle())
                     // default to this year, e.g. when creating new datasets. This field is required!
             .withPublicationYear(getYear(new Date()))
-            .withResourceType().withResourceTypeGeneral(ResourceType.DATASET).withValue(d.getType().name()).end()
-            .withCreators()
-            .addCreator()
-            .withCreatorName(publisher.getTitle())
-            .withNameIdentifier()
-            .withValue(publisher.getKey().toString())
-            .withSchemeURI("gbif.org")
-            .withNameIdentifierScheme("GBIF")
-            .end()
-            .end()
-            .end()
-            .withRelatedIdentifiers().end();
+            .withResourceType().withResourceTypeGeneral(ResourceType.DATASET).withValue(d.getType().name()).end();
 
     if (d.getCreated() != null) {
       b.withPublicationYear(getYear(d.getModified()))
@@ -118,23 +126,33 @@ public class DataCiteConverter {
     if(d.getContacts() != null && !d.getContacts().isEmpty()){
       ContactAdapter contactAdapter = new ContactAdapter(d.getContacts());
 
-      Contact resourceCreator = contactAdapter.getResourceCreator();
-      if(resourceCreator != null){
-        b.withCreators().addCreator(toDataCiteCreator(resourceCreator)).end().end();
+      //handle Creators
+      List<Contact> resourceCreators = contactAdapter.getCreators();
+      DataCiteMetadata.Creators.Builder creatorsBuilder = b.withCreators();
+      for(Contact resourceCreator : resourceCreators) {
+        creatorsBuilder.addCreator(toDataCiteCreator(resourceCreator)).end();
       }
+      creatorsBuilder.end();
+      // creator is mandatory, build a default one
+      if(resourceCreators.isEmpty()){
+        b.withCreators().addCreator(getDefaultGBIFDataCiteCreator(d.getCreatedBy())).end().end();
+      }
+
+      //handle Contributors
+      List<Contact> contributors = Lists.newArrayList(d.getContacts());
+      contributors.removeAll(resourceCreators);
+
+      if(!contributors.isEmpty()) {
+        DataCiteMetadata.Contributors.Builder contributorsBuilder = b.withContributors();
+        for (Contact contributor : contributors) {
+          contributorsBuilder.addContributor(toDataCiteContributor(contributor)).end();
+        }
+        contributorsBuilder.end();
+      }
+
     }
-    else{ // creator is mandatory
-      b.withCreators()
-              .addCreator()
-              .withCreatorName(d.getCreatedBy())
-              .withNameIdentifier()
-              .withValue(d.getCreatedBy())
-              .withSchemeURI("gbif.org")
-              .withNameIdentifierScheme("GBIF")
-              .end()// withNameIdentifier
-              .end()// withCreatorName
-              .end()// addCreator
-              .end(); // withCreators
+    else{ // creator is mandatory, build a default one
+      b.withCreators().addCreator(getDefaultGBIFDataCiteCreator(d.getCreatedBy())).end().end();
     }
 
     if (d.getPubDate() != null) {
@@ -308,6 +326,7 @@ public class DataCiteConverter {
 
   /**
    * Transforms a Contact into a Datacite Creator.
+   *
    * @param contact
    * @return
    */
@@ -320,7 +339,7 @@ public class DataCiteConverter {
     }
 
     for(String userId : contact.getUserId()){
-      DataCiteMetadata.Creators.Creator.NameIdentifier nId = userIdToNameIdentifier(userId);
+      DataCiteMetadata.Creators.Creator.NameIdentifier nId = userIdToCreatorNameIdentifier(userId);
       if(nId != null){
         creator.setNameIdentifier(nId);
         //we take the first we can support
@@ -330,14 +349,55 @@ public class DataCiteConverter {
     return creator;
   }
 
+  /**
+   * Transforms a Contact into a Datacite Creator.
+   * @param fullname
+   * @return
+   */
+  private static DataCiteMetadata.Creators.Creator getDefaultGBIFDataCiteCreator(String fullname){
+    DataCiteMetadata.Creators.Creator creator = FACTORY.createDataCiteMetadataCreatorsCreator();
+    creator.setCreatorName(fullname);
+    DataCiteMetadata.Creators.Creator.NameIdentifier nid =
+            FACTORY.createDataCiteMetadataCreatorsCreatorNameIdentifier();
+    nid.setValue(fullname);
+    nid.setSchemeURI("gbif.org");
+    nid.setNameIdentifierScheme("GBIF");
+    creator.setNameIdentifier(nid);
+    return creator;
+  }
+
+  /**
+   * Transforms a Contact into a Datacite Contributor.
+   *
+   * @param contact
+   * @return
+   */
+  private static DataCiteMetadata.Contributors.Contributor toDataCiteContributor(Contact contact){
+    DataCiteMetadata.Contributors.Contributor contributor = FACTORY.createDataCiteMetadataContributorsContributor();
+    contributor.setContributorName(ContactAdapter.formatContactName(contact));
+
+    ContributorType contributorType = REGISTRY_DATACITE_ROLE_MAPPING.containsKey(contact.getType()) ?
+            REGISTRY_DATACITE_ROLE_MAPPING.get(contact.getType()) : ContributorType.RELATED_PERSON;
+    contributor.setContributorType(contributorType);
+
+    for(String userId : contact.getUserId()){
+      DataCiteMetadata.Contributors.Contributor.NameIdentifier nId = userIdToContributorNameIdentifier(userId);
+      if(nId != null){
+        contributor.setNameIdentifier(nId);
+        //we take the first we can support
+        break;
+      }
+    }
+    return contributor;
+  }
 
   /**
    * Transforms an userId in the form of https://orcid.org/0000-0000-0000-00001 into a Datacite NameIdentifier object.
    * @param userId
-   * @return a NameIdentifier instance or null if the object can not be built (e.g. unsupported scheme)
+   * @return a Creator.NameIdentifier instance or null if the object can not be built (e.g. unsupported scheme)
    */
   @VisibleForTesting
-  protected static DataCiteMetadata.Creators.Creator.NameIdentifier userIdToNameIdentifier(String userId){
+  protected static DataCiteMetadata.Creators.Creator.NameIdentifier userIdToCreatorNameIdentifier(String userId){
     if(Strings.isNullOrEmpty(userId)){
       return null;
     }
@@ -347,6 +407,31 @@ public class DataCiteConverter {
       if(matcher.matches()){
         DataCiteMetadata.Creators.Creator.NameIdentifier nid =
                 FACTORY.createDataCiteMetadataCreatorsCreatorNameIdentifier();
+        // group 0 = the entire string
+        nid.setSchemeURI(matcher.group(1));
+        nid.setValue(matcher.group(2));
+        nid.setNameIdentifierScheme(SUPPORTED_SCHEMES.get(pattern));
+        return nid;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Transforms an userId in the form of https://orcid.org/0000-0000-0000-00001 into a Datacite NameIdentifier object.
+   * @param userId
+   * @return a Contributor.NameIdentifier instance or null if the object can not be built (e.g. unsupported scheme)
+   */
+  protected static DataCiteMetadata.Contributors.Contributor.NameIdentifier userIdToContributorNameIdentifier(String userId){
+    if(Strings.isNullOrEmpty(userId)){
+      return null;
+    }
+
+    for(Pattern pattern : SUPPORTED_SCHEMES.keySet()){
+      Matcher matcher = pattern.matcher(userId);
+      if(matcher.matches()){
+        DataCiteMetadata.Contributors.Contributor.NameIdentifier nid =
+                FACTORY.createDataCiteMetadataContributorsContributorNameIdentifier();
         // group 0 = the entire string
         nid.setSchemeURI(matcher.group(1));
         nid.setValue(matcher.group(2));
--- registry-doi/src/test/java/org/gbif/registry/doi/DataCiteConverterTest.java
+++ registry-doi/src/test/java/org/gbif/registry/doi/DataCiteConverterTest.java
@@ -39,162 +39,169 @@ import static org.mockito.Mockito.when;
 
 public class DataCiteConverterTest {
 
-    @Test
-    public void testGetYear() throws Exception {
-        Date d = new Date(1418340702253l);
-        assertEquals("2014", DataCiteConverter.getYear(d));
-        assertNull(DataCiteConverter.getYear(null));
-    }
+  @Test
+  public void testGetYear() throws Exception {
+    Date d = new Date(1418340702253l);
+    assertEquals("2014", DataCiteConverter.getYear(d));
+    assertNull(DataCiteConverter.getYear(null));
+  }
 
-    @Test
-    public void testConvertDataset() throws Exception {
-        Organization publisher = new Organization();
-        publisher.setTitle("X-Publisher");
-        publisher.setKey(UUID.randomUUID());
+  @Test
+  public void testConvertDataset() throws Exception {
+    Organization publisher = new Organization();
+    publisher.setTitle("X-Publisher");
+    publisher.setKey(UUID.randomUUID());
 
-        final DOI doi = new DOI("10.1234/5678");
-        Dataset d = new Dataset();
-        d.setKey(UUID.randomUUID());
-        d.setType(DatasetType.OCCURRENCE);
-        d.setTitle("my title");
-        d.setCreated(new Date());
-        d.setModified(new Date());
-        d.setCreatedBy("Markus GBIF User");
-        d.setLanguage(Language.NORWEGIAN);
-        d.setDataLanguage(Language.NORWEGIAN);
-        d.setLicense(License.CC0_1_0);
+    final DOI doi = new DOI("10.1234/5678");
+    Dataset d = new Dataset();
+    d.setKey(UUID.randomUUID());
+    d.setType(DatasetType.OCCURRENCE);
+    d.setTitle("my title");
+    d.setCreated(new Date());
+    d.setModified(new Date());
+    d.setCreatedBy("Markus GBIF User");
+    d.setLanguage(Language.NORWEGIAN);
+    d.setDataLanguage(Language.NORWEGIAN);
+    d.setLicense(License.CC0_1_0);
 
-        Contact contact = new Contact();
-        contact.setFirstName("Markus");
-        contact.setType(ContactType.ORIGINATOR);
-        d.getContacts().add(contact);
+    Contact contact = new Contact();
+    contact.setFirstName("Markus");
+    contact.setType(ContactType.ORIGINATOR);
+    d.getContacts().add(contact);
 
-        DataCiteMetadata m = convertAndValidate(doi, d, publisher);
-        assertEquals("my title", m.getTitles().getTitle().get(0).getValue());
-        assertEquals("Markus", m.getCreators().getCreator().get(0).getCreatorName());
-        assertEquals(doi.getDoiName(), m.getIdentifier().getValue());
+    contact = new Contact();
+    contact.setFirstName("Hubert");
+    contact.setLastName("Reeves");
+    contact.setType(ContactType.METADATA_AUTHOR);
+    d.getContacts().add(contact);
 
-        d.setDoi(doi);
-        d.setDescription("bla bla bla bla bla bla bla bla - I talk too much");
-        List<GeospatialCoverage> geos = Lists.newArrayList();
-        d.setGeographicCoverages(geos);
-        GeospatialCoverage g = new GeospatialCoverage();
-        geos.add(g);
-        g.setBoundingBox(new BoundingBox(1, 2, 3, 4));
+    DataCiteMetadata m = convertAndValidate(doi, d, publisher);
+    assertEquals("my title", m.getTitles().getTitle().get(0).getValue());
+    assertEquals("Markus", m.getCreators().getCreator().get(0).getCreatorName());
+    assertEquals(doi.getDoiName(), m.getIdentifier().getValue());
 
-        m = convertAndValidate(doi, d, publisher);
-        assertEquals("my title", m.getTitles().getTitle().get(0).getValue());
-        assertEquals("Markus", m.getCreators().getCreator().get(0).getCreatorName());
-        assertEquals("10.1234/5678", m.getIdentifier().getValue());
-        assertEquals(Lists.<Double>newArrayList(1d, 3d, 2d, 4d), m.getGeoLocations().getGeoLocation().get(0).getGeoLocationBox());
-        assertEquals(d.getDescription(), m.getDescriptions().getDescription().get(0).getContent().get(0));
-        assertEquals(License.CC0_1_0.getLicenseUrl(), m.getRightsList().getRights().get(0).getRightsURI());
-    }
+    d.setDoi(doi);
+    d.setDescription("bla bla bla bla bla bla bla bla - I talk too much");
+    List<GeospatialCoverage> geos = Lists.newArrayList();
+    d.setGeographicCoverages(geos);
+    GeospatialCoverage g = new GeospatialCoverage();
+    geos.add(g);
+    g.setBoundingBox(new BoundingBox(1, 2, 3, 4));
 
-    private DataCiteMetadata convertAndValidate(DOI doi, Dataset d, Organization publisher) throws InvalidMetadataException {
-        DataCiteMetadata m = DataCiteConverter.convert(d, publisher);
-        DataCiteValidator.toXml(doi, m);
-        return m;
-    }
+    m = convertAndValidate(doi, d, publisher);
+    assertEquals("my title", m.getTitles().getTitle().get(0).getValue());
+    assertEquals("Markus", m.getCreators().getCreator().get(0).getCreatorName());
+    assertEquals("Hubert Reeves", m.getContributors().getContributor().get(0).getContributorName());
+    assertEquals("10.1234/5678", m.getIdentifier().getValue());
+    assertEquals(Lists.<Double>newArrayList(1d, 3d, 2d, 4d), m.getGeoLocations().getGeoLocation().get(0).getGeoLocationBox());
+    assertEquals(d.getDescription(), m.getDescriptions().getDescription().get(0).getContent().get(0));
+    assertEquals(License.CC0_1_0.getLicenseUrl(), m.getRightsList().getRights().get(0).getRightsURI());
+  }
 
-    @Test
-    public void testConvertDownload() throws Exception {
-        DatasetOccurrenceDownloadUsage du1 = new DatasetOccurrenceDownloadUsage();
-        du1.setDatasetKey(UUID.randomUUID());
-        du1.setDatasetTitle("my title");
-        du1.setDatasetDOI(new DOI("10.1234/5679"));
-        du1.setNumberRecords(101);
+  private DataCiteMetadata convertAndValidate(DOI doi, Dataset d, Organization publisher) throws InvalidMetadataException {
+    DataCiteMetadata m = DataCiteConverter.convert(d, publisher);
+    DataCiteValidator.toXml(doi, m);
+    return m;
+  }
 
-        DatasetOccurrenceDownloadUsage du2 = new DatasetOccurrenceDownloadUsage();
-        du2.setDatasetKey(UUID.randomUUID());
-        du2.setDatasetTitle("my title #2");
-        du2.setDatasetDOI(new DOI("10.1234/klimbim"));
-        du2.setNumberRecords(2002);
+  @Test
+  public void testConvertDownload() throws Exception {
+    DatasetOccurrenceDownloadUsage du1 = new DatasetOccurrenceDownloadUsage();
+    du1.setDatasetKey(UUID.randomUUID());
+    du1.setDatasetTitle("my title");
+    du1.setDatasetDOI(new DOI("10.1234/5679"));
+    du1.setNumberRecords(101);
 
-        Download download = new Download();
-        download.setCreated(new Date());
-        download.setDoi(new DOI("10.1234/5678"));
-        download.setKey("1");
-        download.setModified(new Date());
-        download.setNumberDatasets(1l);
-        download.setSize(100);
-        download.setStatus(Download.Status.SUCCEEDED);
-        download.setTotalRecords(10);
-        DownloadRequest downloadRequest = new DownloadRequest();
-        downloadRequest.setCreator("dev@gbif.org");
-        download.setRequest(downloadRequest);
+    DatasetOccurrenceDownloadUsage du2 = new DatasetOccurrenceDownloadUsage();
+    du2.setDatasetKey(UUID.randomUUID());
+    du2.setDatasetTitle("my title #2");
+    du2.setDatasetDOI(new DOI("10.1234/klimbim"));
+    du2.setNumberRecords(2002);
 
-        User user = new User();
-        user.setUserName("peta");
-        user.setEmail("aha@music.com");
-        user.setFirstName("Pete");
-        user.setEmail("Doherty");
+    Download download = new Download();
+    download.setCreated(new Date());
+    download.setDoi(new DOI("10.1234/5678"));
+    download.setKey("1");
+    download.setModified(new Date());
+    download.setNumberDatasets(1l);
+    download.setSize(100);
+    download.setStatus(Download.Status.SUCCEEDED);
+    download.setTotalRecords(10);
+    DownloadRequest downloadRequest = new DownloadRequest();
+    downloadRequest.setCreator("dev@gbif.org");
+    download.setRequest(downloadRequest);
 
-        // mock title lookup API
-        TitleLookup tl = mock(TitleLookup.class);
-        when(tl.getDatasetTitle(anyString())).thenReturn("PonTaurus");
-        when(tl.getSpeciesName(anyString())).thenReturn("Abies alba Mill.");
+    User user = new User();
+    user.setUserName("peta");
+    user.setEmail("aha@music.com");
+    user.setFirstName("Pete");
+    user.setEmail("Doherty");
 
-        DataCiteMetadata metadata = DataCiteConverter.convert(download, user, Lists.newArrayList(du1, du2), tl);
-        String xml = DataCiteValidator.toXml(download.getDoi(), metadata);
-        assertTrue(xml.contains(du1.getDatasetDOI().getDoiName()));
-        assertTrue(xml.contains(du2.getDatasetDOI().getDoiName()));
-        assertTrue(xml.contains(String.valueOf(du1.getNumberRecords())));
-        assertTrue(xml.contains(String.valueOf(du2.getNumberRecords())));
-    }
+    // mock title lookup API
+    TitleLookup tl = mock(TitleLookup.class);
+    when(tl.getDatasetTitle(anyString())).thenReturn("PonTaurus");
+    when(tl.getSpeciesName(anyString())).thenReturn("Abies alba Mill.");
 
-    @Test
-    public void testDatasetLicense() throws Exception {
-        Organization publisher = new Organization();
-        publisher.setTitle("My Publisher");
-        publisher.setKey(UUID.randomUUID());
+    DataCiteMetadata metadata = DataCiteConverter.convert(download, user, Lists.newArrayList(du1, du2), tl);
+    String xml = DataCiteValidator.toXml(download.getDoi(), metadata);
+    assertTrue(xml.contains(du1.getDatasetDOI().getDoiName()));
+    assertTrue(xml.contains(du2.getDatasetDOI().getDoiName()));
+    assertTrue(xml.contains(String.valueOf(du1.getNumberRecords())));
+    assertTrue(xml.contains(String.valueOf(du2.getNumberRecords())));
+  }
 
-        final DOI doi = new DOI("10.1234/5679");
-        Dataset d = new Dataset();
-        d.setKey(UUID.randomUUID());
-        d.setType(DatasetType.METADATA);
-        d.setTitle("My Metadata");
-        d.setCreated(new Date());
-        d.setModified(new Date());
-        d.setCreatedBy("Jim");
-        d.setLanguage(Language.ENGLISH);
-        d.setDataLanguage(Language.ENGLISH);
-        d.setRights("Copyright ©");
+  @Test
+  public void testDatasetLicense() throws Exception {
+    Organization publisher = new Organization();
+    publisher.setTitle("My Publisher");
+    publisher.setKey(UUID.randomUUID());
 
-        DataCiteMetadata m = convertAndValidate(doi, d, publisher);
-        assertEquals("Copyright ©", m.getRightsList().getRights().get(0).getValue());
-    }
+    final DOI doi = new DOI("10.1234/5679");
+    Dataset d = new Dataset();
+    d.setKey(UUID.randomUUID());
+    d.setType(DatasetType.METADATA);
+    d.setTitle("My Metadata");
+    d.setCreated(new Date());
+    d.setModified(new Date());
+    d.setCreatedBy("Jim");
+    d.setLanguage(Language.ENGLISH);
+    d.setDataLanguage(Language.ENGLISH);
+    d.setRights("Copyright ©");
 
-    @Test
-    public void testUserIdToNameIdentifier() throws Exception {
-        DataCiteMetadata.Creators.Creator.NameIdentifier nid =
-                DataCiteConverter.userIdToNameIdentifier("http://orcid.org/0000-0000-0000-0001");
-        assertEquals("http://orcid.org/", nid.getSchemeURI());
-        assertEquals("0000-0000-0000-0001", nid.getValue());
-    }
+    DataCiteMetadata m = convertAndValidate(doi, d, publisher);
+    assertEquals("Copyright ©", m.getRightsList().getRights().get(0).getValue());
+  }
 
-    @Test
-    public void testTruncateDesription() throws Exception {
-        DOI doi = new DOI("10.15468/dl.v8zc57");
-        String xml = Resources.toString(Resources.getResource("metadata/datacite-large.xml"), Charsets.UTF_8);
-        String xml2 = DataCiteConverter.truncateDescription(doi, xml, URI.create("http://gbif.org"));
-        //System.out.println(xml2);
-        DataCiteValidator.validateMetadata(xml2);
-        assertTrue(xml2.contains("for full list of all constituents"));
-        assertFalse(xml2.contains("University of Ghent"));
-        assertTrue(xml2.contains("10.15468/siye1z"));
-        assertEquals(3648, xml2.length());
-    }
+  @Test
+  public void testUserIdToNameIdentifier() throws Exception {
+    DataCiteMetadata.Creators.Creator.NameIdentifier nid =
+            DataCiteConverter.userIdToNameIdentifier("http://orcid.org/0000-0000-0000-0001");
+    assertEquals("http://orcid.org/", nid.getSchemeURI());
+    assertEquals("0000-0000-0000-0001", nid.getValue());
+  }
 
-    @Test
-    public void testTruncateConstituents() throws Exception {
-        DOI doi = new DOI("10.15468/dl.v8zc57");
-        String xml = Resources.toString(Resources.getResource("metadata/datacite-large.xml"), Charsets.UTF_8);
-        String xml2 = DataCiteConverter.truncateConstituents(doi, xml, URI.create("http://gbif.org"));
-        DataCiteValidator.validateMetadata(xml2);
-        assertTrue(xml2.contains("for full list of all constituents"));
-        assertFalse(xml2.contains("University of Ghent"));
-        assertFalse(xml2.contains("10.15468/siye1z"));
-        assertEquals(2310, xml2.length());
-    }
+  @Test
+  public void testTruncateDesription() throws Exception {
+    DOI doi = new DOI("10.15468/dl.v8zc57");
+    String xml = Resources.toString(Resources.getResource("metadata/datacite-large.xml"), Charsets.UTF_8);
+    String xml2 = DataCiteConverter.truncateDescription(doi, xml, URI.create("http://gbif.org"));
+    //System.out.println(xml2);
+    DataCiteValidator.validateMetadata(xml2);
+    assertTrue(xml2.contains("for full list of all constituents"));
+    assertFalse(xml2.contains("University of Ghent"));
+    assertTrue(xml2.contains("10.15468/siye1z"));
+    assertEquals(3648, xml2.length());
+  }
+
+  @Test
+  public void testTruncateConstituents() throws Exception {
+    DOI doi = new DOI("10.15468/dl.v8zc57");
+    String xml = Resources.toString(Resources.getResource("metadata/datacite-large.xml"), Charsets.UTF_8);
+    String xml2 = DataCiteConverter.truncateConstituents(doi, xml, URI.create("http://gbif.org"));
+    DataCiteValidator.validateMetadata(xml2);
+    assertTrue(xml2.contains("for full list of all constituents"));
+    assertFalse(xml2.contains("University of Ghent"));
+    assertFalse(xml2.contains("10.15468/siye1z"));
+    assertEquals(2310, xml2.length());
+  }
 }
