d98ea391acfd3bcb3161a96183f0b68c998a96c9
Kyle Braak <kbraak@gbif.org>
2016-10-24 11:54:40 +0200

    Fix ensures DatasetResource.sanitizeDataset method won't remove HTML line break added during metadata parsing

--- registry-metadata/src/main/java/org/gbif/registry/metadata/parse/ParagraphContainer.java
+++ registry-metadata/src/main/java/org/gbif/registry/metadata/parse/ParagraphContainer.java
@@ -11,12 +11,12 @@ import com.google.common.collect.Lists;
  * concatenated string argument in other rules.
  * Digester needs public access to this otherwise package scoped class.
  * </br>
- * Note HTML is used to concatenate paragraphs using break tag </br> instead of newline character ("\n"), see POR-3138.
+ * Note HTML is used to concatenate paragraphs using break tag <br> instead of newline character ("\n"), see POR-3138.
  * @see <a href="http://dev.gbif.org/issues/browse/POR-3138">POR-3138</a>
  */
 public class ParagraphContainer {
 
-  private static Joiner PARA_JOIN = Joiner.on("</br>");
+  private static Joiner PARA_JOIN = Joiner.on("<br />");
   private List<String> paragraphs = Lists.newArrayList();
 
   public void appendParagraph(String para) {
