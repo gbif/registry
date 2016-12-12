a2a6e0d457a22758dc584563f6ec4c6184bb526e
Markus Döring <m.doering@mac.com>
2016-12-12 13:49:17 +0100

    use oozie only for java indexing

--- registry-index-builder/install-workflow.sh
+++ registry-index-builder/install-workflow.sh
@@ -10,11 +10,15 @@ curl -s -H "Authorization: token $TOKEN" -H 'Accept: application/vnd.github.v3.r
 
 #extract the oozie.url value from the properties file
 oozie_url=`cat $P.properties| grep "oozie.url" | cut -d'=' -f2`
+solr_url=`cat $P.properties| grep "solr.url" | cut -d'=' -f2`
+solr_collection=`cat $P.properties| grep "solr.dataset.collection" | cut -d'=' -f2`
+solr_collection_opts=`cat $P.properties| grep "oozie.url" | cut -d'=' -f2`
+solr_home=`cat $P.properties| grep "solr.home" | cut -d'=' -f2`
+zk_host=`cat $P.properties| grep "solr.dataset.home" | cut -d'=' -f2`
 
 echo "Assembling jar for $ENV"
 
 mvn -Poozie,$P clean package -DskipTests assembly:single
-mvn -Psolr,$P package -DskipTests assembly:single
 
 if hdfs dfs -test -d /registry-index-builder-$P/; then
    echo "Removing content of current Oozie workflow directory"
@@ -26,6 +30,15 @@ fi
 echo "Copying new Oozie workflow to HDFS"
 hdfs dfs -copyFromLocal target/oozie-workflow/* /registry-index-builder-$P/
 hdfs dfs -copyFromLocal $P.properties /registry-index-builder-$P/lib/
+
+echo "Delete existing collection"
+curl -s """${solr_url}"/admin/collections?action=DELETE\&name="${solr_collection}"""
+
+echo "Delete existing collection"
+${solr_home}/server/scripts/cloud-scripts/zkcli.sh  -zkhost ${zk_host} -cmd upconfig -confname ${solr_collection} -confdir solr/dataset/conf/
+
+echo "Delete existing collection"
+curl -s """${solr_url}"/admin/collections?action=CREATE\&name="${solr_collection}"\&"${solr_collection_opts}"\&collection.configName="${solr_collection}"""
 
 echo "Executing Oozie workflow"
 oozie job --oozie ${oozie_url} -config $P.properties -run
