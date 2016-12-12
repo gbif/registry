5dd25e2368de0fb2b0b0a91207d2eed030ec7df1
Markus Döring <m.doering@mac.com>
2016-12-12 13:29:34 +0100

    Use released gbif deps

---
+++ registry-index-builder/src/main/java/org/gbif/registry/search/dataset/DatasetIndexApp.java
@@ -0,0 +1,16 @@
+package org.gbif.registry.search.dataset;
+
+/**
+ *
+ */
+public class DatasetIndexApp {
+
+  public static void main(String[] args) throws Exception {
+    // run dataset index builder just as in oozie
+    DatasetIndexBuilder.main(args);
+
+    // run checklist updaters just as in oozie
+    DatasetIndexChecklistUpdater.main(args);
+  }
+
+}
