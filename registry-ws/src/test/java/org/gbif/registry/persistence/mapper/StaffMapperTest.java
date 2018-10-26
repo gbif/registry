3110ecb472a489414f28ec3049f5bee30bea0456
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-10-26 13:30:51 +0200

    collections mappers

--- registry-ws/src/test/java/org/gbif/registry/persistence/mapper/StaffMapperTest.java
+++ registry-ws/src/test/java/org/gbif/registry/persistence/mapper/StaffMapperTest.java
@@ -1,8 +1,10 @@
 package org.gbif.registry.persistence.mapper;
 
 import org.gbif.api.model.collections.Address;
+import org.gbif.api.model.collections.Collection;
 import org.gbif.api.model.collections.Institution;
 import org.gbif.api.model.collections.Staff;
+import org.gbif.api.model.common.paging.Pageable;
 import org.gbif.registry.database.DatabaseInitializer;
 import org.gbif.registry.database.LiquibaseInitializer;
 import org.gbif.registry.database.LiquibaseModules;
@@ -20,6 +22,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -73,6 +76,117 @@ public class StaffMapperTest {
     // assert address
     assertEquals((Integer) 1, staffStored.getMailingAddress().getKey());
     assertEquals("dummy address", staffStored.getMailingAddress().getAddress());
+
+    staff.setFirstName("FN2");
+    staff.setLastName(null);
+    staff.setPhone("12234");
+    staffMapper.update(staff);
+    staffStored = staffMapper.get(key);
+
+    assertEquals("FN2", staffStored.getFirstName());
+    assertNull(staffStored.getLastName());
+    assertEquals("12234", staffStored.getPhone());
+    assertEquals("test@test.com", staffStored.getEmail());
+
+    // assert address
+    assertEquals((Integer) 1, staffStored.getMailingAddress().getKey());
+    assertEquals("dummy address", staffStored.getMailingAddress().getAddress());
+
+    // delete address
+    staff.setMailingAddress(null);
+    staffMapper.update(staff);
+    staffStored = staffMapper.get(key);
+    assertNull(staffStored.getMailingAddress());
+
+    // delete entity
+    staffMapper.delete(key);
+    staffStored = staffMapper.get(key);
+    assertNotNull(staffStored.getDeleted());
+  }
+
+  @Test
+  public void listTest() {
+    Staff s1 = new Staff();
+    s1.setKey(UUID.randomUUID());
+    s1.setFirstName("FN1");
+    s1.setCreatedBy("test");
+    s1.setModifiedBy("test");
+
+    Staff s2 = new Staff();
+    s2.setKey(UUID.randomUUID());
+    s2.setFirstName("FN2");
+    s2.setCreatedBy("test");
+    s2.setModifiedBy("test");
+
+    staffMapper.create(s1);
+    staffMapper.create(s2);
+
+    Pageable pageable = new Pageable() {
+      @Override
+      public int getLimit() {
+        return 5;
+      }
+
+      @Override
+      public long getOffset() {
+        return 0;
+      }
+    };
+
+    List<Staff> staffs = staffMapper.list(pageable);
+    assertEquals(2, staffs.size());
+  }
+
+  @Test
+  public void searchTest() {
+    Staff s1 = new Staff();
+    s1.setKey(UUID.randomUUID());
+    s1.setFirstName("FN1");
+    s1.setFax("12345");
+    s1.setCreatedBy("test");
+    s1.setModifiedBy("test");
+
+    Address address = new Address();
+    address.setKey(1);
+    address.setAddress("dummy address");
+    addressMapper.create(address);
+
+    s1.setMailingAddress(address);
+
+    Staff s2 = new Staff();
+    s2.setKey(UUID.randomUUID());
+    s2.setFirstName("FN2");
+    s2.setFax("12345");
+    s2.setCreatedBy("test");
+    s2.setModifiedBy("test");
+
+    staffMapper.create(s1);
+    staffMapper.create(s2);
+
+    Pageable pageable = new Pageable() {
+      @Override
+      public int getLimit() {
+        return 5;
+      }
+
+      @Override
+      public long getOffset() {
+        return 0;
+      }
+    };
+
+    List<Staff> staffs = staffMapper.search("FN1", pageable);
+    assertEquals(1, staffs.size());
+    assertEquals("FN1", staffs.get(0).getFirstName());
+
+    staffs = staffMapper.search("FN0", pageable);
+    assertEquals(0, staffs.size());
+
+    staffs = staffMapper.search("12345", pageable);
+    assertEquals(2, staffs.size());
+
+    staffs = staffMapper.search("dummy address", pageable);
+    assertEquals(1, staffs.size());
   }
 
 }
