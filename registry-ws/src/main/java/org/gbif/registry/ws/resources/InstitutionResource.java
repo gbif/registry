5cfcd1d6945fd7107db8686510c05606b5277d38
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-10-29 16:17:07 +0100

    fix update method

--- registry-ws/src/main/java/org/gbif/registry/ws/resources/InstitutionResource.java
+++ registry-ws/src/main/java/org/gbif/registry/ws/resources/InstitutionResource.java
@@ -29,6 +29,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -184,7 +185,7 @@ public class InstitutionResource implements InstitutionService {
         institutionMapper.search(query, pageable));
   }
 
-  @DELETE
+  @PUT
   @Path("{key}")
   @NullToNotFound
   @Validate
