d402228fd741ea419d8f96ef08c9ac63b0b8b985
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-10-31 17:21:02 +0100

    gbif-api version

--- registry-ws/src/main/java/org/gbif/registry/ws/resources/collections/CollectionResource.java
+++ registry-ws/src/main/java/org/gbif/registry/ws/resources/collections/CollectionResource.java
@@ -1,51 +1,28 @@
 package org.gbif.registry.ws.resources.collections;
 
 import org.gbif.api.model.collections.Collection;
-import org.gbif.api.model.collections.Staff;
 import org.gbif.api.model.common.paging.Pageable;
 import org.gbif.api.model.common.paging.PagingRequest;
 import org.gbif.api.model.common.paging.PagingResponse;
-import org.gbif.api.model.registry.Identifier;
-import org.gbif.api.model.registry.PrePersist;
-import org.gbif.api.model.registry.Tag;
 import org.gbif.api.service.collections.CollectionService;
-import org.gbif.registry.persistence.WithMyBatis;
 import org.gbif.registry.persistence.mapper.IdentifierMapper;
 import org.gbif.registry.persistence.mapper.TagMapper;
 import org.gbif.registry.persistence.mapper.collections.AddressMapper;
 import org.gbif.registry.persistence.mapper.collections.CollectionMapper;
-import org.gbif.registry.ws.guice.Trim;
-import org.gbif.ws.server.interceptor.NullToNotFound;
 
-import java.util.List;
 import java.util.UUID;
 import javax.annotation.Nullable;
-import javax.annotation.security.RolesAllowed;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.Default;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
 
 import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import org.apache.bval.guice.Validate;
-import org.mybatis.guice.transactional.Transactional;
-
-import static org.gbif.registry.ws.security.UserRoles.ADMIN_ROLE;
-import static org.gbif.registry.ws.security.UserRoles.EDITOR_ROLE;
-
-import static com.google.common.base.Preconditions.checkArgument;
 
 @Singleton
 @Consumes(MediaType.APPLICATION_JSON)
