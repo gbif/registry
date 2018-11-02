753b68fdaa0e07beed8765470f7741562e818ebf
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-11-02 19:55:37 +0100

    paging types in ws client collections

--- registry-ws-client/src/main/java/org/gbif/registry/ws/client/collections/CollectionWsClient.java
+++ registry-ws-client/src/main/java/org/gbif/registry/ws/client/collections/CollectionWsClient.java
@@ -1,27 +1,25 @@
-package org.gbif.registry.ws.client;
+package org.gbif.registry.ws.client.collections;
 
 import org.gbif.api.model.collections.Collection;
-import org.gbif.api.model.collections.Staff;
 import org.gbif.api.model.common.paging.Pageable;
 import org.gbif.api.model.common.paging.PagingResponse;
-import org.gbif.api.model.registry.Identifier;
-import org.gbif.api.model.registry.Tag;
 import org.gbif.api.service.collections.CollectionService;
 import org.gbif.registry.ws.client.guice.RegistryWs;
-import org.gbif.ws.client.BaseWsGetClient;
 import org.gbif.ws.client.QueryParamBuilder;
 
-import java.util.List;
 import java.util.UUID;
 import javax.annotation.Nullable;
-import javax.validation.constraints.NotNull;
 
 import com.google.inject.Inject;
+import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.ClientFilter;
 
-public class CollectionWsClient extends BaseExtendableColletionEntityClient<Collection>
+public class CollectionWsClient extends BaseExtendableCollectionEntityClient<Collection>
     implements CollectionService {
+
+  private static final GenericType<PagingResponse<Collection>> PAGING_COLLECTION =
+      new GenericType<PagingResponse<Collection>>() {};
 
   /**
    * @param resource the base url to the underlying webservice
@@ -30,15 +28,14 @@ public class CollectionWsClient extends BaseExtendableColletionEntityClient<Coll
   @Inject
   protected CollectionWsClient(
       @RegistryWs WebResource resource, @Nullable ClientFilter authFilter) {
-    super(
-        Collection.class, resource.path("collection"), authFilter, GenericTypes.PAGING_COLLECTION);
+    super(Collection.class, resource.path("collection"), authFilter, PAGING_COLLECTION);
   }
 
   @Override
   public PagingResponse<Collection> listByInstitution(
       UUID institutionKey, @Nullable Pageable pageable) {
     return get(
-        GenericTypes.PAGING_COLLECTION,
+        PAGING_COLLECTION,
         null,
         QueryParamBuilder.create("institution", institutionKey).build(),
         pageable);
