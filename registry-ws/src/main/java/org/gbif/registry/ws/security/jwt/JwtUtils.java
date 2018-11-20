6b115598c0a9bf5a715a328052236a80c68e1bc6
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-11-20 10:37:39 +0100

    JWT changes + tests

--- registry-ws/src/main/java/org/gbif/registry/ws/security/jwt/JwtUtils.java
+++ registry-ws/src/main/java/org/gbif/registry/ws/security/jwt/JwtUtils.java
@@ -1,13 +1,14 @@
 package org.gbif.registry.ws.security.jwt;
 
-import org.gbif.api.model.common.GbifUser;
 import org.gbif.registry.ws.security.jwt.JwtConfiguration.GbifClaims;
 
 import java.util.Date;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.HttpHeaders;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.sun.jersey.spi.container.ContainerRequest;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -17,12 +18,12 @@ public class JwtUtils {
   //Patterns that catches case insensitive versions of word 'bearer'
   private static final Pattern BEARER_PATTERN = Pattern.compile("(?i)bearer");
 
-  public static String generateJwt(GbifUser user, JwtConfiguration config) {
+  public static String generateJwt(String username, JwtConfiguration config) {
     return Jwts.builder()
       .setExpiration(new Date(System.currentTimeMillis() + config.getExpiryTimeInMs()))
       .setIssuedAt(new Date(System.currentTimeMillis()))
       .setIssuer(config.getIssuer())
-      .claim(GbifClaims.USERNAME, user.getUserName())
+      .claim(GbifClaims.USERNAME, username)
       .signWith(SignatureAlgorithm.HS256, config.getSigningKey())
       .compact();
   }
@@ -30,8 +31,8 @@ public class JwtUtils {
   public static Optional<String> findTokenInRequest(ContainerRequest containerRequest, JwtConfiguration config) {
     // TODO: ask morten if the token will come always in the header or in the cookie too?
     // check header first
-    return Optional.ofNullable(containerRequest.getHeaderValue(ContainerRequest.AUTHORIZATION))
-      .filter(header -> BEARER_PATTERN.matcher(header).find())
+    return Optional.ofNullable(containerRequest.getHeaderValue(HttpHeaders.AUTHORIZATION))
+      .filter(JwtUtils::containsBearer)
       .map(s -> Optional.of(removeBearer(s)))
       // if not found check cookie
       .orElseGet(() -> Optional.ofNullable(containerRequest.getCookies().get(config.getCookieName()))
@@ -45,8 +46,14 @@ public class JwtUtils {
    *
    * @return a token without whitespaces and the word 'bearer'
    */
-  private static String removeBearer(String token) {
+  @VisibleForTesting
+  public static String removeBearer(String token) {
     return BEARER_PATTERN.matcher(token).replaceAll("").trim();
+  }
+
+  @VisibleForTesting
+  public static boolean containsBearer(String header) {
+    return BEARER_PATTERN.matcher(header).find();
   }
 
 }
