2319c48349ee905e5a21350cceecc4606cdf3e40
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-06-17 16:28:23 +0200

    Identifier resolver for grscicoll

---
+++ registry-ws/src/main/java/org/gbif/registry/ws/util/GrscicollUtils.java
@@ -0,0 +1,12 @@
+package org.gbif.registry.ws.util;
+
+/**
+ * Utilities class to use in grsicoll classes.
+ */
+public class GrscicollUtils {
+
+  public static final String GRSCICOLL_PATH = "grscicoll";
+
+  private GrscicollUtils() {}
+
+}
