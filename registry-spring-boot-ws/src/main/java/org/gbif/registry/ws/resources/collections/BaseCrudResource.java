dd97547ef0faea62311c1666cfed39b5be7d184a
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-08-21 15:19:33 +0200

    BaseCrudResource and PersonResource

--- registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/resources/collections/BaseCrudResource.java
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/resources/collections/BaseCrudResource.java
@@ -3,12 +3,29 @@ package org.gbif.registry.ws.resources.collections;
 import com.google.common.eventbus.EventBus;
 import org.gbif.api.model.collections.CollectionEntity;
 import org.gbif.api.service.collections.CrudService;
+import org.gbif.registry.events.collections.DeleteCollectionEntityEvent;
 import org.gbif.registry.persistence.mapper.collections.CrudMapper;
+import org.gbif.registry.ws.Trim;
+import org.gbif.ws.server.interceptor.NullToNotFound;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.gbif.registry.ws.security.UserRoles.ADMIN_ROLE;
+import static org.gbif.registry.ws.security.UserRoles.GRSCICOLL_ADMIN_ROLE;
 
 /**
  * Base class to implement the CRUD methods of a {@link CollectionEntity}.
@@ -25,18 +42,58 @@ public abstract class BaseCrudResource<T extends CollectionEntity> implements Cr
     this.objectClass = objectClass;
   }
 
-  // TODO: 2019-07-24 implement all methods
-  // TODO: 2019-07-24 implement @Validate
-  // TODO: 2019-07-24 implement @Trim
-  // TODO: 2019-07-24 Authentication can't inject because it's not a rest controller
-//  @RequestMapping(method = RequestMethod.POST)
-//  @Trim
+  // TODO: 2019-08-21 implement validation
+
+  @RequestMapping(method = RequestMethod.POST)
+  @Trim
 //  @Validate
   @Transactional
-//  @Secured({ADMIN_ROLE, GRSCICOLL_ADMIN_ROLE})
+  @Secured({ADMIN_ROLE, GRSCICOLL_ADMIN_ROLE})
   public UUID create(@NotNull T entity, Authentication authentication) {
-//    entity.setCreatedBy(authentication.getName());
-//    entity.setModifiedBy(authentication.getName());
+    entity.setCreatedBy(((UserDetails) authentication.getPrincipal()).getUsername());
+    entity.setModifiedBy(((UserDetails) authentication.getPrincipal()).getUsername());
     return create(entity);
+  }
+
+  @DeleteMapping("{key}")
+//  @Validate
+  @Transactional
+  @Secured({ADMIN_ROLE, GRSCICOLL_ADMIN_ROLE})
+  public void delete(@PathVariable("key") @NotNull UUID key, Authentication authentication) {
+    T entityToDelete = get(key);
+    entityToDelete.setModifiedBy(((UserDetails) authentication.getPrincipal()).getUsername());
+    update(entityToDelete);
+
+    delete(key);
+  }
+
+  @Transactional
+//  @Validate
+  @Override
+  public void delete(@NotNull UUID key) {
+    T objectToDelete = get(key);
+    crudMapper.delete(key);
+    eventBus.post(DeleteCollectionEntityEvent.newInstance(objectToDelete, objectClass));
+  }
+
+  @GetMapping("{key}")
+  @Nullable
+  @NullToNotFound
+//  @Validate(validateReturnedValue = true)
+  @Override
+  public T get(@PathVariable("key") @NotNull UUID key) {
+    return crudMapper.get(key);
+  }
+
+  @PutMapping("{key}")
+//  @Validate
+  @Transactional
+  @Secured({ADMIN_ROLE, GRSCICOLL_ADMIN_ROLE})
+  public void update(@PathVariable("key") @NotNull UUID key, @NotNull @Trim T entity) {
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    checkArgument(
+        key.equals(entity.getKey()), "Provided entity must have the same key as the resource URL");
+    entity.setModifiedBy(((UserDetails) authentication.getPrincipal()).getUsername());
+    update(entity);
   }
 }
--- registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/resources/collections/PersonResource.java
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/resources/collections/PersonResource.java
@@ -1,23 +1,33 @@
 package org.gbif.registry.ws.resources.collections;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Strings;
+import com.google.common.eventbus.EventBus;
 import org.gbif.api.model.collections.Person;
 import org.gbif.api.model.common.paging.Pageable;
+import org.gbif.api.model.common.paging.PagingRequest;
 import org.gbif.api.model.common.paging.PagingResponse;
 import org.gbif.api.model.registry.search.collections.PersonSuggestResult;
 import org.gbif.api.service.collections.PersonService;
+import org.gbif.registry.events.collections.CreateCollectionEntityEvent;
+import org.gbif.registry.events.collections.UpdateCollectionEntityEvent;
 import org.gbif.registry.persistence.mapper.collections.AddressMapper;
 import org.gbif.registry.persistence.mapper.collections.PersonMapper;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Nullable;
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.core.Context;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,30 +35,30 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static org.gbif.registry.ws.security.UserRoles.ADMIN_ROLE;
 import static org.gbif.registry.ws.security.UserRoles.GRSCICOLL_ADMIN_ROLE;
 
-// TODO: 2019-07-24 do smth with BasicCrudResource
 @RestController
 @RequestMapping("/grscicoll/person")
-public class PersonResource implements PersonService {
+public class PersonResource extends BaseCrudResource<Person> implements PersonService {
 
   private final PersonMapper personMapper;
   private final AddressMapper addressMapper;
+  private final EventBus eventBus;
 
-  public PersonResource(PersonMapper personMapper, AddressMapper addressMapper) {
-    // TODO: 2019-07-24 add EventBus
+  public PersonResource(PersonMapper personMapper, AddressMapper addressMapper, EventBus eventBus) {
+    super(personMapper, eventBus, Person.class);
     this.personMapper = personMapper;
     this.addressMapper = addressMapper;
+    this.eventBus = eventBus;
   }
 
-  // TODO: 2019-07-24 implement Validate
+  // TODO: 2019-08-21 implement validation
+  // TODO: 2019-08-21 check Context annotation
+
   @Override
   @RequestMapping(method = RequestMethod.POST)
   @Transactional
 //  @Validate(groups = {PrePersist.class, Default.class})
   @Secured({ADMIN_ROLE, GRSCICOLL_ADMIN_ROLE})
-  public UUID create(
-      // TODO: 2019-07-25 implement validation
-//      @Valid @NotNull
-      @RequestBody Person person) {
+  public UUID create(@Valid @NotNull @RequestBody Person person) {
     checkArgument(person.getKey() == null, "Unable to create an entity which already has a key");
 
     final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -63,39 +73,72 @@ public class PersonResource implements PersonService {
     person.setKey(UUID.randomUUID());
     personMapper.create(person);
 
-    // TODO: 2019-07-24 implement EventBus stuff
-//    eventBus.post(CreateCollectionEntityEvent.newInstance(person, Person.class));
+    eventBus.post(CreateCollectionEntityEvent.newInstance(person, Person.class));
     return person.getKey();
   }
 
-  // TODO: 2019-07-24 implement these methods
+  @Transactional
+//  @Validate
   @Override
-  public void delete(@NotNull UUID uuid) {
-    throw new UnsupportedOperationException("not implemented yet!");
+  public void update(@Valid @NotNull Person person) {
+    Person oldPerson = get(person.getKey());
+    checkArgument(oldPerson != null, "Entity doesn't exist");
+
+    if (oldPerson.getDeleted() != null) {
+      // if it's deleted we only allow to update it if we undelete it
+      checkArgument(person.getDeleted() == null,
+          "Unable to update a previously deleted entity unless you clear the deletion timestamp");
+    } else {
+      // not allowed to delete when updating
+      checkArgument(person.getDeleted() == null, "Can't delete an entity when updating");
+    }
+
+    // update mailing address
+    if (person.getMailingAddress() != null) {
+      if (oldPerson.getMailingAddress() == null) {
+        checkArgument(person.getMailingAddress().getKey() == null,
+            "Unable to create an address which already has a key");
+        addressMapper.create(person.getMailingAddress());
+      } else {
+        addressMapper.update(person.getMailingAddress());
+      }
+    }
+
+    // update entity
+    personMapper.update(person);
+
+    // check if we have to delete the mailing address
+    if (person.getMailingAddress() == null && oldPerson.getMailingAddress() != null) {
+      addressMapper.delete(oldPerson.getMailingAddress().getKey());
+    }
+
+    // check if we have to delete the address
+    Person newPerson = get(person.getKey());
+    eventBus.post(UpdateCollectionEntityEvent.newInstance(newPerson, oldPerson, Person.class));
   }
 
+  @RequestMapping(method = RequestMethod.GET)
   @Override
-  public Person get(@NotNull UUID uuid) {
-    throw new UnsupportedOperationException("not implemented yet!");
+  public PagingResponse<Person> list(@Nullable @RequestParam("q") String query,
+                                     @Nullable @RequestParam("primaryInstitution") UUID institutionKey,
+                                     @Nullable @RequestParam("primaryCollection") UUID collectionKey,
+                                     @Nullable @Context Pageable page) {
+    page = page == null ? new PagingRequest() : page;
+    query = query != null ? Strings.emptyToNull(CharMatcher.WHITESPACE.trimFrom(query)) : query;
+    long total = personMapper.count(institutionKey, collectionKey, query);
+    return new PagingResponse<>(page, total, personMapper.list(institutionKey, collectionKey, query, page));
   }
 
+  @GetMapping("deleted")
   @Override
-  public void update(@NotNull Person person) {
-    throw new UnsupportedOperationException("not implemented yet!");
+  public PagingResponse<Person> listDeleted(@Nullable @Context Pageable page) {
+    page = page == null ? new PagingRequest() : page;
+    return new PagingResponse<>(page, personMapper.countDeleted(), personMapper.deleted(page));
   }
 
+  @GetMapping("suggest")
   @Override
-  public PagingResponse<Person> list(@Nullable String s, @Nullable UUID uuid, @Nullable UUID uuid1, @Nullable Pageable pageable) {
-    throw new UnsupportedOperationException("not implemented yet!");
-  }
-
-  @Override
-  public PagingResponse<Person> listDeleted(@Nullable Pageable pageable) {
-    throw new UnsupportedOperationException("not implemented yet!");
-  }
-
-  @Override
-  public List<PersonSuggestResult> suggest(@Nullable String s) {
-    throw new UnsupportedOperationException("not implemented yet!");
+  public List<PersonSuggestResult> suggest(@Nullable @RequestParam("q") String q) {
+    return personMapper.suggest(q);
   }
 }
