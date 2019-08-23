297e74ae0a464598b2602ee995d6ff1be45d0b61
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-08-23 09:25:46 +0200

    DatasetMapper, InstallationMapper

---
+++ registry-spring-boot-persistence/src/main/java/org/gbif/registry/persistence/mapper/InstallationMapper.java
@@ -0,0 +1,37 @@
+package org.gbif.registry.persistence.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.gbif.api.model.common.paging.Pageable;
+import org.gbif.api.model.registry.Installation;
+import org.gbif.api.model.registry.search.KeyTitleResult;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface InstallationMapper extends BaseNetworkEntityMapper<Installation> {
+
+  long countInstallationsEndorsedBy(@Param("nodeKey") UUID nodeKey);
+
+  List<Installation> listInstallationsEndorsedBy(@Param("nodeKey") UUID nodeKey, @Nullable @Param("page") Pageable page);
+
+  long countInstallationsByOrganization(@Param("organizationKey") UUID organizationKey);
+
+  List<Installation> listInstallationsByOrganization(@Param("organizationKey") UUID organizationKey,
+                                                     @Nullable @Param("page") Pageable page);
+
+  List<Installation> deleted(@Nullable @Param("page") Pageable page);
+
+  long countDeleted();
+
+  long countNonPublishing();
+
+  List<Installation> nonPublishing(@Nullable @Param("page") Pageable page);
+
+  /**
+   * A simple suggest by title service.
+   */
+  List<KeyTitleResult> suggest(@Nullable @Param("q") String q);
+}
