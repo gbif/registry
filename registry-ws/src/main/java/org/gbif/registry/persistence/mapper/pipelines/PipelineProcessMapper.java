cca3d7f592528c61c75549d3e99f19d3451d1036
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-08-30 16:49:16 +0200

    pipelines history tracking service migrated to the registry

---
+++ registry-ws/src/main/java/org/gbif/registry/persistence/mapper/pipelines/PipelineProcessMapper.java
@@ -0,0 +1,67 @@
+package org.gbif.registry.persistence.mapper.pipelines;
+
+import org.gbif.api.model.common.paging.Pageable;
+import org.gbif.api.model.crawler.pipelines.PipelineProcess;
+import org.gbif.api.model.crawler.pipelines.PipelineStep;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import javax.annotation.Nullable;
+
+import org.apache.ibatis.annotations.Param;
+
+/** Mapper for {@link PipelineProcess} entities. */
+public interface PipelineProcessMapper {
+
+  /**
+   * Inserts a new {@link PipelineProcess}.
+   *
+   * <p>The id generated is set to the {@link PipelineProcess} received as parameter.
+   *
+   * @param process to insert
+   */
+  void create(PipelineProcess process);
+
+  /**
+   * Retrieves a {@link PipelineProcess} by dataset key and attempt.
+   *
+   * @param datasetKey
+   * @param attempt
+   * @return {@link PipelineProcess}
+   */
+  PipelineProcess get(@Param("datasetKey") UUID datasetKey, @Param("attempt") int attempt);
+
+  Optional<Integer> getLastAttempt(@Param("datasetKey") UUID datasetKey);
+
+  /**
+   * Adds a {@link PipelineStep} to an existing {@link PipelineProcess}.
+   *
+   * @param pipelinesProcessKey key of the process where we want to add the step
+   * @param step step to add
+   */
+  void addPipelineStep(@Param("pipelinesProcessKey") long pipelinesProcessKey, @Param("step") PipelineStep step);
+
+  /**
+   * Lists {@link PipelineProcess} based in the search parameters.
+   *
+   * <p>It supports paging.
+   *
+   * @param datasetKey dataset key
+   * @param attempt attempt
+   * @param page page to specify the offset and the limit
+   * @return list of {@link PipelineProcess}
+   */
+  List<PipelineProcess> list(
+    @Nullable @Param("datasetKey") UUID datasetKey,
+    @Nullable @Param("attempt") Integer attempt,
+    @Nullable @Param("page") Pageable page
+  );
+
+  /** Counts the number of {@link PipelineProcess} based in the search parameters. */
+  long count(@Nullable @Param("datasetKey") UUID datasetKey, @Nullable @Param("attempt") Integer attempt);
+
+  PipelineStep getPipelineStep(@Param("key") long key);
+
+  void updatePipelineStep(@Param("step") PipelineStep step);
+}
---
+++ registry-ws/src/main/java/org/gbif/registry/pipelines/PipelinesCoordinatorTrackingServiceImpl.java
@@ -0,0 +1,345 @@
+package org.gbif.registry.pipelines;
+
+import org.gbif.api.model.common.paging.Pageable;
+import org.gbif.api.model.common.paging.PagingRequest;
+import org.gbif.api.model.common.paging.PagingResponse;
+import org.gbif.api.model.crawler.pipelines.*;
+import org.gbif.api.model.registry.Dataset;
+import org.gbif.api.service.registry.DatasetService;
+import org.gbif.api.vocabulary.DatasetType;
+import org.gbif.common.messaging.api.MessagePublisher;
+import org.gbif.common.messaging.api.messages.*;
+import org.gbif.registry.persistence.mapper.pipelines.PipelineProcessMapper;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
+import com.google.inject.Inject;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Service that allows to re-run pipeline steps on an specific attempt.
+ */
+public class PipelinesCoordinatorTrackingServiceImpl implements PipelinesHistoryTrackingService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PipelinesCoordinatorTrackingServiceImpl.class);
+
+  //Used to iterate over all datasets
+  private static final int PAGE_SIZE = 200;
+
+  //Used to read serialized messages stored in the data base as strings.
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+  static {
+    OBJECT_MAPPER.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+    // determines whether encountering of unknown properties (ones that do not map to a property, and there is no
+    // "any setter" or handler that can handle it) should result in a failure (throwing a JsonMappingException) or not.
+    OBJECT_MAPPER.disable(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+    // Enforce use of ISO-8601 format dates (http://wiki.fasterxml.com/JacksonFAQDateHandling)
+    OBJECT_MAPPER.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
+  }
+
+  /**
+   * The messagePublisher can be optional, and optional is not supported in constructor injection.
+   */
+  @Inject(optional = true)
+  private final MessagePublisher publisher = null;
+
+  //MyBatis mapper
+  private final PipelineProcessMapper mapper;
+
+  private final DatasetService datasetService;
+
+
+  @Inject
+  public PipelinesCoordinatorTrackingServiceImpl(PipelineProcessMapper mapper, DatasetService datasetService) {
+    this.mapper = mapper;
+    this.datasetService = datasetService;
+  }
+
+  @Override
+  public RunPipelineResponse runLastAttempt(UUID datasetKey, Set<StepType> steps, String reason, String user) {
+    Integer lastAttempt = mapper.getLastAttempt(datasetKey).orElse(null);
+    return runPipelineAttempt(datasetKey, lastAttempt, steps, reason, user);
+  }
+
+  /**
+   * Utility method to run batch jobs on all dataset elements
+   */
+  private RunPipelineResponse doOnAllDatasets(Consumer<Dataset> onDataset) {
+    PagingRequest pagingRequest = new PagingRequest(0, PAGE_SIZE);
+    PagingResponse<Dataset> response = datasetService.listByType(DatasetType.OCCURRENCE, pagingRequest);
+    try {
+      do {
+        response.getResults().forEach(onDataset);
+        pagingRequest.setOffset(response.getResults().size());
+        response = datasetService.list(pagingRequest);
+      } while (response.isEndOfRecords());
+    } catch (Exception ex) {
+      LOG.error("Error processing all datasets", ex);
+      return RunPipelineResponse.builder().setResponseStatus(RunPipelineResponse.ResponseStatus.ERROR).build();
+    }
+    return RunPipelineResponse.builder().setResponseStatus(RunPipelineResponse.ResponseStatus.OK).build();
+  }
+
+  @Override
+  public RunPipelineResponse runLastAttempt(Set<StepType> steps, String reason, String user) {
+    return RunPipelineResponse.builder(doOnAllDatasets(dataset -> runLastAttempt(dataset.getKey(), steps, reason, user)))
+                              .setStep(steps).build();
+  }
+
+  @Override
+  public RunPipelineResponse crawlAll() {
+    return doOnAllDatasets(dataset -> {
+                                         try {
+                                           publisher.send(new StartCrawlMessage(dataset.getKey()));
+                                         } catch (IOException ex) {
+                                           LOG.error("Error crawling all datasets", ex);
+                                           throw new RuntimeException(ex);
+                                         }
+                                       });
+  }
+
+
+  /**
+   * Search the last step executed of a specific StepType.
+   * @param pipelineProcess container of steps
+   * @param step to be searched
+   * @return optionally, the las step found
+   */
+  private Optional<PipelineStep> getLatestSuccessfulStep(PipelineProcess pipelineProcess, StepType step) {
+    return  pipelineProcess.getSteps().stream()
+              .filter(s -> step.equals(s.getType()))
+              .max(Comparator.comparing(PipelineStep::getStarted));
+  }
+
+  /**
+   * Calculates the general state of a {@link PipelineProcess}.
+   * If one the latest steps of a specific {@link StepType} has a {@link PipelineStep.Status#FAILED}, the process is considered as FAILED.
+   * If all the latest steps of all {@link StepType} have the same {@link PipelineStep.Status}, that status  used for the {@link PipelineProcess}.
+   * If it has step in {@link PipelineStep.Status#RUNNING} it is decided as the process status, otherwise is {@link PipelineStep.Status#COMPLETED}
+   *
+   * @param pipelineProcess that contains all the steps.
+   * @return the calculated status of a {@link PipelineProcess}
+   */
+  private PipelineStep.Status getStatus(PipelineProcess pipelineProcess) {
+
+    //Collects the latest steps per type.
+    Set<PipelineStep.Status> statuses = new HashSet<>();
+    for (StepType stepType : StepType.values()) {
+      pipelineProcess.getSteps()
+        .stream()
+        .filter(s -> stepType == s.getType())
+        .max(Comparator.comparing(PipelineStep::getStarted))
+        .ifPresent(step -> statuses.add(step.getState()));
+    }
+
+    //Only has one states, it could means that all steps have the same status
+    if (statuses.size() == 1 ) {
+      return statuses.iterator().next();
+    } else {
+      //Checks the states by priority
+      if (statuses.contains(PipelineStep.Status.FAILED)) {
+        return PipelineStep.Status.FAILED;
+      } else if (statuses.contains(PipelineStep.Status.RUNNING)) {
+        return PipelineStep.Status.RUNNING;
+      } else {
+        return PipelineStep.Status.COMPLETED;
+      }
+    }
+  }
+
+  @Override
+  public PagingResponse<PipelineProcess> history(Pageable pageable) {
+    long count = mapper.count(null, null);
+    List<PipelineProcess> statuses = mapper.list(null, null, pageable);
+    return new PagingResponse<>(pageable, count, statuses);
+  }
+
+  @Override
+  public PagingResponse<PipelineProcess> history(UUID datasetKey, Pageable pageable) {
+    Objects.requireNonNull(datasetKey, "DatasetKey can't be null");
+
+    long count = mapper.count(datasetKey, null);
+    List<PipelineProcess> statuses = mapper.list(datasetKey, null, pageable);
+    return new PagingResponse<>(pageable, count, statuses);
+  }
+
+  @Override
+  public RunPipelineResponse runPipelineAttempt(UUID datasetKey, Integer attempt, Set<StepType> steps, String reason,
+                                                String user) {
+    Objects.requireNonNull(datasetKey, "DatasetKey can't be null");
+    Objects.requireNonNull(attempt, "Attempt can't be null");
+    Objects.requireNonNull(steps, "Steps can't be null");
+    Objects.requireNonNull(reason, "Reason can't be null");
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(user), "user can't be null");
+
+    PipelineProcess status = mapper.get(datasetKey, attempt);
+
+    //Checks that the pipelines is not in RUNNING state
+    if(getStatus(status) == PipelineStep.Status.RUNNING) {
+      return new RunPipelineResponse.Builder()
+              .setResponseStatus(RunPipelineResponse.ResponseStatus.PIPELINE_IN_SUBMITTED)
+              .setStep(steps)
+             .build();
+    }
+
+    //Performs the messaging and updates the status onces the message has been sent
+    RunPipelineResponse.Builder responseBuilder = RunPipelineResponse.builder().setStep(steps);
+    steps.forEach(stepName ->
+        getLatestSuccessfulStep(status, stepName).ifPresent(step -> {
+          try {
+            if (stepName == StepType.HIVE_VIEW || stepName == StepType.INTERPRETED_TO_INDEX) {
+              responseBuilder.setResponseStatus(RunPipelineResponse.ResponseStatus.OK);
+              publisher.send(OBJECT_MAPPER.readValue(step.getMessage(), PipelinesInterpretedMessage.class));
+            } else if (steps.contains(StepType.VERBATIM_TO_INTERPRETED)) {
+              responseBuilder.setResponseStatus(RunPipelineResponse.ResponseStatus.OK);
+              publisher.send(OBJECT_MAPPER.readValue(step.getMessage(), PipelinesVerbatimMessage.class));
+            } else if (steps.contains(StepType.DWCA_TO_VERBATIM)) {
+              responseBuilder.setResponseStatus(RunPipelineResponse.ResponseStatus.OK);
+              publisher.send(OBJECT_MAPPER.readValue(step.getMessage(), PipelinesDwcaMessage.class));
+            } else if (steps.contains(StepType.ABCD_TO_VERBATIM)) {
+              responseBuilder.setResponseStatus(RunPipelineResponse.ResponseStatus.OK);
+              publisher.send(OBJECT_MAPPER.readValue(step.getMessage(), PipelinesAbcdMessage.class));
+            } else if (steps.contains(StepType.XML_TO_VERBATIM)) {
+              responseBuilder.setResponseStatus(RunPipelineResponse.ResponseStatus.OK);
+              publisher.send(OBJECT_MAPPER.readValue(step.getMessage(), PipelinesXmlMessage.class));
+            } else {
+              responseBuilder.setResponseStatus(RunPipelineResponse.ResponseStatus.UNSUPPORTED_STEP);
+            }
+
+            // update rerun reason and modifier user
+            mapper.updatePipelineStep(step.setRerunReason(reason).setModifiedBy(user));
+          } catch (IOException ex) {
+            LOG.error("Error reading message", ex);
+            throw Throwables.propagate(ex);
+          }
+       })
+    );
+
+    return  responseBuilder.build();
+  }
+
+  @Override
+  public PipelineProcess get(UUID datasetKey, Integer attempt) {
+    Objects.requireNonNull(datasetKey, "DatasetKey can't be null");
+    Objects.requireNonNull(attempt, "Attempt can't be null");
+
+    return mapper.get(datasetKey, attempt);
+  }
+
+  @Override
+  public PipelineProcess create(UUID datasetKey, Integer attempt, String creator) {
+    Objects.requireNonNull(datasetKey, "DatasetKey can't be null");
+    Objects.requireNonNull(attempt, "Attempt can't be null");
+    Objects.requireNonNull(creator, "Creator can't be null");
+
+    PipelineProcess pipelineProcess = new PipelineProcess();
+    LocalDateTime now = LocalDateTime.now();
+    pipelineProcess.setAttempt(attempt);
+    pipelineProcess.setCreated(now);
+    pipelineProcess.setCreatedBy(creator);
+    mapper.create(pipelineProcess);
+    return pipelineProcess;
+  }
+
+  @Override
+  public PipelineStep addPipelineStep(Long pipelineProcessKey, PipelineStep pipelineStep, String user) {
+    Objects.requireNonNull(pipelineProcessKey, "PipelineProcessKey can't be null");
+    Objects.requireNonNull(pipelineStep, "PipelineStep can't be null");
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(user), "user can't be null");
+
+    pipelineStep.setModifiedBy(user);
+    mapper.addPipelineStep(pipelineProcessKey, pipelineStep);
+    return pipelineStep;
+  }
+
+  @Override
+  public void updatePipelineStepStatus(Long pipelineStepKey, PipelineStep.Status status, String user) {
+    Objects.requireNonNull(pipelineStepKey, "PipelineStepKey can't be null");
+    Objects.requireNonNull(status, "Status can't be null");
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(user), "user can't be null");
+
+    PipelineStep step = mapper.getPipelineStep(pipelineStepKey);
+    if (step.getState() != status && (PipelineStep.Status.FAILED == status || PipelineStep.Status.COMPLETED == status)) {
+      step.setFinished(LocalDateTime.now());
+    }
+
+    step.setState(status);
+    step.setModifiedBy(user);
+    mapper.updatePipelineStep(step);
+  }
+
+  @Override
+  public PipelineWorkflow getPipelineWorkflow(UUID datasetKey, Integer attempt) {
+    Objects.requireNonNull(datasetKey, "datasetKey can't be null");
+    Objects.requireNonNull(attempt, "attempt can't be null");
+
+    PipelineProcess process = mapper.get(datasetKey, attempt);
+
+    // group the steps by its execution order in the workflow and then by name. This will create something
+    // like:
+    // 1 -> DWCA_TO_AVRO -> List<PipelineStep>
+    // 2 -> VERBATIM_TO_INTERPRETED -> List<PipelineStep>
+    // 3 -> INTERPRETED_TO_INDEX -> List<PipelineStep>
+    //   -> HIVE_VIEW -> List<PipelineStep>
+    //
+    // The map is sorted by the step execution order in descending order
+    Map<Integer, Map<StepType, List<PipelineStep>>> stepsByOrderAndName =
+        process.getSteps().stream()
+            .collect(
+                Collectors.groupingBy(
+                    s -> s.getType().getExecutionOrder(),
+                    () ->
+                        new TreeMap<Integer, Map<StepType, List<PipelineStep>>>(
+                            Comparator.reverseOrder()),
+                    Collectors.groupingBy(PipelineStep::getType)));
+
+    // iterate from steps in the last position to the ones in the first one so that we can create
+    // the worfklow hierarchy
+    List<WorkflowStep> stepsPreviousIteration = null;
+    for (Map.Entry<Integer, Map<StepType, List<PipelineStep>>> stepsByOrder :
+        stepsByOrderAndName.entrySet()) {
+
+      List<WorkflowStep> currentSteps = new ArrayList<>();
+      for (Map.Entry<StepType, List<PipelineStep>> stepsByType :
+          stepsByOrder.getValue().entrySet()) {
+
+        // create workflow step
+        WorkflowStep step = new WorkflowStep();
+        step.setStepType(stepsByType.getKey());
+        step.getAllSteps().addAll(stepsByType.getValue());
+        step.setLastStep(step.getAllSteps().iterator().next());
+        // link this step to its next steps
+        step.setNextSteps(stepsPreviousIteration);
+
+        // accumulate all the steps of this StepType
+        currentSteps.add(step);
+      }
+
+      // the steps of this iteration now become the steps of the previous iteration
+      stepsPreviousIteration = currentSteps;
+    }
+
+    // create workflow
+    PipelineWorkflow workflow = new PipelineWorkflow();
+    workflow.setDatasetKey(process.getDatasetKey());
+    workflow.setAttempt(process.getAttempt());
+    // the last steps created will be the started steps of the workflow
+    workflow.setSteps(stepsPreviousIteration);
+
+    return workflow;
+  }
+}
