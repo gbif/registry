f477a0ef17b05e3c7d8b870474c3ffcc61e229de
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-09-05 16:37:11 +0200

    Pageable and SearchRequest providers

---
+++ spring-boot-common-ws/src/main/java/org/gbif/ws/server/provider/PageableProvider.java
@@ -0,0 +1,95 @@
+package org.gbif.ws.server.provider;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.gbif.api.model.common.paging.Pageable;
+import org.gbif.api.model.common.paging.PagingRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+import static org.gbif.api.model.common.paging.PagingConstants.DEFAULT_PARAM_LIMIT;
+import static org.gbif.api.model.common.paging.PagingConstants.DEFAULT_PARAM_OFFSET;
+import static org.gbif.api.model.common.paging.PagingConstants.PARAM_LIMIT;
+import static org.gbif.api.model.common.paging.PagingConstants.PARAM_OFFSET;
+
+/**
+ * Jersey provider class that extracts the page size and offset from the query parameters, or provides the default
+ * implementation if necessary.
+ * <p/>
+ * Example resource use:
+ * <pre>
+ * {@code
+ * public List<Checklist> list(@QueryParam("page") Pageable pageable) {
+ *   // do stuff
+ * }
+ * }
+ * </pre>
+ * <p/>
+ * Note, this implementation is based on the documentation provided on:
+ * http://stackoverflow.com/questions/5722506/how-do-you-map-multiple-query-parameters-to-the-fields-of-a-bean-on-jersey-get-re
+ */
+public class PageableProvider implements ContextProvider<Pageable> {
+
+  private static final Logger LOG = LoggerFactory.getLogger(PageableProvider.class);
+
+  @VisibleForTesting
+  static final int LIMIT_CAP = 1000;
+
+  @Override
+  public Pageable getValue(WebRequest webRequest) {
+    return getPagingRequest(webRequest);
+  }
+
+  // TODO: 05/09/2019 refactor PageableHandlerMethodArgumentResolver
+  public static PagingRequest getPagingRequest(WebRequest webRequest) {
+    Map<String, String[]> params = webRequest.getParameterMap();
+
+    int limit = DEFAULT_PARAM_LIMIT;
+    if (getFirst(params, PARAM_LIMIT) != null) {
+      try {
+        limit = Integer.parseInt(getFirst(params, PARAM_LIMIT));
+        if (limit < 0) {
+          LOG.info("Limit parameter was no positive integer [{}]. Using default {}",
+              getFirst(params, PARAM_LIMIT), DEFAULT_PARAM_LIMIT);
+          limit = DEFAULT_PARAM_LIMIT;
+        } else if (limit > LIMIT_CAP) {
+          LOG.info("Limit parameter too high. Use maximum {}", LIMIT_CAP);
+          limit = LIMIT_CAP;
+        }
+      } catch (NumberFormatException e) {
+        LOG.warn("Unparsable value supplied for limit [{}]. Using default {}", getFirst(params, PARAM_LIMIT),
+            DEFAULT_PARAM_LIMIT);
+      }
+    }
+
+    long offset = DEFAULT_PARAM_OFFSET;
+    if (getFirst(params, PARAM_OFFSET) != null) {
+      try {
+        offset = Long.parseLong(getFirst(params, PARAM_OFFSET));
+        if (offset < 0) {
+          LOG.warn("Offset parameter is a negative integer [{}]. Using default {}", getFirst(params, PARAM_OFFSET),
+              DEFAULT_PARAM_OFFSET);
+          offset = DEFAULT_PARAM_OFFSET;
+        }
+      } catch (NumberFormatException e) {
+        LOG.warn("Unparsable value supplied for offset [{}]. Using default {}", getFirst(params, PARAM_OFFSET),
+            DEFAULT_PARAM_OFFSET);
+      }
+    }
+    return new PagingRequest(offset, limit);
+  }
+
+  // TODO: 05/09/2019 should be util? 
+  private static String getFirst(Map<String, String[]> params, String param) {
+    final String[] values = params.get(param);
+    String resultValue = null;
+
+    if (values != null && values[0] != null) {
+      resultValue = values[0];
+    }
+
+    return resultValue;
+  }
+}
---
+++ spring-boot-common-ws/src/main/java/org/gbif/ws/server/provider/SearchRequestProvider.java
@@ -0,0 +1,145 @@
+package org.gbif.ws.server.provider;
+
+import com.google.common.base.Strings;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Lists;
+import org.gbif.api.model.common.search.SearchParameter;
+import org.gbif.api.model.common.search.SearchRequest;
+import org.gbif.api.util.SearchTypeValidator;
+import org.gbif.api.util.VocabularyUtils;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import static org.gbif.ws.util.WebserviceParameter.PARAM_HIGHLIGHT;
+import static org.gbif.ws.util.WebserviceParameter.PARAM_QUERY_STRING;
+import static org.gbif.ws.util.WebserviceParameter.PARAM_SPELLCHECK;
+import static org.gbif.ws.util.WebserviceParameter.PARAM_SPELLCHECK_COUNT;
+
+/**
+ * Provider class that transforms a set of HTTP parameters into a SearchRequest class instance.
+ * This assumes the existence of the following parameters in the HTTP request:
+ * 'page_size', 'offset', 'q' and any of the search parameter enum member names case insensitively.
+ */
+public class SearchRequestProvider<RT extends SearchRequest<P>, P extends Enum<?> & SearchParameter> implements ContextProvider<RT> {
+
+  private final Class<P> searchParameterClass;
+  private final Class<RT> requestType;
+  private static final int NON_SPELL_CHECK_COUNT = -1;
+
+  public SearchRequestProvider(Class<RT> requestType, Class<P> searchParameterClass) {
+    this.requestType = requestType;
+    this.searchParameterClass = searchParameterClass;
+  }
+
+  @Override
+  public RT getValue(WebRequest webRequest) {
+    try {
+      RT req = requestType.newInstance();
+      return getSearchRequest(webRequest, req);
+    } catch (InstantiationException | IllegalAccessException e) {
+      // should never happen
+      Throwables.propagate(e);
+    }
+    return null;
+  }
+
+  protected P findSearchParam(String name) {
+    try {
+      return VocabularyUtils.lookupEnum(name, searchParameterClass);
+    } catch (IllegalArgumentException e) {
+      // we have all params here, not only the enum ones, so this is ok to end up here a few times
+    }
+    return null;
+  }
+
+  protected RT getSearchRequest(WebRequest webRequest, RT searchRequest) {
+    searchRequest.copyPagingValues(PageableProvider.getPagingRequest(webRequest));
+
+    final Map<String, String[]> params = webRequest.getParameterMap();
+
+    getSearchRequestFromQueryParams(searchRequest, params);
+
+    return searchRequest;
+  }
+
+  /**
+   * Override this method for populating specific search/suggest requests
+   */
+  protected void getSearchRequestFromQueryParams(RT searchRequest, final Map<String, String[]> params) {
+    final String q = getFirst(params, PARAM_QUERY_STRING);
+    final String highlightValue = getFirst(params, PARAM_HIGHLIGHT);
+    final String spellCheck = getFirst(params, PARAM_SPELLCHECK);
+    final String spellCheckCount = getFirst(params, PARAM_SPELLCHECK_COUNT);
+
+    if (!Strings.isNullOrEmpty(q)) {
+      searchRequest.setQ(q);
+    }
+
+    if (!Strings.isNullOrEmpty(highlightValue)) {
+      searchRequest.setHighlight(Boolean.parseBoolean(highlightValue));
+    }
+
+    if (!Strings.isNullOrEmpty(spellCheck)) {
+      searchRequest.setSpellCheck(Boolean.parseBoolean(spellCheck));
+    }
+
+    if (!Strings.isNullOrEmpty(spellCheckCount)) {
+      searchRequest.setSpellCheckCount(Integer.parseInt(spellCheckCount));
+    } else {
+      searchRequest.setSpellCheckCount(NON_SPELL_CHECK_COUNT);
+    }
+
+    // find search parameter enum based filters
+    setSearchParams(searchRequest, params);
+  }
+
+  /**
+   * Removes all empty and null parameters from the list.
+   * Each value is trimmed(String.trim()) in order to remove all sizes of empty parameters.
+   */
+  private static List<String> removeEmptyParameters(List<String> parameters) {
+    List<String> cleanParameters = Lists.newArrayListWithCapacity(parameters.size());
+    for (String param : parameters) {
+      String cleanParam = Strings.nullToEmpty(param).trim();
+      if (!cleanParam.isEmpty()) {
+        cleanParameters.add(cleanParam);
+      }
+    }
+    return cleanParameters;
+  }
+
+  private static String getFirst(Map<String, String[]> params, String param) {
+    final String[] values = params.get(param);
+    String resultValue = null;
+
+    if (values != null && values[0] != null) {
+      resultValue = values[0];
+    }
+
+    return resultValue;
+  }
+
+  /**
+   * Iterates over the params map and adds to the search request the recognized parameters (i.e.: those that have a
+   * correspondent value in the P generic parameter).
+   * Empty (of all size) and null parameters are discarded.
+   */
+  private void setSearchParams(RT searchRequest, Map<String, String[]> params) {
+    for (Entry<String, String[]> entry : params.entrySet()) {
+      P p = findSearchParam(entry.getKey());
+      if (p != null) {
+        final List<String> list = entry.getValue() != null ? Arrays.asList(entry.getValue()) : Collections.emptyList();
+        for (String val : removeEmptyParameters(list)) {
+          // validate value for certain types
+          SearchTypeValidator.validate(p, val);
+          searchRequest.addParameter(p, val);
+        }
+      }
+    }
+  }
+}
