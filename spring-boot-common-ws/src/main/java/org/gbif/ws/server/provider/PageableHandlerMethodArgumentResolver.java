d0924c587cbfa35ab0cb8c7ecf2af7c6b1d2e364
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-09-06 09:35:36 +0200

    Refactor Pageable method argument resolver

--- spring-boot-common-ws/src/main/java/org/gbif/ws/server/provider/PageableHandlerMethodArgumentResolver.java
+++ spring-boot-common-ws/src/main/java/org/gbif/ws/server/provider/PageableHandlerMethodArgumentResolver.java
@@ -1,21 +1,13 @@
 package org.gbif.ws.server.provider;
 
 import org.gbif.api.model.common.paging.Pageable;
-import org.gbif.api.model.common.paging.PagingRequest;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import java.util.Optional;
-
-import static org.gbif.api.model.common.paging.PagingConstants.DEFAULT_PARAM_LIMIT;
-import static org.gbif.api.model.common.paging.PagingConstants.DEFAULT_PARAM_OFFSET;
-import static org.gbif.api.model.common.paging.PagingConstants.PARAM_LIMIT;
-import static org.gbif.api.model.common.paging.PagingConstants.PARAM_OFFSET;
-
-public class PageableHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+public class PageableHandlerMethodArgumentResolver extends PageableProvider implements HandlerMethodArgumentResolver {
 
   @Override
   public boolean supportsParameter(MethodParameter parameter) {
@@ -24,13 +16,6 @@ public class PageableHandlerMethodArgumentResolver implements HandlerMethodArgum
 
   @Override
   public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-    final int limit = Optional.ofNullable(webRequest.getParameter(PARAM_LIMIT))
-        .map(Integer::parseInt)
-        .orElse(DEFAULT_PARAM_LIMIT);
-    final long offset = Optional.ofNullable(webRequest.getParameter(PARAM_OFFSET))
-        .map(Long::parseLong)
-        .orElse(DEFAULT_PARAM_OFFSET);
-
-    return new PagingRequest(offset, limit);
+    return getValue(webRequest);
   }
 }
