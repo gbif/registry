accae824dc35a30b1e7059995d643d2d81c3e25f
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-08 20:11:45 +0200

    Add an empty class to the integration-tests module

---
+++ registry-spring-boot-integration-tests/src/main/java/org/gbif/registry/it/RegistryIntegrationTestsModule.java
@@ -0,0 +1,6 @@
+package org.gbif.registry.it;
+
+// This is a temporal fix for the following problem:
+// JAR will be empty - no content was marked for inclusion!
+public class RegistryIntegrationTestsModule {
+}
