5c6501d3e98cc650b8687fce1d79dd893907680f
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-16 15:58:26 +0200

    Configure jacoco properly to calculate coverage from integration-tests module

--- registry-spring-boot-integration-tests/src/main/java/org/gbif/registry/it/RegistryIntegrationTestsModule.java
+++ registry-spring-boot-integration-tests/src/main/java/org/gbif/registry/it/RegistryIntegrationTestsModule.java
@@ -2,5 +2,5 @@ package org.gbif.registry.it;
 
 // This is a temporal fix for the following problem:
 // JAR will be empty - no content was marked for inclusion!
-public class RegistryIntegrationTestsModule {
+interface RegistryIntegrationTestsModule {
 }
