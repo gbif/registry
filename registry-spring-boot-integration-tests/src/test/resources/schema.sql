83f39e69b20215e72babb7c976e6fbc76657fb50
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-16 09:30:15 +0200

    Add cleanup scripts for integration-tests to schema.sql

--- registry-spring-boot-integration-tests/src/test/resources/schema.sql
+++ registry-spring-boot-integration-tests/src/test/resources/schema.sql
@@ -1,14 +1,59 @@
-DELETE
-FROM public.editor_rights
-WHERE 1 = 1;
+DELETE FROM contact;
+DELETE FROM endpoint;
+DELETE FROM tag;
+DELETE FROM identifier;
+DELETE FROM comment;
+DELETE FROM node_identifier;
+DELETE FROM node_machine_tag;
+DELETE FROM node_tag;
+DELETE FROM node_comment;
+DELETE FROM occurrence_download;
+DELETE FROM organization_contact;
+DELETE FROM organization_endpoint;
+DELETE FROM organization_machine_tag;
+DELETE FROM organization_tag;
+DELETE FROM organization_identifier;
+DELETE FROM organization_comment;
+DELETE FROM installation_contact;
+DELETE FROM installation_endpoint;
+DELETE FROM installation_machine_tag;
+DELETE FROM installation_tag;
+DELETE FROM installation_comment;
+DELETE FROM dataset_contact;
+DELETE FROM dataset_endpoint;
+DELETE FROM dataset_machine_tag;
+DELETE FROM dataset_tag;
+DELETE FROM dataset_identifier;
+DELETE FROM dataset_comment;
+DELETE FROM network_contact;
+DELETE FROM network_endpoint;
+DELETE FROM network_machine_tag;
+DELETE FROM network_tag;
+DELETE FROM network_comment;
+DELETE FROM machine_tag;
+DELETE FROM metadata;
+DELETE FROM editor_rights;
+DELETE FROM network;
+DELETE FROM dataset;
+DELETE FROM installation;
+DELETE FROM organization;
+DELETE FROM node;
+DELETE FROM public."user";
+DELETE FROM challenge_code;
+DELETE FROM collection_collection_person;
+DELETE FROM collection_identifier;
+DELETE FROM collection_tag;
+DELETE FROM institution_collection_person;
+DELETE FROM institution_identifier;
+DELETE FROM institution_tag;
+DELETE FROM collection_person;
+DELETE FROM collection;
+DELETE FROM institution;
+DELETE FROM address;
+TRUNCATE gbif_doi;
+DELETE FROM pipeline_step;
+DELETE FROM pipeline_process;
 
-DELETE
-FROM public."user"
-WHERE 1 = 1;
-
-DELETE
-FROM public.challenge_code
-WHERE 1 = 1;
 
 INSERT INTO public."user" (key, username, email, password, first_name, last_name, roles, settings, system_settings,
                            created, last_login, deleted, challenge_code_key)
