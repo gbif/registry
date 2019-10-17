a330a5d14b1740d6c1b00178d09d84ed23203ae6
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-17 15:41:58 +0200

    Migrate OrganizationIT to cucumber

---
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/ws/resources/organization/OrganizationCucumberIT.java
@@ -0,0 +1,18 @@
+package org.gbif.registry.ws.resources.organization;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.gbif.registry.ws.resources.SpringIT;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    strict = true,
+    features = {
+        "classpath:features/organization/organization_positive.feature"
+    },
+    glue = "org.gbif.registry.ws.resources.organization",
+    plugin = "pretty"
+)
+public class OrganizationCucumberIT extends SpringIT {
+}
--- registry-spring-boot-integration-tests/src/test/resources/scripts/organization/organization_prepare.sql
+++ registry-spring-boot-integration-tests/src/test/resources/scripts/organization/organization_prepare.sql
@@ -1,16 +1,9 @@
--- Node
-INSERT INTO public.node (key, gbif_region, continent, title, country, created_by, modified_by, created, modified,
-                         deleted, fulltext_search, type, participation_status)
-VALUES ('f698c938-d36a-41ac-8120-c35903e1acb9', 'EUROPE', 'EUROPE', 'The UK National Node', 'GB', 'WS TEST', 'WS TEST',
-        '2019-10-15 08:41:46.006372', '2019-10-15 08:41:46.006372', null,
-        '''countri'':5 ''europ'':7,8 ''gb'':9 ''nation'':3 ''node'':4 ''uk'':2 ''vote'':6', 'COUNTRY', 'VOTING');
-
 -- Prepare 2 organization for suggest testing
 INSERT INTO public.organization (key, endorsing_node_key, endorsement_approved, password, title, abbreviation,
                                  description, language, logo_url, city, province, country, postal_code, latitude,
                                  longitude, created_by, modified_by, created, modified, deleted, fulltext_search, email,
                                  phone, homepage, address, challenge_code_key)
-VALUES ('f433944a-ad93-4ea8-bad7-68de7348e65a', 'f698c938-d36a-41ac-8120-c35903e1acb9', false, 'password', 'ORG',
+VALUES ('f433944a-ad93-4ea8-bad7-68de7348e65a', 'f698c938-d36a-41ac-8120-c35903e1acb9', false, 'password', 'The ORG',
         'BGBM', 'The Berlin Botanical...', 'de', 'http://www.example.org', 'BERLIN', 'BERLIN', 'DE', '1408', null, null,
         'WS TEST', 'WS TEST', '2019-10-15 08:44:43.732781', '2019-10-15 08:44:43.732781', null,
         '''1408'':15 ''2920202'':8 ''a@b.com'':7 ''berlin'':4,11,12,13 ''bgbm'':2 ''botan'':5 ''de'':6,14 ''tim'':1 ''www.example.org'':9,10',
