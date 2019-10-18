d107a75a4b91e0cffd9a408dffc9657a48048bc9
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-18 14:25:59 +0200

    Create Organization and User ChallengeCodeManager

--- registry-spring-boot-surety/src/main/java/org/gbif/registry/surety/persistence/ChallengeCodeManager.java
+++ registry-spring-boot-surety/src/main/java/org/gbif/registry/surety/persistence/ChallengeCodeManager.java
@@ -1,64 +1,31 @@
 package org.gbif.registry.surety.persistence;
 
 import org.gbif.api.model.ChallengeCode;
-import org.gbif.registry.persistence.ChallengeCodeSupportMapper;
-import org.gbif.registry.persistence.mapper.surety.ChallengeCodeMapper;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 import java.util.UUID;
-
-// TODO: 2019-06-26 check all methods
 
 /**
  * Helper class to manage challengeCode references it.
  *
  * @param <K> type of the key used by the entity who is pointing to the challengeCode.
  */
-@Service
-public class ChallengeCodeManager<K> {
-
-  private final ChallengeCodeMapper challengeCodeMapper;
-  private final ChallengeCodeSupportMapper<K> challengeCodeSupportMapper;
-
-  public ChallengeCodeManager(
-      ChallengeCodeMapper challengeCodeMapper,
-      // TODO: 2019-08-23 check if it's really user mapper needed here
-      @Qualifier("userChallengeCodeSupportMapper") ChallengeCodeSupportMapper<K> challengeCodeSupportMapper) {
-    this.challengeCodeMapper = challengeCodeMapper;
-    this.challengeCodeSupportMapper = challengeCodeSupportMapper;
-  }
+public interface ChallengeCodeManager<K> {
 
   /**
    * Check if the provided challengeCode is valid for a specific entity key.
    */
-  public boolean isValidChallengeCode(K key, UUID challengeCode) {
-    if (key == null || challengeCode == null) {
-      return false;
-    }
-
-    Integer ccKey = challengeCodeSupportMapper.getChallengeCodeKey(key);
-    return ccKey != null && challengeCode.equals(challengeCodeMapper.getChallengeCode(ccKey));
-  }
+  boolean isValidChallengeCode(K key, UUID challengeCode);
 
   /**
    * Check if a given key is associated with a challengeCode.
    */
-  public boolean hasChallengeCode(K key) {
-    return Optional.ofNullable(challengeCodeSupportMapper.getChallengeCodeKey(key)).isPresent();
-  }
+  boolean hasChallengeCode(K key);
 
   /**
    * Creates a new challengeCode and updates the link between the entity and the challengeCode.
    * Should be called inside a @Transactional method
    */
-  public ChallengeCode create(K key) {
-    ChallengeCode challengeCode = ChallengeCode.newRandom();
-    challengeCodeMapper.createChallengeCode(challengeCode);
-    challengeCodeSupportMapper.updateChallengeCodeKey(key, challengeCode.getKey());
-    return challengeCode;
-  }
+  ChallengeCode create(K key);
 
   /**
    * Removes a challengeCode and removes the link between the entity and the challengeCode.
@@ -66,13 +33,5 @@ public class ChallengeCodeManager<K> {
    *
    * @return the challengeCode was removed successfully.
    */
-  public boolean remove(K key) {
-    return Optional.ofNullable(challengeCodeSupportMapper.getChallengeCodeKey(key))
-        .map(challengeCodeKey -> {
-          //remove the challengeCode from the referencing table first
-          challengeCodeSupportMapper.updateChallengeCodeKey(key, null);
-          challengeCodeMapper.deleteChallengeCode(challengeCodeKey);
-          return true;
-        }).orElse(Boolean.FALSE);
-  }
+  boolean remove(K key);
 }
