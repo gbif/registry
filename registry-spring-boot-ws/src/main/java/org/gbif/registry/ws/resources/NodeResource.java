81138953440fe77e09597bc5830c8dccf8c81ccc
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-25 23:09:11 +0200

    Refactor BaseNetworkEntityResource#addContact

--- registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/resources/NodeResource.java
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/resources/NodeResource.java
@@ -32,6 +32,7 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -210,6 +211,7 @@ public class NodeResource extends BaseNetworkEntityResource<Node> implements Nod
     throw new UnsupportedOperationException("Contacts are manually managed in the Directory");
   }
 
+  @PostMapping("{key}/contact")
   @Override
   public int addContact(@PathVariable("key") UUID targetEntityKey, @NotNull @Valid @Trim Contact contact) {
     throw new UnsupportedOperationException("Contacts are manually managed in the Directory");
