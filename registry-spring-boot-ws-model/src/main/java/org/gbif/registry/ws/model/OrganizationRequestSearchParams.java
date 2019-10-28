f6ef05b034a8fa30b83019f00617d4a85f525da1
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-28 11:22:06 +0100

    Move common params to RequestSearchParams

--- registry-spring-boot-ws-model/src/main/java/org/gbif/registry/ws/model/OrganizationRequestSearchParams.java
+++ registry-spring-boot-ws-model/src/main/java/org/gbif/registry/ws/model/OrganizationRequestSearchParams.java
@@ -1,18 +1,10 @@
 package org.gbif.registry.ws.model;
 
-import org.gbif.api.vocabulary.IdentifierType;
-
 import javax.annotation.Nullable;
 
-public class OrganizationRequestSearchParams {
+public class OrganizationRequestSearchParams extends RequestSearchParams {
 
   private Boolean isEndorsed;
-  private IdentifierType identifierType;
-  private String identifier;
-  private String machineTagNamespace; // namespace
-  private String machineTagName; // name
-  private String machineTagValue; // value
-  private String q; // query
 
   @Nullable
   public Boolean getIsEndorsed() {
@@ -21,59 +13,5 @@ public class OrganizationRequestSearchParams {
 
   public void setIsEndorsed(@Nullable Boolean isEndorsed) {
     this.isEndorsed = isEndorsed;
-  }
-
-  @Nullable
-  public IdentifierType getIdentifierType() {
-    return identifierType;
-  }
-
-  public void setIdentifierType(@Nullable IdentifierType identifierType) {
-    this.identifierType = identifierType;
-  }
-
-  @Nullable
-  public String getIdentifier() {
-    return identifier;
-  }
-
-  public void setIdentifier(@Nullable String identifier) {
-    this.identifier = identifier;
-  }
-
-  @Nullable
-  public String getMachineTagNamespace() {
-    return machineTagNamespace;
-  }
-
-  public void setMachineTagNamespace(@Nullable String machineTagNamespace) {
-    this.machineTagNamespace = machineTagNamespace;
-  }
-
-  @Nullable
-  public String getMachineTagName() {
-    return machineTagName;
-  }
-
-  public void setMachineTagName(@Nullable String machineTagName) {
-    this.machineTagName = machineTagName;
-  }
-
-  @Nullable
-  public String getMachineTagValue() {
-    return machineTagValue;
-  }
-
-  public void setMachineTagValue(@Nullable String machineTagValue) {
-    this.machineTagValue = machineTagValue;
-  }
-
-  @Nullable
-  public String getQ() {
-    return q;
-  }
-
-  public void setQ(@Nullable String q) {
-    this.q = q;
   }
 }
