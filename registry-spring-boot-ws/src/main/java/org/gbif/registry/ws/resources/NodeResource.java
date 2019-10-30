b667c74a2128077ed7702eea5a5a830c33d3764a
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-30 11:13:22 +0100

    Split back BaseNetworkEntityResource#addContact because of IptResource#register

--- registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/resources/NodeResource.java
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/resources/NodeResource.java
@@ -33,7 +33,6 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -205,7 +204,6 @@ public class NodeResource
     throw new UnsupportedOperationException("Contacts are manually managed in the Directory");
   }
 
-  @PostMapping("{key}/contact")
   @Override
   public int addContact(@PathVariable("key") UUID targetEntityKey, @NotNull @Valid @Trim Contact contact) {
     throw new UnsupportedOperationException("Contacts are manually managed in the Directory");
