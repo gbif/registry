6399ff6424f9c6a68fea14e16c0626207fd6fc47
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-31 11:03:09 +0100

    Security improvements, GbifAuthenticationManager

--- spring-boot-common-ws/src/main/java/org/gbif/ws/security/GbifAuthentication.java
+++ spring-boot-common-ws/src/main/java/org/gbif/ws/security/GbifAuthentication.java
@@ -4,8 +4,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Extends Spring's {@link UsernamePasswordAuthenticationToken}.
@@ -22,41 +22,32 @@ public class GbifAuthentication extends UsernamePasswordAuthenticationToken {
    */
   private String authenticationScheme;
 
-  /**
-   * Http request.
-   */
-  private HttpServletRequest request;
-
   // Take into account that a create object would have 'authenticate' false because of the constructor of the superclass.
   public GbifAuthentication(
-      UserDetails principal,
-      Object credentials,
-      String authenticationScheme,
-      HttpServletRequest request) {
+    UserDetails principal,
+    Object credentials,
+    String authenticationScheme) {
     super(principal, credentials);
     this.principal = principal;
     this.authenticationScheme = authenticationScheme;
-    this.request = request;
   }
 
   public GbifAuthentication(
-      UserDetails principal,
-      Object credentials,
-      Collection<? extends GrantedAuthority> authorities,
-      String authenticationScheme,
-      HttpServletRequest request) {
+    UserDetails principal,
+    Object credentials,
+    Collection<? extends GrantedAuthority> authorities,
+    String authenticationScheme) {
     super(principal, credentials, authorities);
     this.principal = principal;
     this.authenticationScheme = authenticationScheme;
-    this.request = request;
+  }
+
+  public static GbifAuthentication anonymous() {
+    return new GbifAuthentication(null, null, Collections.emptyList(), "");
   }
 
   public String getAuthenticationScheme() {
     return authenticationScheme;
-  }
-
-  public HttpServletRequest getRequest() {
-    return request;
   }
 
   @Override
