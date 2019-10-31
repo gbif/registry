c72c5865f25a33f8c749c4d78c9a7e75b3b84919
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-10-31 13:15:53 +0100

    Security improvements, GbifAuthenticationToken

--- spring-boot-common-ws/src/main/java/org/gbif/ws/security/GbifAuthenticationManager.java
+++ spring-boot-common-ws/src/main/java/org/gbif/ws/security/GbifAuthenticationManager.java
@@ -1,11 +1,10 @@
 package org.gbif.ws.security;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
 
 import javax.servlet.http.HttpServletRequest;
 
 public interface GbifAuthenticationManager extends AuthenticationManager {
 
-  Authentication authenticate(HttpServletRequest request);
+  GbifAuthentication authenticate(HttpServletRequest request);
 }
