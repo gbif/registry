b082b3f5fa24d9cc66a3e7b4456c5cb99c179701
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-11-04 15:58:09 +0100

    CitationGeneratorTest

---
+++ registry-spring-boot-metadata/src/test/java/org/gbif/registry/metadata/CitationGeneratorTest.java
@@ -0,0 +1,174 @@
+package org.gbif.registry.metadata;
+
+import org.gbif.api.model.common.DOI;
+import org.gbif.api.model.registry.Contact;
+import org.gbif.api.model.registry.Dataset;
+import org.gbif.api.model.registry.Organization;
+import org.gbif.api.vocabulary.ContactType;
+import org.gbif.api.vocabulary.DatasetType;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.gbif.api.model.common.DOI.TEST_PREFIX;
+import static org.gbif.registry.metadata.CitationGenerator.getAuthors;
+
+/**
+ * Unit tests related to {@link CitationGenerator}.
+ */
+public class CitationGeneratorTest {
+
+  @Test
+  public void testAuthorNames() {
+    Contact c = new Contact();
+    c.setLastName("Doe");
+    c.setFirstName("John D.");
+    assertEquals("Doe J D", CitationGenerator.getAuthorName(c));
+
+    //test with missing first name
+    c = new Contact();
+    c.setLastName("Doe");
+    c.setOrganization("Awesome Organization");
+    assertEquals("Doe", CitationGenerator.getAuthorName(c));
+
+    //test with missing parts
+    c = new Contact();
+    c.setFirstName("John");
+    c.setOrganization("Awesome Organization");
+    assertEquals("Awesome Organization", CitationGenerator.getAuthorName(c));
+  }
+
+  @Test
+  public void testCompleteCitation() {
+
+    Organization org = new Organization();
+    org.setTitle("Cited Organization");
+
+    Dataset dataset = getTestDatasetObject();
+    dataset.getContacts().add(createContact("John D.", "Doe", ContactType.ORIGINATOR));
+
+    assertEquals("Doe J D (2009). Dataset to be cited. Version 2.1. Cited Organization. " +
+        "Sampling event dataset https://doi.org/10.21373/abcd accessed via GBIF.org on " + LocalDate.now().toString() + ".",
+      CitationGenerator.generateCitation(dataset, org));
+  }
+
+  @Test
+  public void testCompleteCitationNoAuthors() {
+    Organization org = new Organization();
+    org.setTitle("Cited Organization");
+
+    Dataset dataset = getTestDatasetObject();
+    dataset.getContacts().add(createContact(null, null, "We are not using this field int the citation",
+      ContactType.ORIGINATOR));
+
+    assertEquals("Cited Organization (2009). Dataset to be cited. Version 2.1. " +
+        "Sampling event dataset https://doi.org/10.21373/abcd accessed via GBIF.org on " + LocalDate.now().toString() + ".",
+      CitationGenerator.generateCitation(dataset, org));
+  }
+
+  @Test
+  public void testCompleteCitationNoYear() {
+    Organization org = new Organization();
+    org.setTitle("Cited Organization");
+
+    Dataset dataset = getTestDatasetObject();
+    dataset.setPubDate(null);
+    dataset.getContacts().add(createContact("John", "Doe", ContactType.ORIGINATOR));
+
+    assertEquals("Doe J. Dataset to be cited. Version 2.1. Cited Organization. " +
+        "Sampling event dataset https://doi.org/10.21373/abcd accessed via GBIF.org on " + LocalDate.now().toString() + ".",
+      CitationGenerator.generateCitation(dataset, org));
+  }
+
+  @Test
+  public void testCompleteCitationAuthorMultipleRoles() {
+    Organization org = new Organization();
+    org.setTitle("Cited Organization");
+
+    Dataset dataset = getTestDatasetObject();
+
+    dataset.getContacts().add(createContact("John D.", "Doe", ContactType.ORIGINATOR));
+    dataset.getContacts().add(createContact("Jim", "Carey", ContactType.PROGRAMMER));
+    dataset.getContacts().add(createContact("John D.", "Doe", ContactType.METADATA_AUTHOR));
+
+    assertEquals("Doe J D (2009). Dataset to be cited. Version 2.1. Cited Organization. " +
+        "Sampling event dataset https://doi.org/10.21373/abcd accessed via GBIF.org on " +
+        LocalDate.now().toString() + ".",
+      CitationGenerator.generateCitation(dataset, org));
+  }
+
+  @Test
+  public void testCompleteCitationNoOriginator() {
+    Organization org = new Organization();
+    org.setTitle("Cited Organization");
+    Dataset dataset = getTestDatasetObject();
+    dataset.getContacts().add(createContact("John D.", "Doe", ContactType.METADATA_AUTHOR));
+
+    assertEquals("Cited Organization (2009). Dataset to be cited. Version 2.1. " +
+        "Sampling event dataset https://doi.org/10.21373/abcd accessed via GBIF.org on " +
+        LocalDate.now().toString() + ".",
+      CitationGenerator.generateCitation(dataset, org));
+  }
+
+  @Test
+  public void testCompleteCitationOriginatorNoName() {
+    Organization org = new Organization();
+    org.setTitle("Cited Organization");
+    Dataset dataset = getTestDatasetObject();
+
+    dataset.getContacts().add(createContact(null, null, "Test Org.", ContactType.ORIGINATOR));
+    dataset.getContacts().add(createContact("John D.", "Doe", ContactType.METADATA_AUTHOR));
+
+    assertEquals("Cited Organization (2009). Dataset to be cited. Version 2.1. " +
+        "Sampling event dataset https://doi.org/10.21373/abcd accessed via GBIF.org on " +
+        LocalDate.now().toString() + ".",
+      CitationGenerator.generateCitation(dataset, org));
+  }
+
+  @Test
+  public void testAuthors() {
+    Organization org = new Organization();
+    org.setTitle("Cited Organization");
+
+    Dataset dataset = getTestDatasetObject();
+
+    dataset.getContacts().add(createContact("John D.", "Doe", ContactType.ORIGINATOR));
+    dataset.getContacts().add(createContact("John D.", "Doe", "Awesome Organization", ContactType.ORIGINATOR));
+    //author with incomplete name
+    dataset.getContacts().add(createContact("Programmer", "Last", ContactType.PROGRAMMER));
+
+    //we expect 1 author since the names (first and last) are mandatory
+    assertEquals(1, getAuthors(dataset.getContacts()).size());
+
+    //but, we can only generate the name for one of them
+    assertEquals(1, CitationGenerator.generateAuthorsName(getAuthors(dataset.getContacts())).size());
+  }
+
+  private Dataset getTestDatasetObject() {
+    Dataset dataset = new Dataset();
+    dataset.setTitle("Dataset to be cited");
+    dataset.setVersion("2.1");
+    dataset.setDoi(new DOI(TEST_PREFIX + "/abcd"));
+    dataset.setPubDate(new Date(LocalDate.of(2009, 2, 8).atStartOfDay(ZoneId.of("UTC")).toInstant().toEpochMilli()));
+
+    dataset.setType(DatasetType.SAMPLING_EVENT);
+
+    return dataset;
+  }
+
+  private Contact createContact(String firstName, String lastName, ContactType ct) {
+    return createContact(firstName, lastName, null, ct);
+  }
+
+  private Contact createContact(String firstName, String lastName, String organization, ContactType ct) {
+    Contact c = new Contact();
+    c.setFirstName(firstName);
+    c.setLastName(lastName);
+    c.setOrganization(organization);
+    c.setType(ct);
+    return c;
+  }
+}
