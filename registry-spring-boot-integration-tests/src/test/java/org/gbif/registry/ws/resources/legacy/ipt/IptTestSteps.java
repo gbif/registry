44b8ea82ef7f34333dbcac1cac9b41d69a5b9156
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-11-05 15:15:48 +0100

    IptIT update

--- registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/ws/resources/legacy/ipt/IptTestSteps.java
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/ws/resources/legacy/ipt/IptTestSteps.java
@@ -30,11 +30,13 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.Date;
 import java.util.Objects;
 import java.util.UUID;
 
 import static org.gbif.registry.ws.resources.legacy.ipt.AssertLegacyInstallation.assertLegacyInstallations;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
 import static org.springframework.http.MediaType.APPLICATION_XML;
@@ -43,7 +45,6 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @SpringBootTest(classes = {TestEmailConfiguration.class, RegistryIntegrationTestsConfiguration.class},
   webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -57,6 +58,10 @@ public class IptTestSteps {
   private Installation actualInstallation;
   private UUID organizationKey;
   private UUID installationKey;
+  Integer contactKeyBeforeSecondUpdate;
+  Integer endpointKeyBeforeSecondUpdate;
+  Date installationCreationDate;
+  String installationCreatedBy;
 
   @Autowired
   private ObjectMapper objectMapper;
@@ -111,6 +116,8 @@ public class IptTestSteps {
   public void prepareInstallation(String instName, String installationKey) {
     this.installationKey = UUID.fromString(installationKey);
     actualInstallation = installationService.get(this.installationKey);
+    installationCreationDate = actualInstallation.getCreated();
+    installationCreatedBy = actualInstallation.getCreatedBy();
   }
 
   @When("register new installation for organization {string}")
@@ -167,6 +174,8 @@ public class IptTestSteps {
     LegacyInstallation expected = LegacyInstallations.newInstance(organizationKey);
     actualInstallation = installationService.get(installationKey);
     assertLegacyInstallations(expected, actualInstallation);
+    assertEquals(installationCreationDate, actualInstallation.getCreated());
+    assertEquals(installationCreatedBy, actualInstallation.getCreatedBy());
   }
 
   @Then("total number of installation is {int}")
@@ -182,5 +191,23 @@ public class IptTestSteps {
   @Then("total number of endpoints is {int}")
   public void checkNumberOfEndpoints(int endpointsNumber) {
     assertEquals(endpointsNumber, actualInstallation.getEndpoints().size());
+  }
+
+  @Given("store contactKey and endpointKey")
+  public void storeContactKeyAndEndpointKey() {
+    contactKeyBeforeSecondUpdate = actualInstallation.getContacts().get(0).getKey();
+    endpointKeyBeforeSecondUpdate = actualInstallation.getEndpoints().get(0).getKey();
+  }
+
+  @Then("contactKey is the same")
+  public void checkContactKeySameAfterUpdate() {
+    // compare contact key and make sure it doesn't change after update (Contacts are mutable)
+    assertEquals(contactKeyBeforeSecondUpdate, actualInstallation.getContacts().get(0).getKey());
+  }
+
+  @Then("endpointKey was updated")
+  public void checkEndpointKeyNewAfterUpdate() {
+    // compare endpoint key and make sure it does change after update (Endpoints are not mutable)
+    assertNotEquals(endpointKeyBeforeSecondUpdate, actualInstallation.getEndpoints().get(0).getKey());
   }
 }
