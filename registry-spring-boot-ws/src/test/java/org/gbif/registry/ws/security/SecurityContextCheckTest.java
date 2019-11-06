1deda2dea1a7da2d87611a6cc14e68266913b946
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-11-06 23:59:34 +0100

    EditorAuthorizationFilterTest

--- registry-spring-boot-ws/src/test/java/org/gbif/registry/ws/security/SecurityContextCheckTest.java
+++ registry-spring-boot-ws/src/test/java/org/gbif/registry/ws/security/SecurityContextCheckTest.java
@@ -18,7 +18,6 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@SuppressWarnings("ResultOfMethodCallIgnored")
 @RunWith(MockitoJUnitRunner.class)
 public class SecurityContextCheckTest {
 
@@ -43,7 +42,6 @@ public class SecurityContextCheckTest {
     assertTrue(SecurityContextCheck.checkUserInRole(mockAuth, "A", "B"));
     assertTrue(SecurityContextCheck.checkUserInRole(mockAuth, "D", "C"));
     assertFalse(SecurityContextCheck.checkUserInRole(mockAuth));
-    assertFalse(SecurityContextCheck.checkUserInRole(mockAuth, null));
     assertFalse(SecurityContextCheck.checkUserInRole(mockAuth, ""));
   }
 
