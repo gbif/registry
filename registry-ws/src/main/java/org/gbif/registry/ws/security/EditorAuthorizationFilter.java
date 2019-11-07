ef14d31eaee0da74dc353cfdd2ae392b501b9d9c
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-11-07 11:27:21 +0100

    EditorAuthorizationFilter change regex pattern in order to match the whole path

--- registry-ws/src/main/java/org/gbif/registry/ws/security/EditorAuthorizationFilter.java
+++ registry-ws/src/main/java/org/gbif/registry/ws/security/EditorAuthorizationFilter.java
@@ -32,7 +32,7 @@ public class EditorAuthorizationFilter implements ContainerRequestFilter {
 
   private static final Logger LOG = LoggerFactory.getLogger(EditorAuthorizationFilter.class);
 
-  private static final String ENTITY_KEY = "^%s/([a-f0-9-]+)";
+  private static final String ENTITY_KEY = "^/?%s/([a-f0-9-]+).*";
   private static final Pattern NODE_NETWORK_PATTERN = Pattern.compile(String.format(ENTITY_KEY, "(?:network|node)"));
   private static final Pattern ORGANIZATION_PATTERN = Pattern.compile(String.format(ENTITY_KEY, "organization"));
   private static final Pattern DATASET_PATTERN = Pattern.compile(String.format(ENTITY_KEY, "dataset"));
