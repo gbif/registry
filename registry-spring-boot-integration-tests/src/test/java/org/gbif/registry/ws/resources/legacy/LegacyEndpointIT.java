876354caa0d41efb8df7db4f5e085de04fbe3925
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-11-21 13:53:13 +0100

    LegacyEndpointIT

---
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/ws/resources/legacy/LegacyEndpointIT.java
@@ -0,0 +1,17 @@
+package org.gbif.registry.ws.resources.legacy;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+  strict = true,
+  features = {
+    "classpath:features/legacy_endpoint.feature"
+  },
+  glue = {"org.gbif.registry.ws.resources.legacy.endpoint", "org.gbif.registry.utils.cucumber"},
+  plugin = "pretty"
+)
+public class LegacyEndpointIT {
+}
