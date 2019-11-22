ca3398981f4d5cf8b58705fdab01d9210b909360
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-11-23 00:28:30 +0100

    LegacyEndpointResponse cucumber transformers

--- registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/utils/cucumber/CucumberTypeRegistryConfigurer.java
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/utils/cucumber/CucumberTypeRegistryConfigurer.java
@@ -9,6 +9,7 @@ import org.gbif.api.model.registry.Endpoint;
 import org.gbif.api.model.registry.Installation;
 import org.gbif.api.service.common.LoggedUserWithToken;
 import org.gbif.registry.ws.model.LegacyEndpoint;
+import org.gbif.registry.ws.model.LegacyEndpointResponse;
 import org.gbif.registry.ws.model.LegacyOrganizationBriefResponse;
 import org.gbif.registry.ws.model.LegacyOrganizationResponse;
 
@@ -31,5 +32,6 @@ public class CucumberTypeRegistryConfigurer implements TypeRegistryConfigurer {
     typeRegistry.defineDataTableType(new DataTableType(LegacyOrganizationResponse.class, new LegacyOrganizationResponseTableEntryTransformer()));
     typeRegistry.defineDataTableType(new DataTableType(LegacyOrganizationBriefResponse.class, new LegacyOrganizationBriefResponseTableEntryTransformer()));
     typeRegistry.defineDataTableType(new DataTableType(LegacyEndpoint.class, new LegacyEndpointTableEntryTransformer()));
+    typeRegistry.defineDataTableType(new DataTableType(LegacyEndpointResponse.class, new LegacyEndpointResponseTableEntryTransformer()));
   }
 }
---
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/utils/cucumber/LegacyEndpointResponseTableEntryTransformer.java
@@ -0,0 +1,24 @@
+package org.gbif.registry.utils.cucumber;
+
+import io.cucumber.datatable.TableEntryTransformer;
+import org.gbif.registry.ws.model.LegacyEndpointResponse;
+
+import java.util.Map;
+
+public class LegacyEndpointResponseTableEntryTransformer implements TableEntryTransformer<LegacyEndpointResponse> {
+
+  @Override
+  public LegacyEndpointResponse transform(Map<String, String> entry) {
+    LegacyEndpointResponse endpoint = new LegacyEndpointResponse();
+    endpoint.setKey(entry.get("key"));
+    endpoint.setOrganisationKey(entry.get("organisationKey"));
+    endpoint.setResourceKey(entry.get("resourceKey"));
+    endpoint.setDescription(entry.get("description"));
+    endpoint.setDescriptionLanguage(entry.get("descriptionLanguage"));
+    endpoint.setType(entry.get("type"));
+    endpoint.setTypeDescription(entry.get("typeDescription"));
+    endpoint.setAccessPointURL(entry.get("accessPointURL"));
+
+    return endpoint;
+  }
+}
