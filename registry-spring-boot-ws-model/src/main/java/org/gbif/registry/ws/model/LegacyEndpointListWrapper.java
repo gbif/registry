102d12b5f52bec9b2e65f7e05e1dfaf7bfdec56a
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-11-25 11:16:50 +0100

    LegacyEndpointIT get endpoints for dataset, json case

--- registry-spring-boot-ws-model/src/main/java/org/gbif/registry/ws/model/LegacyEndpointListWrapper.java
+++ registry-spring-boot-ws-model/src/main/java/org/gbif/registry/ws/model/LegacyEndpointListWrapper.java
@@ -1,9 +1,16 @@
 package org.gbif.registry.ws.model;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.io.IOException;
 import java.util.List;
 
+@JsonSerialize(using = LegacyEndpointListWrapper.LegacyEndpointResponseListWrapperJsonSerializer.class)
 @XmlRootElement(name = "legacyEndpointResponses")
 public class LegacyEndpointListWrapper {
 
@@ -23,5 +30,24 @@ public class LegacyEndpointListWrapper {
 
   public void setLegacyEndpointResponses(List<LegacyEndpointResponse> legacyEndpointResponses) {
     this.legacyEndpointResponses = legacyEndpointResponses;
+  }
+
+  public static class LegacyEndpointResponseListWrapperJsonSerializer extends JsonSerializer<LegacyEndpointListWrapper> {
+
+    @Override
+    public void serialize(LegacyEndpointListWrapper value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+      if (value == null) {
+        jgen.writeNull();
+        return;
+      }
+
+      jgen.writeStartArray();
+      if (value.getLegacyEndpointResponses() != null) {
+        for (LegacyEndpointResponse item : value.getLegacyEndpointResponses()) {
+          jgen.writeObject(item);
+        }
+      }
+      jgen.writeEndArray();
+    }
   }
 }
