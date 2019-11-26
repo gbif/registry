4dcee76ff89ff40d15be5124f99c91458499b273
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-11-26 14:04:32 +0100

    Update cucumber table transformers

--- registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/utils/cucumber/ContactTableEntryTransformer.java
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/utils/cucumber/ContactTableEntryTransformer.java
@@ -3,6 +3,7 @@ package org.gbif.registry.utils.cucumber;
 import io.cucumber.datatable.TableEntryTransformer;
 import org.gbif.api.model.registry.Contact;
 import org.gbif.api.vocabulary.ContactType;
+import org.gbif.api.vocabulary.Country;
 
 import java.util.Collections;
 import java.util.Map;
@@ -13,6 +14,25 @@ public class ContactTableEntryTransformer implements TableEntryTransformer<Conta
   @Override
   public Contact transform(Map<String, String> entry) {
     Contact contact = new Contact();
+    Optional.ofNullable(entry.get("key"))
+      .map(Integer::parseInt)
+      .ifPresent(contact::setKey);
+    Optional.ofNullable(entry.get("primary"))
+      .map(Boolean::parseBoolean)
+      .ifPresent(contact::setPrimary);
+    Optional.ofNullable(entry.get("position"))
+      .map(Collections::singletonList)
+      .ifPresent(contact::setPosition);
+    contact.setDescription(entry.get("description"));
+    contact.setOrganization(entry.get("organization"));
+    contact.setCity(entry.get("city"));
+    contact.setProvince(entry.get("province"));
+    Optional.ofNullable(entry.get("country"))
+      .map(Country::valueOf)
+      .ifPresent(contact::setCountry);
+    contact.setPostalCode(entry.get("postalCode"));
+    contact.setCreatedBy(entry.get("createdBy"));
+    contact.setModifiedBy(entry.get("modifiedBy"));
     Optional.ofNullable(entry.get("type"))
       .map(ContactType::inferType)
       .ifPresent(contact::setType);
--- registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/utils/cucumber/DatasetTableEntryTransformer.java
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/utils/cucumber/DatasetTableEntryTransformer.java
@@ -3,6 +3,7 @@ package org.gbif.registry.utils.cucumber;
 import io.cucumber.datatable.TableEntryTransformer;
 import org.gbif.api.model.registry.Dataset;
 import org.gbif.api.vocabulary.DatasetType;
+import org.gbif.api.vocabulary.Language;
 
 import java.net.URI;
 import java.util.Map;
@@ -14,6 +15,9 @@ public class DatasetTableEntryTransformer implements TableEntryTransformer<Datas
   @Override
   public Dataset transform(Map<String, String> entry) {
     Dataset dataset = new Dataset();
+    Optional.ofNullable(entry.get("key"))
+      .map(UUID::fromString)
+      .ifPresent(dataset::setKey);
     Optional.ofNullable(entry.get("publishingOrganizationKey"))
       .map(UUID::fromString)
       .ifPresent(dataset::setPublishingOrganizationKey);
@@ -31,6 +35,14 @@ public class DatasetTableEntryTransformer implements TableEntryTransformer<Datas
     Optional.ofNullable(entry.get("homepage"))
       .map(URI::create)
       .ifPresent(dataset::setHomepage);
+    Optional.ofNullable(entry.get("external"))
+      .map(Boolean::parseBoolean)
+      .ifPresent(dataset::setExternal);
+    dataset.setCreatedBy(entry.get("createdBy"));
+    dataset.setModifiedBy(entry.get("modifiedBy"));
+    Optional.ofNullable(entry.get("language"))
+      .map(Language::valueOf)
+      .ifPresent(dataset::setLanguage);
 
     return dataset;
   }
