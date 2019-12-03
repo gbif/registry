c6e1316172ee32e53aaf672860c8ec2a3c787576
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-12-03 23:58:41 +0100

    Add condition on EmailSender bean creation

--- registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/RegistryIntegrationTestsConfiguration.java
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/RegistryIntegrationTestsConfiguration.java
@@ -1,14 +1,25 @@
 package org.gbif.registry;
 
+import org.gbif.registry.mail.EmailSender;
+import org.gbif.registry.mail.InMemoryEmailSender;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @TestConfiguration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"org.gbif.ws.server.interceptor",
-    "org.gbif.ws.server.filter", "org.gbif.ws.security", "org.gbif.query", "org.gbif.registry"})
+  "org.gbif.ws.server.filter", "org.gbif.ws.security", "org.gbif.query", "org.gbif.registry"})
 @PropertySource("classpath:application-test.yml")
 public class RegistryIntegrationTestsConfiguration {
+
+  // use InMemoryEmailSender if devemail is disabled
+  @Bean
+  @ConditionalOnProperty(value = "mail.devemail.enabled", havingValue = "false")
+  public EmailSender emailSender() {
+    return new InMemoryEmailSender();
+  }
 }
