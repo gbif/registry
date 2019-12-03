245e293f648e995f1089ad4473b8a51779280b32
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-12-03 15:51:57 +0100

    Enable mail subject message formatting

--- registry-spring-boot-mail/src/main/java/org/gbif/registry/mail/EmailDataProvider.java
+++ registry-spring-boot-mail/src/main/java/org/gbif/registry/mail/EmailDataProvider.java
@@ -2,9 +2,27 @@ package org.gbif.registry.mail;
 
 import java.util.Locale;
 
+/**
+ * Email helper class which provide template name and email subject.
+ */
 public interface EmailDataProvider {
 
-  String getSubject(Locale locale, EmailType emailType);
+  /**
+   * Returns email subject for selected locale and email type.
+   *
+   * @param locale language locale
+   * @param emailType email type
+   * @param subjectParams computable params for subject message formatting
+   * @return email subject
+   */
+  String getSubject(Locale locale, EmailType emailType, String... subjectParams);
 
+  /**
+   * Returns template name for selected locale and email type.
+   *
+   * @param locale language locale
+   * @param emailType email type
+   * @return template name
+   */
   String getTemplate(Locale locale, EmailType emailType);
 }
