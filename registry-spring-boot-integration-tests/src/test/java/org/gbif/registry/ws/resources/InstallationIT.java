76306db794058630021c8a01956e21e488fba3e8
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-12-04 15:06:27 +0100

    Initialize InstallationIT

---
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/ws/resources/InstallationIT.java
@@ -0,0 +1,17 @@
+package org.gbif.registry.ws.resources;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+  strict = true,
+  features = {
+    "classpath:features/installation.feature"
+  },
+  glue = "org.gbif.registry.ws.resources.installation",
+  plugin = "pretty"
+)
+public class InstallationIT {
+}
