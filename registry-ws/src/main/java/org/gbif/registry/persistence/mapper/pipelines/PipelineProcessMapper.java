8afc8d68c9a4b402b6567fdd3386db1839da40a3
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-12-05 15:37:02 +0100

    ingestion service that merges crawl and pipelines history

--- registry-ws/src/main/java/org/gbif/registry/persistence/mapper/pipelines/PipelineProcessMapper.java
+++ registry-ws/src/main/java/org/gbif/registry/persistence/mapper/pipelines/PipelineProcessMapper.java
@@ -1,6 +1,7 @@
 package org.gbif.registry.persistence.mapper.pipelines;
 
 import org.gbif.api.model.common.paging.Pageable;
+import org.gbif.api.model.pipelines.PipelineExecution;
 import org.gbif.api.model.pipelines.PipelineProcess;
 import org.gbif.api.model.pipelines.PipelineStep;
 
@@ -15,13 +16,14 @@ import org.apache.ibatis.annotations.Param;
 public interface PipelineProcessMapper {
 
   /**
-   * Inserts a new {@link PipelineProcess}.
+   * Inserts a new {@link PipelineProcess} if it doesn't exist. If it exists it handles the conflict
+   * and doesn't break.
    *
    * <p>The id generated is set to the {@link PipelineProcess} received as parameter.
    *
    * @param process to insert
    */
-  void create(PipelineProcess process);
+  void createIfNotExists(PipelineProcess process);
 
   /**
    * Retrieves a {@link PipelineProcess} by dataset key and attempt.
@@ -30,8 +32,8 @@ public interface PipelineProcessMapper {
    * @param attempt
    * @return {@link PipelineProcess}
    */
-  PipelineProcess getByDatasetAndAttempt(@Param("datasetKey") UUID datasetKey, @Param("attempt") int attempt);
-
+  PipelineProcess getByDatasetAndAttempt(
+      @Param("datasetKey") UUID datasetKey, @Param("attempt") int attempt);
 
   /**
    * Retrieves a {@link PipelineProcess} by key.
@@ -44,12 +46,31 @@ public interface PipelineProcessMapper {
   Optional<Integer> getLastAttempt(@Param("datasetKey") UUID datasetKey);
 
   /**
-   * Adds a {@link PipelineStep} to an existing {@link PipelineProcess}.
+   * Adds a {@link PipelineExecution} to an existing {@link PipelineProcess}.
    *
    * @param pipelinesProcessKey key of the process where we want to add the step
+   * @param execution execution to add
+   */
+  void addPipelineExecution(
+      @Param("pipelinesProcessKey") long pipelinesProcessKey,
+      @Param("execution") PipelineExecution execution);
+
+  /**
+   * Retrieves a {@link PipelineExecution} by key.
+   *
+   * @param pipelineExecutionKey key of the pipeline execution
+   * @return {@link PipelineExecution}
+   */
+  PipelineExecution getPipelineExecution(@Param("key") long pipelineExecutionKey);
+
+  /**
+   * Adds a {@link PipelineStep} to an existing {@link PipelineExecution}.
+   *
+   * @param pipelineExecutionKey key of the process where we want to add the step
    * @param step step to add
    */
-  void addPipelineStep(@Param("pipelinesProcessKey") long pipelinesProcessKey, @Param("step") PipelineStep step);
+  void addPipelineStep(
+      @Param("pipelineExecutionKey") long pipelineExecutionKey, @Param("step") PipelineStep step);
 
   /**
    * Lists {@link PipelineProcess} based in the search parameters.
@@ -62,13 +83,13 @@ public interface PipelineProcessMapper {
    * @return list of {@link PipelineProcess}
    */
   List<PipelineProcess> list(
-    @Nullable @Param("datasetKey") UUID datasetKey,
-    @Nullable @Param("attempt") Integer attempt,
-    @Nullable @Param("page") Pageable page
-  );
+      @Nullable @Param("datasetKey") UUID datasetKey,
+      @Nullable @Param("attempt") Integer attempt,
+      @Nullable @Param("page") Pageable page);
 
   /** Counts the number of {@link PipelineProcess} based in the search parameters. */
-  long count(@Nullable @Param("datasetKey") UUID datasetKey, @Nullable @Param("attempt") Integer attempt);
+  long count(
+      @Nullable @Param("datasetKey") UUID datasetKey, @Nullable @Param("attempt") Integer attempt);
 
   PipelineStep getPipelineStep(@Param("key") long key);
 
