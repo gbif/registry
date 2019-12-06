812788031952e0ae281c55f53d8dc6fa50e72145
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-12-06 18:52:09 +0100

    Network entity ITs update

--- registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/ws/resources/networkentity/NetworkEntityTestSteps.java
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/ws/resources/networkentity/NetworkEntityTestSteps.java
@@ -6,14 +6,19 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.commons.beanutils.BeanUtils;
 import org.gbif.api.model.registry.Comment;
 import org.gbif.api.model.registry.Contact;
-import org.gbif.api.model.registry.Dataset;
 import org.gbif.api.model.registry.Endpoint;
-import org.gbif.api.model.registry.LenientEquals;
 import org.gbif.api.model.registry.MachineTag;
 import org.gbif.api.model.registry.NetworkEntity;
 import org.gbif.api.model.registry.Tag;
+import org.gbif.api.service.registry.DatasetService;
+import org.gbif.api.service.registry.InstallationService;
+import org.gbif.api.service.registry.NetworkEntityService;
+import org.gbif.api.service.registry.NetworkService;
+import org.gbif.api.service.registry.NodeService;
+import org.gbif.api.service.registry.OrganizationService;
 import org.gbif.registry.RegistryIntegrationTestsConfiguration;
 import org.gbif.registry.utils.Comments;
 import org.gbif.registry.utils.Contacts;
@@ -34,7 +39,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -44,12 +48,9 @@ import java.util.UUID;
 import static org.gbif.registry.utils.LenientAssert.assertLenientEquals;
 import static org.gbif.registry.ws.fixtures.TestConstants.TEST_ADMIN;
 import static org.gbif.registry.ws.fixtures.TestConstants.TEST_PASSWORD;
-import static org.gbif.registry.ws.resources.networkentity.NetworkEntityProvider.ENTITIES;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
@@ -65,20 +66,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 public class NetworkEntityTestSteps {
 
-  private static final UUID UK_NODE_KEY = UUID.fromString("f698c938-d36a-41ac-8120-c35903e1acb9");
-  private static final UUID UK_NODE_2_KEY = UUID.fromString("9996f2f2-f71c-4f40-8e69-031917b314e0");
+  private static final UUID NODE_KEY = UUID.fromString("f698c938-d36a-41ac-8120-c35903e1acb9");
+  private static final UUID NETWORK_KEY = UUID.fromString("83a2c5d5-5d85-4c9e-ac80-dc9e14766e7e");
   private static final UUID ORGANIZATION_KEY = UUID.fromString("f433944a-ad93-4ea8-bad7-68de7348e65a");
   private static final UUID INSTALLATION_KEY = UUID.fromString("70d1ffaf-8e8a-4f40-9c5d-00a0ddbefa4c");
+  private static final UUID DATASET_KEY = UUID.fromString("d82273f6-9738-48a5-a639-2086f9c49d18");
 
-  private static final Map<String, UUID> NODE_MAP = new HashMap<>();
+
+  private static final Map<String, NetworkEntityService> SERVICES = new HashMap<>();
+  private static final Map<String, UUID> ENTITY_KEYS = new HashMap<>();
+
+  static {
+    ENTITY_KEYS.put("node", NODE_KEY);
+    ENTITY_KEYS.put("network", NETWORK_KEY);
+    ENTITY_KEYS.put("organization", ORGANIZATION_KEY);
+    ENTITY_KEYS.put("installation", INSTALLATION_KEY);
+    ENTITY_KEYS.put("dataset", DATASET_KEY);
+  }
 
   private MockMvc mvc;
   private ResultActions result;
-  private NetworkEntity actualEntity;
-  private NetworkEntity expectedEntity;
-  private String key;
-  private Date modificationDateBeforeUpdate;
-  private Date creationDateBeforeUpdate;
+  private UUID entityKey;
   private List<Contact> contacts;
   private Contact expectedContact;
   private List<Endpoint> endpoints;
@@ -101,6 +109,21 @@ public class NetworkEntityTestSteps {
 
   private Connection connection;
 
+  @Autowired
+  private NodeService nodeService;
+
+  @Autowired
+  private NetworkService networkService;
+
+  @Autowired
+  private OrganizationService organizationService;
+
+  @Autowired
+  private InstallationService installationService;
+
+  @Autowired
+  private DatasetService datasetService;
+
   @Before("@NetworkEntity")
   public void setUp() throws Exception {
     connection = ds.getConnection();
@@ -109,19 +132,18 @@ public class NetworkEntityTestSteps {
     ScriptUtils.executeSqlScript(connection,
       new ClassPathResource("/scripts/network_entities_cleanup.sql"));
     ScriptUtils.executeSqlScript(connection,
-      new ClassPathResource("/scripts/organization/organization_node_prepare.sql"));
-    ScriptUtils.executeSqlScript(connection,
-      new ClassPathResource("/scripts/organization/organization_prepare.sql"));
-    ScriptUtils.executeSqlScript(connection,
-      new ClassPathResource("/scripts/organization/network_installation_prepare.sql"));
+      new ClassPathResource("/scripts/networkentity/network_entity_prepare.sql"));
 
     mvc = MockMvcBuilders
       .webAppContextSetup(context)
       .apply(springSecurity())
       .build();
 
-    NODE_MAP.put("UK Node", UK_NODE_KEY);
-    NODE_MAP.put("UK Node 2", UK_NODE_2_KEY);
+    SERVICES.put("node", nodeService);
+    SERVICES.put("network", networkService);
+    SERVICES.put("organization", organizationService);
+    SERVICES.put("installation", installationService);
+    SERVICES.put("dataset", datasetService);
   }
 
   @After("@NetworkEntity")
@@ -139,9 +161,9 @@ public class NetworkEntityTestSteps {
 
   @When("create new {word} by {word} {string} and password {string}")
   public void createEntity(String entityType, String userType, String username, String password) throws Exception {
-    expectedEntity = NetworkEntityProvider.prepare(entityType, UK_NODE_KEY, ORGANIZATION_KEY, INSTALLATION_KEY);
-    expectedEntity.setTitle("New entity " + entityType);
-    String entityJson = objectMapper.writeValueAsString(expectedEntity);
+    NetworkEntity entity = NetworkEntityProvider.prepare(entityType, NODE_KEY, ORGANIZATION_KEY, INSTALLATION_KEY);
+    entity.setTitle("New entity " + entityType);
+    String entityJson = objectMapper.writeValueAsString(entity);
 
     result = mvc
       .perform(
@@ -158,91 +180,38 @@ public class NetworkEntityTestSteps {
       .andExpect(status().is(status));
   }
 
-  @When("get {word} by key")
-  public void getEntityById(String entityType) throws Exception {
-    result = mvc
-      .perform(
-        get("/" + entityType + "/{key}", key));
+  @When("update {word} by {word} {string} and password {string}")
+  public void updateEntity(String entityType, String userType, String username, String password,
+                           Map<String, String> fieldsToUpdate) throws Exception {
+    NetworkEntityService service = SERVICES.get(entityType);
+    UUID key = ENTITY_KEYS.get(entityType);
+    NetworkEntity entity = ((NetworkEntity) service.get(key));
 
-    actualEntity = objectMapper.readValue(result.andReturn().getResponse().getContentAsByteArray(), ENTITIES.get(entityType));
-  }
+    for (Map.Entry<String, String> entry : fieldsToUpdate.entrySet()) {
+      BeanUtils.setProperty(entity, entry.getKey(), entry.getValue());
+    }
 
-  @When("update {word} with new title {string}")
-  public void updateEntity(String entityType, String newTitle) throws Exception {
-    modificationDateBeforeUpdate = actualEntity.getModified();
-    creationDateBeforeUpdate = actualEntity.getCreated();
-    actualEntity.setTitle(newTitle);
-    expectedEntity.setTitle(newTitle);
-
-    String entityJson = objectMapper.writeValueAsString(actualEntity);
+    String entityJson = objectMapper.writeValueAsString(entity);
 
     result = mvc
       .perform(
         put("/" + entityType + "/{key}", key)
-          .with(httpBasic(TEST_ADMIN, TEST_PASSWORD))
+          .with(httpBasic(username, password))
           .content(entityJson)
           .accept(MediaType.APPLICATION_JSON)
           .contentType(MediaType.APPLICATION_JSON));
   }
 
-  @Then("title is new {string}")
-  public void checkUpdatedEntityTitle(String newTitle) {
-    assertEquals("Entity's title was to be updated", newTitle, actualEntity.getTitle());
-  }
-
-  @Then("modification date was updated")
-  public void checkModificationDateWasChangedAfterUpdate() {
-    assertNotNull(actualEntity.getModified());
-    assertTrue("Modification date was to be changed",
-      actualEntity.getModified().after(modificationDateBeforeUpdate));
-  }
-
-  @Then("modification date is after the creation date")
-  public void checkModificationDateIsAfterCreationDate() {
-    assertNotNull(actualEntity.getModified());
-    assertTrue("Modification date must be after the creation date",
-      actualEntity.getModified().after(creationDateBeforeUpdate));
-  }
-
-  @Then("modification and creation dates are present")
-  public void checkModificationCreationDatesPresent() {
-    assertNotNull(actualEntity.getCreated());
-    assertNotNull(actualEntity.getModified());
-    assertNotNull(actualEntity.getCreatedBy());
-    assertNotNull(actualEntity.getModifiedBy());
-  }
-
-  @Then("{word} is not marked as deleted")
-  public void checkEntityIsNotDeleted(String entityType) {
-    assertNull(actualEntity.getDeleted());
-  }
-
-  @SuppressWarnings("unchecked")
-  @Then("created {word} reflects the original one")
-  public void checkCreatedEntityEqualsOriginalOne(String entityType) {
-    // put processed properties (citation)
-    if (entityType.equals("dataset")) {
-      ((Dataset) expectedEntity).setCitation(((Dataset) actualEntity).getCitation());
-    }
-    assertLenientEquals("Persisted does not reflect original", ((LenientEquals) actualEntity), expectedEntity);
-  }
-
-  @SuppressWarnings("unchecked")
-  @Then("deleted {word} reflects the original one")
-  public void checkDeletedEntityEqualsOriginalOne(String entityType) {
-    // put processed properties (citation)
-    if (entityType.equals("dataset")) {
-      ((Dataset) expectedEntity).setCitation(((Dataset) actualEntity).getCitation());
-    }
-    expectedEntity.setDeleted(actualEntity.getDeleted());
-    assertLenientEquals("Persisted does not reflect original", ((LenientEquals) actualEntity), expectedEntity);
+  @When("update {word}")
+  public void updateEntity(String entityType, Map<String, String> fieldsToUpdate) throws Exception {
+    updateEntity(entityType, "admin", TEST_ADMIN, TEST_PASSWORD, fieldsToUpdate);
   }
 
   @When("delete {word} by key")
   public void deleteEntityByKey(String entityType) throws Exception {
     result = mvc
       .perform(
-        delete("/" + entityType + "/{key}", key)
+        delete("/" + entityType + "/{key}", entityKey)
           .with(httpBasic(TEST_ADMIN, TEST_PASSWORD)));
   }
 
@@ -250,7 +219,7 @@ public class NetworkEntityTestSteps {
   public void listEntityContacts(String entityType) throws Exception {
     result = mvc
       .perform(
-        get("/" + entityType + "/{key}/contact", key));
+        get("/" + entityType + "/{key}/contact", entityKey));
   }
 
   @Then("{word} contacts list should contain {int} contacts")
@@ -275,19 +244,18 @@ public class NetworkEntityTestSteps {
 
     result = mvc
       .perform(
-        post("/" + entityType + "/{key}/contact", key)
+        post("/" + entityType + "/{key}/contact", entityKey)
           .with(httpBasic(TEST_ADMIN, TEST_PASSWORD))
           .content(entityJson)
           .accept(MediaType.APPLICATION_JSON)
           .contentType(MediaType.APPLICATION_JSON));
   }
 
-  // TODO: 26/10/2019 add update contact case
   @When("delete {word} contact")
   public void deleteContact(String entityType) throws Exception {
     result = mvc
       .perform(
-        delete("/" + entityType + "/{key}/contact/{contactKey}", key, contacts.get(0).getKey())
+        delete("/" + entityType + "/{key}/contact/{contactKey}", entityKey, contacts.get(0).getKey())
           .with(httpBasic(TEST_ADMIN, TEST_PASSWORD)));
   }
 
@@ -300,7 +268,7 @@ public class NetworkEntityTestSteps {
   public void listEntityEndpoints(String entityType) throws Exception {
     result = mvc
       .perform(
-        get("/" + entityType + "/{key}/endpoint", key));
+        get("/" + entityType + "/{key}/endpoint", entityKey));
   }
 
   @Then("{word} endpoints list should contain {int} endpoints")
@@ -319,7 +287,7 @@ public class NetworkEntityTestSteps {
 
     result = mvc
       .perform(
-        post("/" + entityType + "/{key}/endpoint", key)
+        post("/" + entityType + "/{key}/endpoint", entityKey)
           .with(httpBasic(TEST_ADMIN, TEST_PASSWORD))
           .content(entityJson)
           .accept(MediaType.APPLICATION_JSON)
@@ -330,7 +298,7 @@ public class NetworkEntityTestSteps {
   public void listEntityComments(String entityType) throws Exception {
     result = mvc
       .perform(
-        get("/" + entityType + "/{key}/comment", key));
+        get("/" + entityType + "/{key}/comment", entityKey));
   }
 
   @Then("{word} comments list should contain {int} comments")
@@ -349,7 +317,7 @@ public class NetworkEntityTestSteps {
 
     result = mvc
       .perform(
-        post("/" + entityType + "/{key}/comment", key)
+        post("/" + entityType + "/{key}/comment", entityKey)
           .with(httpBasic(TEST_ADMIN, TEST_PASSWORD))
           .content(entityJson)
           .accept(MediaType.APPLICATION_JSON)
@@ -360,7 +328,7 @@ public class NetworkEntityTestSteps {
   public void listEntityMachineTags(String entityType) throws Exception {
     result = mvc
       .perform(
-        get("/" + entityType + "/{key}/machineTag", key));
+        get("/" + entityType + "/{key}/machineTag", entityKey));
   }
 
   @Then("{word} machine tags list should contain {int} machine tags")
@@ -379,7 +347,7 @@ public class NetworkEntityTestSteps {
 
     result = mvc
       .perform(
-        post("/" + entityType + "/{key}/machineTag", key)
+        post("/" + entityType + "/{key}/machineTag", entityKey)
           .with(httpBasic(TEST_ADMIN, TEST_PASSWORD))
           .content(entityJson)
           .accept(MediaType.APPLICATION_JSON)
@@ -390,7 +358,7 @@ public class NetworkEntityTestSteps {
   public void listEntityTags(String entityType) throws Exception {
     result = mvc
       .perform(
-        get("/" + entityType + "/{key}/tag", key));
+        get("/" + entityType + "/{key}/tag", entityKey));
   }
 
   @Then("{word} tags list should contain {int} tags")
@@ -409,7 +377,7 @@ public class NetworkEntityTestSteps {
 
     result = mvc
       .perform(
-        post("/" + entityType + "/{key}/tag", key)
+        post("/" + entityType + "/{key}/tag", entityKey)
           .with(httpBasic(TEST_ADMIN, TEST_PASSWORD))
           .content(entityJson)
           .accept(MediaType.APPLICATION_JSON)
@@ -418,7 +386,7 @@ public class NetworkEntityTestSteps {
 
   @Then("{word} key is present in response")
   public void extractKeyFromResponse(String entityType) throws Exception {
-    key = RegistryITUtils.removeQuotes(result.andReturn().getResponse().getContentAsString());
-    expectedEntity.setKey(UUID.fromString(key));
+    entityKey =
+      UUID.fromString(RegistryITUtils.removeQuotes(result.andReturn().getResponse().getContentAsString()));
   }
 }
