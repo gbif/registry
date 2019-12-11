fb8c123ddc390309b602f7bb060cc22dc4702ec2
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2019-12-11 11:13:43 +0100

    Custom hamcrest matcher for DOI and dates

---
+++ registry-spring-boot-integration-tests/src/test/java/org/gbif/registry/utils/matcher/IsDoi.java
@@ -0,0 +1,23 @@
+package org.gbif.registry.utils.matcher;
+
+import org.gbif.api.model.common.DOI;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class IsDoi extends TypeSafeMatcher<String> {
+
+  @Override
+  protected boolean matchesSafely(String s) {
+    if (DOI.isParsable(s)) {
+      DOI doi = new DOI(s);
+      return doi.getPrefix().equals(DOI.TEST_PREFIX);
+    }
+
+    return false;
+  }
+
+  @Override
+  public void describeTo(Description description) {
+    description.appendText("DOI");
+  }
+}
