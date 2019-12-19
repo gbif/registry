b1d7a71b42b394ecffe5fd7befaa52056fedff48
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-12-19 14:58:42 +0100

    fix case when there is no dataset process statues in ingestion history

--- registry-ws/src/main/java/org/gbif/registry/pipelines/DefaultIngestionHistoryService.java
+++ registry-ws/src/main/java/org/gbif/registry/pipelines/DefaultIngestionHistoryService.java
@@ -52,14 +52,14 @@ public class DefaultIngestionHistoryService implements IngestionHistoryService {
   @Override
   public PagingResponse<IngestionProcess> ingestionHistory(UUID datasetKey, Pageable pageable) {
     // get datasets process statuses
-    long count = datasetProcessStatusMapper.countByDataset(datasetKey);
-
-    if (count == 0) {
-      return new PagingResponse<>(pageable, count);
-    }
-
     List<DatasetProcessStatus> datasetProcessStatuses =
         datasetProcessStatusMapper.listByDataset(datasetKey, pageable);
+
+    if (datasetProcessStatuses.isEmpty()) {
+      return new PagingResponse<>(pageable, 0L);
+    }
+
+    long count = datasetProcessStatusMapper.countByDataset(datasetKey);
 
     // get all attempts retrieved from the DB
     List<Integer> attempts =
