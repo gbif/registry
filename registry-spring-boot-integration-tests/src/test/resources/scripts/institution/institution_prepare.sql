b9a5cb989981b0522c4fa3a6cf30938b8044e2a6
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-01-03 16:46:00 +0100

    InstitutionIT negative test cases

--- registry-spring-boot-integration-tests/src/test/resources/scripts/institution/institution_prepare.sql
+++ registry-spring-boot-integration-tests/src/test/resources/scripts/institution/institution_prepare.sql
@@ -9,7 +9,7 @@ INSERT INTO public.institution (key, code, name, description, type, active, home
                                 taxonomic_description, number_specimens, index_herbariorum_record, logo_url,
                                 fulltext_search, cites_permit_number, created_by, modified_by, created, modified,
                                 deleted)
-VALUES ('b40143bf-c810-4e67-b998-2936cef72bb3', 'II', 'Institution name', 'dummy description', null, false,
+VALUES ('b40143bf-c810-4e67-b998-2936cef72bb3', 'II', 'First institution', 'dummy description', null, false,
         'http://dummy', null, null, null, null, null, null, null, 1, null, null, null, null, 0, false, null,
         '''descript'':5 ''dummi'':4,6 ''ii'':1 ''institut'':2 ''name'':3', null, 'WS TEST', 'WS TEST',
         '2020-01-02 15:12:03.897224', '2020-01-02 15:12:03.897224', null);
@@ -19,7 +19,7 @@ INSERT INTO public.institution (key, code, name, description, type, active, home
                                 taxonomic_description, number_specimens, index_herbariorum_record, logo_url,
                                 fulltext_search, cites_permit_number, created_by, modified_by, created, modified,
                                 deleted)
-VALUES ('4c7db8e8-23ac-4392-824f-82e17dd19cc8', 'II2', 'Institution name2', 'dummy description', null, false,
+VALUES ('4c7db8e8-23ac-4392-824f-82e17dd19cc8', 'II2', 'Second institution', 'dummy description', null, false,
         'http://dummy', null, null, null, null, null, null, null, 2, null, null, null, null, 0, false, null,
         '''descript'':5 ''dummi'':4,6 ''ii2'':1 ''institut'':2 ''name2'':3', null, 'WS TEST', 'WS TEST',
         '2020-01-02 15:12:03.950265', '2020-01-02 15:12:03.950265', null);
