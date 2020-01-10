a74689d1b11ac67909329fd9a8884ccd5d2912d8
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-01-10 15:16:07 +0100

    Exclude spring-boot solr autoconfiguration

--- registry-spring-boot-ws/src/main/java/org/gbif/registry/RegistryWsApplication.java
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/RegistryWsApplication.java
@@ -2,11 +2,23 @@ package org.gbif.registry;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.solr.SolrHealthContributorAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = {"org.gbif.ws.server.interceptor", "org.gbif.ws.server.aspect",
-  "org.gbif.ws.server.filter", "org.gbif.ws.security", "org.gbif.query", "org.gbif.registry"})
+@SpringBootApplication(
+  scanBasePackages = {
+    "org.gbif.ws.server.interceptor",
+    "org.gbif.ws.server.aspect",
+    "org.gbif.ws.server.filter",
+    "org.gbif.ws.security",
+    "org.gbif.query",
+    "org.gbif.registry"},
+  exclude = {
+    SolrAutoConfiguration.class,
+    SolrHealthContributorAutoConfiguration.class
+  })
 @MapperScan("org.gbif.registry.persistence.mapper")
 @EnableConfigurationProperties
 public class RegistryWsApplication {
