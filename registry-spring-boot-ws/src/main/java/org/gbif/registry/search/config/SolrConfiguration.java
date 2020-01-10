1dd98206357b2a79f568d147e17bf12567d8860b
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-01-10 15:17:24 +0100

    Add solr properties

---
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/search/config/SolrConfiguration.java
@@ -0,0 +1,29 @@
+package org.gbif.registry.search.config;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.gbif.common.search.solr.SolrConfig;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SolrConfiguration {
+
+  private final SolrConfig cfg;
+
+  public SolrConfiguration(SolrDatasetProperties solrProperties) {
+    cfg = new SolrConfig();
+    cfg.setServerType(solrProperties.getType());
+    cfg.setServerHome(solrProperties.getHome());
+    cfg.setCollection(solrProperties.getCollection());
+    // TODO: 10/01/2020 idField was not found in configuration files
+//    cfg.setIdField();
+    cfg.setDeleteOnExit(solrProperties.getDelete());
+  }
+
+  @Qualifier("datasetSolr")
+  @Bean
+  public SolrClient datasetSolr() {
+    return cfg.buildSolr();
+  }
+}
