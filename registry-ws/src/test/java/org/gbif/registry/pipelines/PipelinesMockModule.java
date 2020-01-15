d0c8bc70db3fbcfcb727cd3fe6bd06ac66449bb8
nvolik <nvolik@gbif.org>
2020-01-15 16:13:06 +0100

    Pipelines - fixed test case, changed binding

--- registry-ws/src/test/java/org/gbif/registry/pipelines/PipelinesMockModule.java
+++ registry-ws/src/test/java/org/gbif/registry/pipelines/PipelinesMockModule.java
@@ -5,23 +5,18 @@ import java.util.concurrent.Executors;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
-import com.google.inject.TypeLiteral;
-import com.google.inject.name.Names;
-
-import static org.gbif.registry.pipelines.guice.PipelinesModule.THREAD_POOL_SIZE;
 
 public class PipelinesMockModule extends AbstractModule {
 
   @Override
   protected void configure() {
+    bind(ExecutorService.class)
+      .toInstance(Executors.newSingleThreadExecutor());
     bind(PipelinesHistoryTrackingService.class)
         .to(DefaultPipelinesHistoryTrackingService.class)
         .in(Scopes.SINGLETON);
     bind(IngestionHistoryService.class)
         .to(DefaultIngestionHistoryService.class)
         .in(Scopes.SINGLETON);
-    bind(new TypeLiteral<ExecutorService>() {})
-      .annotatedWith(Names.named(THREAD_POOL_SIZE))
-      .toInstance(Executors.newSingleThreadExecutor());
   }
 }
