973dc4a6ec4c8dbb44100e80b38ab083bfc1c034
nvolik <nvolik@gbif.org>
2020-01-15 15:25:47 +0100

    Pipelines - use mulithreading for rerun mq process

--- registry-ws/src/main/java/org/gbif/registry/pipelines/guice/PipelinesModule.java
+++ registry-ws/src/main/java/org/gbif/registry/pipelines/guice/PipelinesModule.java
@@ -1,15 +1,26 @@
 package org.gbif.registry.pipelines.guice;
 
-import org.gbif.registry.pipelines.*;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.gbif.registry.pipelines.DefaultIngestionHistoryService;
+import org.gbif.registry.pipelines.DefaultPipelinesHistoryTrackingService;
+import org.gbif.registry.pipelines.IngestionHistoryService;
+import org.gbif.registry.pipelines.PipelinesHistoryTrackingService;
 import org.gbif.service.guice.PrivateServiceModule;
 
-import java.util.Properties;
-
+import com.google.inject.Provides;
 import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
 
 public class PipelinesModule extends PrivateServiceModule {
 
   private static final String PREFIX = "pipelines.";
+
+  private static final String THREAD_POOL_SIZE = PREFIX + "do.all.threads";
 
   public PipelinesModule(Properties properties) {
     super(PREFIX, properties);
@@ -26,5 +37,14 @@ public class PipelinesModule extends PrivateServiceModule {
 
     expose(PipelinesHistoryTrackingService.class);
     expose(IngestionHistoryService.class);
+  }
+
+  /** To use several threads when we run doAll from history and send messages in queues  */
+  @Provides
+  @Singleton
+  private ExecutorService provideExecutorService(@Named(THREAD_POOL_SIZE) Integer threadPoolSize) {
+    return Optional.ofNullable(threadPoolSize)
+      .map(Executors::newFixedThreadPool)
+      .orElse(Executors.newSingleThreadExecutor());
   }
 }
