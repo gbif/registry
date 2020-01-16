e39c34460987905767f88c76d642833000d4704c
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-01-16 16:21:45 +0100

    Delete module spring-boot-common-ws, use gbif-common-ws from branch

--- spring-boot-common-ws/src/main/java/org/gbif/ws/CommonRuntimeException.java
+++
@@ -1,7 +0,0 @@
-package org.gbif.ws;
-
-public class CommonRuntimeException extends RuntimeException {
-  public CommonRuntimeException(Throwable cause) {
-    super(cause);
-  }
-}
--- spring-boot-common-ws/src/main/java/org/gbif/ws/security/Md5EncodeService.java
+++
@@ -1,6 +0,0 @@
-package org.gbif.ws.security;
-
-public interface Md5EncodeService {
-
-  String encode(Object entity);
-}
