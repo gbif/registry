4ec171a905a896b349b7d63daf6e518e9394c8e7
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2020-01-16 18:02:15 +0100

    rollback test

--- registry-identity/src/test/java/org/gbif/identity/util/PasswordEncoderTest.java
+++ registry-identity/src/test/java/org/gbif/identity/util/PasswordEncoderTest.java
@@ -9,29 +9,29 @@ import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(value = Parameterized.class)
+@RunWith(value = Parameterized.class)
 public class PasswordEncoderTest {
 
   private PasswordEncoder encoder = new PasswordEncoder();
   private String password;
   private String hash;
 
-//  public PasswordEncoderTest(String password, String hash) {
-//    this.password = password;
-//    this.hash = hash;
-//  }
+  public PasswordEncoderTest(String password, String hash) {
+    this.password = password;
+    this.hash = hash;
+  }
 
-//  @Parameterized.Parameters
-//	 public static Collection<Object[]> testData() {
-//    return Lists.newArrayList(
-//      new Object[]{"adrian", "$S$DNbBTrkalsPChLsqajHUQS18pBBxzSTQW0310SzivTy7HDQ.zgyG"},
-//      new Object[]{"test", "$S$DxVn7wubSRzoK9X2pkGx4njeDRkLEgdqPphc2ZXkkb8Viy8JEGf3"},
-//      new Object[]{"markus", "$S$Dy6/BMI3AoImMSlZGHhEkXgKbUenX7yS2SNgmj7NsrA9JPqH01kW"},
-//      new Object[]{"carla", "$S$DN6evrfX6JzGw./kDKHhON8VEUp7tDbF0gyZYhX1Uw4Y3udFN47w"},
-//      new Object[]{"pia", "$S$DnuHhGOmUVLBYF.kXnE9ZD6ffqnFz7GPxWaikVFLh1JAymnJkdB3"},
-//      new Object[]{"password1", "$S$D1UoWp.wjyDqA1oxwy/MjuSuQnYWsRtSzJGF4vVzdKAN1eh9sIVd"}
-//    );
-//	 }
+  @Parameterized.Parameters
+	 public static Collection<Object[]> testData() {
+    return Lists.newArrayList(
+      new Object[]{"adrian", "$S$DNbBTrkalsPChLsqajHUQS18pBBxzSTQW0310SzivTy7HDQ.zgyG"},
+      new Object[]{"test", "$S$DxVn7wubSRzoK9X2pkGx4njeDRkLEgdqPphc2ZXkkb8Viy8JEGf3"},
+      new Object[]{"markus", "$S$Dy6/BMI3AoImMSlZGHhEkXgKbUenX7yS2SNgmj7NsrA9JPqH01kW"},
+      new Object[]{"carla", "$S$DN6evrfX6JzGw./kDKHhON8VEUp7tDbF0gyZYhX1Uw4Y3udFN47w"},
+      new Object[]{"pia", "$S$DnuHhGOmUVLBYF.kXnE9ZD6ffqnFz7GPxWaikVFLh1JAymnJkdB3"},
+      new Object[]{"password1", "$S$D1UoWp.wjyDqA1oxwy/MjuSuQnYWsRtSzJGF4vVzdKAN1eh9sIVd"}
+    );
+	 }
 
   /**
    * Verify that preencoded passwords can be encoded again (e.g. for auth).
@@ -50,12 +50,6 @@ public class PasswordEncoderTest {
     String encoded1 = encoder.encode(password); // encode it which will generate a random salt
     String encoded2 = encoder.encode(password, encoded1); // encode again reading the salt genarate above
     assertEquals(encoded1, encoded2); // verify they
-  }
-
-  @Test
-  public void test() {
-    System.out.println(encoder.encode("MejJ2kqKB9PL77p4"));
-    System.out.println(encoder.encode("MejJ2kqKB9PL77p4"));
   }
 
 }
