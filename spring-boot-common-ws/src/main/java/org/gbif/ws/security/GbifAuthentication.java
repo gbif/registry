c3e7faa956164e8d31570b0f4af4238c6719b785
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-01-16 16:21:45 +0100

    Delete module spring-boot-common-ws, use gbif-common-ws from branch

--- spring-boot-common-ws/src/main/java/org/gbif/ws/security/GbifAuthentication.java
+++
@@ -1,8 +0,0 @@
-package org.gbif.ws.security;
-
-import org.springframework.security.core.Authentication;
-
-public interface GbifAuthentication extends Authentication {
-
-  String getAuthenticationScheme();
-}
