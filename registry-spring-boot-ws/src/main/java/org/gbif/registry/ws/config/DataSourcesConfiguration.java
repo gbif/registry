296640e911065bb1350a8f5fee6badf1ee618afc
fmendezh <federicomh@gmail.com>
2020-01-24 15:30:19 +0100

    adding back config classes and adding missing clb indexing settings

---
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/config/DataSourcesConfiguration.java
@@ -0,0 +1,45 @@
+package org.gbif.registry.ws.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Contains all datasources required.
+ */
+@Configuration
+public class DataSourcesConfiguration {
+
+  @Bean
+  @Primary
+  @ConfigurationProperties("registry.datasource")
+  public DataSourceProperties registryDataSourceProperties() {
+    return new DataSourceProperties();
+  }
+
+  @Bean
+  @Primary
+  @ConfigurationProperties("registry.datasource.hikari")
+  public HikariDataSource registryDataSource() {
+    return registryDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+  }
+
+
+  @Bean
+  @ConfigurationProperties("indexing.datasource.checklistbank")
+  public DataSourceProperties clbDataSourceProperties() {
+    return new DataSourceProperties();
+  }
+
+  /**
+   * Datasource required for dataset indexing.
+   */
+  @Bean(name = "clb_datasource")
+  @ConfigurationProperties("indexing.datasource.checklistbank.hikari")
+  public HikariDataSource clbDataSource() {
+    return clbDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+  }
+}
