eeb7e854f84a4b990ac6a92b62c4613eff9c50df
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-01-30 16:54:07 +0100

    Reformat some classes

--- registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/RegistryWsApplication.java
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/RegistryWsApplication.java
@@ -11,37 +11,38 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication(
-  exclude = {
-    ElasticsearchAutoConfiguration.class,
-    ElasticSearchRestHealthContributorAutoConfiguration.class
-  })
+    exclude = {
+      ElasticsearchAutoConfiguration.class,
+      ElasticSearchRestHealthContributorAutoConfiguration.class
+    })
 @MapperScan("org.gbif.registry.persistence.mapper")
 @EnableConfigurationProperties
 @ComponentScan(
-  basePackages = {
-    "org.gbif.ws.server.interceptor",
-    "org.gbif.ws.server.aspect",
-    "org.gbif.ws.server.filter",
-    "org.gbif.ws.server.advice",
-    "org.gbif.ws.server.mapper",
-    "org.gbif.ws.security",
-    "org.gbif.registry.search.dataset.service",
-    "org.gbif.registry.search.dataset.indexing",
-    "org.gbif.registry.ws",
-    "org.gbif.registry.persistence",
-    "org.gbif.registry.identity",
-    "org.gbif.registry.surety",
-    "org.gbif.registry.mail",
-    "org.gbif.registry.stubs",
-    "org.gbif.registry.doi",
-    "org.gbif.registry.pipelines",
-    "org.gbif.registry.directory",
-    "org.gbif.registry.events",
-  },
-  excludeFilters = {
-    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DatasetBatchIndexBuilder.class)
-  }
-)
+    basePackages = {
+      "org.gbif.ws.server.interceptor",
+      "org.gbif.ws.server.aspect",
+      "org.gbif.ws.server.filter",
+      "org.gbif.ws.server.advice",
+      "org.gbif.ws.server.mapper",
+      "org.gbif.ws.security",
+      "org.gbif.registry.search.dataset.service",
+      "org.gbif.registry.search.dataset.indexing",
+      "org.gbif.registry.ws",
+      "org.gbif.registry.persistence",
+      "org.gbif.registry.identity",
+      "org.gbif.registry.surety",
+      "org.gbif.registry.mail",
+      "org.gbif.registry.stubs",
+      "org.gbif.registry.doi",
+      "org.gbif.registry.pipelines",
+      "org.gbif.registry.directory",
+      "org.gbif.registry.events",
+    },
+    excludeFilters = {
+      @ComponentScan.Filter(
+          type = FilterType.ASSIGNABLE_TYPE,
+          classes = DatasetBatchIndexBuilder.class)
+    })
 public class RegistryWsApplication {
   public static void main(String[] args) {
     SpringApplication.run(RegistryWsApplication.class, args);
