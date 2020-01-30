3971777e72621f06b9cbcdc407da9d4266090b8c
fmendezh <federicomh@gmail.com>
2020-01-30 17:04:59 +0100

    adding coverages derived from occurrences

--- registry-spring-boot-search/src/main/java/org/gbif/registry/search/dataset/indexing/es/EsConfiguration.java
+++ registry-spring-boot-search/src/main/java/org/gbif/registry/search/dataset/indexing/es/EsConfiguration.java
@@ -2,18 +2,31 @@ package org.gbif.registry.search.dataset.indexing.es;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class EsConfiguration {
 
   @Bean
-  public RestHighLevelClient restHighLevelClient(@Value("${elasticsearch.hosts}") String hosts,
-                                                 @Value("${elasticsearch.connectionTimeOut}") int connectionTimeOut,
-                                                 @Value("${elasticsearch.socketTimeOut}") int socketTimeOut,
-                                                 @Value("${elasticsearch.connectionRequestTimeOut}") int connectionRequestTimeOut,
-                                                 @Value("${elasticsearch.maxRetryTimeOut}") int maxRetryTimeOut) {
+  @Primary
+  public RestHighLevelClient restHighLevelClient(@Value("${elasticsearch.registry.hosts}") String hosts,
+                                                 @Value("${elasticsearch.registry.connectionTimeOut}") int connectionTimeOut,
+                                                 @Value("${elasticsearch.registry.socketTimeOut}") int socketTimeOut,
+                                                 @Value("${elasticsearch.registry.connectionRequestTimeOut}") int connectionRequestTimeOut,
+                                                 @Value("${elasticsearch.registry.maxRetryTimeOut}") int maxRetryTimeOut) {
+    return EsClient.provideEsClient(hosts.split(","), connectionTimeOut, socketTimeOut, connectionRequestTimeOut, maxRetryTimeOut);
+  }
+
+
+  @Bean(name = "occurrenceEsClient")
+  public RestHighLevelClient occurrenceRestHighLevelClient(@Value("${elasticsearch.occurrence.hosts}") String hosts,
+                                                 @Value("${elasticsearch.occurrence.connectionTimeOut}") int connectionTimeOut,
+                                                 @Value("${elasticsearch.occurrence.socketTimeOut}") int socketTimeOut,
+                                                 @Value("${elasticsearch.occurrence.connectionRequestTimeOut}") int connectionRequestTimeOut,
+                                                 @Value("${elasticsearch.occurrence.maxRetryTimeOut}") int maxRetryTimeOut) {
     return EsClient.provideEsClient(hosts.split(","), connectionTimeOut, socketTimeOut, connectionRequestTimeOut, maxRetryTimeOut);
   }
 }
