371e009ee2b69734e2494f20d7f1ece8533b9e55
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-01-30 12:18:14 +0100

    Fix components full scan issue

--- registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/RegistryWsApplication.java
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/RegistryWsApplication.java
@@ -1,7 +1,6 @@
-package org.gbif.registry;
+package org.gbif.registry.ws;
 
 import org.gbif.cli.indexing.dataset.DatasetBatchIndexBuilder;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.elasticsearch.ElasticSearchRestHealthContributorAutoConfiguration;
@@ -26,7 +25,18 @@ import org.springframework.context.annotation.FilterType;
     "org.gbif.ws.server.advice",
     "org.gbif.ws.server.mapper",
     "org.gbif.ws.security",
-    "org.gbif.registry"
+    "org.gbif.registry.search.dataset.service",
+    "org.gbif.registry.search.dataset.indexing",
+    "org.gbif.registry.ws",
+    "org.gbif.registry.persistence",
+    "org.gbif.registry.identity",
+    "org.gbif.registry.surety",
+    "org.gbif.registry.mail",
+    "org.gbif.registry.stubs",
+    "org.gbif.registry.doi",
+    "org.gbif.registry.pipelines",
+    "org.gbif.registry.directory",
+    "org.gbif.registry.events",
   },
   excludeFilters = {
     @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DatasetBatchIndexBuilder.class)
