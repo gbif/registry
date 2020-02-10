633f28b5e147b280601125e3232e8ebc18ebd2f1
fmendezh <federicomh@gmail.com>
2020-02-10 16:17:25 +0100

    adding search as module dependency

--- registry-spring-boot-search/src/main/java/org/gbif/registry/events/search/dataset/indexing/DatasetRealtimeIndexer.java
+++ registry-spring-boot-search/src/main/java/org/gbif/registry/events/search/dataset/indexing/DatasetRealtimeIndexer.java
@@ -33,12 +33,12 @@ import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Slf4j
-@Component
+@Service
 public class DatasetRealtimeIndexer {
 
   private final RestHighLevelClient restHighLevelClient;
