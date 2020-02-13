743a5b0c228057a7570e3520d054e56281389b2a
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-02-13 17:48:24 +0100

    Move messaging to dedicated module

--- registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/RegistryWsApplication.java
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/RegistryWsApplication.java
@@ -44,7 +44,6 @@ import org.springframework.context.annotation.FilterType;
       "org.gbif.registry.events.search.dataset.service",
       "org.gbif.registry.events.search.dataset.indexing",
       "org.gbif.registry.ws.advice",
-      "org.gbif.registry.ws.aspect",
       "org.gbif.registry.ws.config",
       "org.gbif.registry.ws.resources",
       "org.gbif.registry.ws.security",
@@ -53,11 +52,11 @@ import org.springframework.context.annotation.FilterType;
       "org.gbif.registry.identity",
       "org.gbif.registry.surety",
       "org.gbif.registry.mail",
-      "org.gbif.registry.stubs",
       "org.gbif.registry.doi",
       "org.gbif.registry.pipelines",
       "org.gbif.registry.directory",
-      "org.gbif.registry.events"
+      "org.gbif.registry.events",
+      "org.gbif.registry.messaging"
     },
     excludeFilters = {
       @ComponentScan.Filter(
