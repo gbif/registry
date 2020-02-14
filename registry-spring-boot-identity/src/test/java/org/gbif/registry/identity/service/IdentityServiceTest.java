cdef141ca4f23127edaba0b9e6d22e8a329c1497
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-02-14 16:29:19 +0100

    Fix codestyle issue

--- registry-spring-boot-identity/src/test/java/org/gbif/registry/identity/service/IdentityServiceTest.java
+++ registry-spring-boot-identity/src/test/java/org/gbif/registry/identity/service/IdentityServiceTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Global Biodiversity Information Facility (GBIF)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gbif.registry.identity.service;
 
 import org.gbif.api.model.common.GbifUser;
@@ -125,8 +140,7 @@ public class IdentityServiceTest {
     gbifUser.setUserName("");
 
     // WHEN
-    UserModelMutationResult result = identityService
-      .create(gbifUser, TEST_PASSWORD);
+    UserModelMutationResult result = identityService.create(gbifUser, TEST_PASSWORD);
 
     // THEN
     assertEquals(ModelMutationError.CONSTRAINT_VIOLATION, result.getError());
