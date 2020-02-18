dfc9257b4c5cc8fcc11bcdefd44540b091a79a34
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-02-18 23:43:01 +0100

    Fix package name

--- registry-spring-boot-occurrence-client/src/main/java/org/gbif/registry/occurrenceclient/OccurrenceMetricsClient.java
+++ registry-spring-boot-occurrence-client/src/main/java/org/gbif/registry/occurrenceclient/OccurrenceMetricsClient.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gbif.registry.occurenceclient;
+package org.gbif.registry.occurrenceclient;
 
 import java.util.UUID;
 
