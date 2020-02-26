41c4ab5a0f02ba885c74bdcdb99888b19a4ab3a6
fmendezh <federicomh@gmail.com>
2020-02-26 16:25:04 +0100

    It seems that after all it is necessary to read the parameters!

---
+++ registry-spring-boot-ws/src/main/java/org/gbif/registry/ws/security/DebugFilter.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 Global Biodiversity Information Facility (GBIF)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.registry.ws.security;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * This filter solved an issue that was preventing form parameters to be read in request filters.
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class DebugFilter implements Filter {
+
+  private static final Logger LOG = LoggerFactory.getLogger(DebugFilter.class);
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    LOG.debug("Params {}: ", request.getParameterMap().size());
+    chain.doFilter(request, response);
+  }
+}
