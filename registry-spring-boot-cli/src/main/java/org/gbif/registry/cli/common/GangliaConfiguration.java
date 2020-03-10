310460591365e8bf5e0a721709407d38daee5754
fmendezh <federicomh@gmail.com>
2020-03-10 16:40:33 +0100

    upgrading to guava 23 to match gbif-common and gbif-cli versions

--- registry-spring-boot-cli/src/main/java/org/gbif/registry/cli/common/GangliaConfiguration.java
+++ registry-spring-boot-cli/src/main/java/org/gbif/registry/cli/common/GangliaConfiguration.java
@@ -19,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 
 import com.beust.jcommander.Parameter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.common.base.Objects;
+import com.google.common.base.MoreObjects;
 import com.yammer.metrics.reporting.GangliaReporter;
 
 /**
@@ -45,6 +45,6 @@ public class GangliaConfiguration {
 
   @Override
   public String toString() {
-    return Objects.toStringHelper(this).add("host", host).add("port", port).toString();
+    return MoreObjects.toStringHelper(this).add("host", host).add("port", port).toString();
   }
 }
