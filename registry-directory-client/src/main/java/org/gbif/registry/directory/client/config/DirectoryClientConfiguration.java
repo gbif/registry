997a96d4e3ad7bd0512971a37dbaa1450a1e52d0
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-01 21:45:12 +0200

    Use client factory for directory clients

--- registry-directory-client/src/main/java/org/gbif/registry/directory/client/config/DirectoryClientConfiguration.java
+++ registry-directory-client/src/main/java/org/gbif/registry/directory/client/config/DirectoryClientConfiguration.java
@@ -18,76 +18,36 @@ package org.gbif.registry.directory.client.config;
 import org.gbif.registry.directory.client.NodeClient;
 import org.gbif.registry.directory.client.ParticipantClient;
 import org.gbif.registry.directory.client.PersonClient;
-import org.gbif.ws.client.ClientContract;
-import org.gbif.ws.client.ClientErrorDecoder;
-import org.gbif.ws.client.GbifAuthRequestInterceptor;
-import org.gbif.ws.security.Md5EncodeService;
-import org.gbif.ws.security.SigningService;
+import org.gbif.ws.client.ClientFactory;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import feign.Contract;
-import feign.Feign;
-import feign.codec.Decoder;
-import feign.codec.ErrorDecoder;
-import feign.jackson.JacksonDecoder;
-
 @Configuration
 public class DirectoryClientConfiguration {
 
-  private String url;
-  private GbifAuthRequestInterceptor requestInterceptor;
-  private Decoder decoder;
-  private ErrorDecoder errorDecoder;
-  private Contract contract;
+  private ClientFactory clientFactory;
 
   public DirectoryClientConfiguration(
       @Value("${directory.ws.url}") String url,
       @Value("${directory.app.key}") String appKey,
-      @Value("${directory.app.secret}") String secretKey,
-      @Qualifier("secretKeySigningService") SigningService signingService,
-      Md5EncodeService md5EncodeService,
-      @Qualifier("registryObjectMapper") ObjectMapper objectMapper) {
-    this.url = url;
-    this.requestInterceptor =
-        new GbifAuthRequestInterceptor(appKey, secretKey, signingService, md5EncodeService);
-    this.decoder = new JacksonDecoder(objectMapper);
-    this.errorDecoder = new ClientErrorDecoder();
-    this.contract = new ClientContract();
+      @Value("${directory.app.secret}") String secretKey) {
+    this.clientFactory = new ClientFactory(appKey, url, appKey, secretKey);
   }
 
   @Bean
   public NodeClient nodeClient() {
-    return Feign.builder()
-        .decoder(decoder)
-        .errorDecoder(errorDecoder)
-        .contract(contract)
-        .requestInterceptor(requestInterceptor)
-        .target(NodeClient.class, url);
+    return clientFactory.newInstance(NodeClient.class);
   }
 
   @Bean
   public ParticipantClient participantClient() {
-    return Feign.builder()
-        .decoder(decoder)
-        .errorDecoder(errorDecoder)
-        .contract(contract)
-        .requestInterceptor(requestInterceptor)
-        .target(ParticipantClient.class, url);
+    return clientFactory.newInstance(ParticipantClient.class);
   }
 
   @Bean
   public PersonClient personClient() {
-    return Feign.builder()
-        .decoder(decoder)
-        .errorDecoder(errorDecoder)
-        .contract(contract)
-        .requestInterceptor(requestInterceptor)
-        .target(PersonClient.class, url);
+    return clientFactory.newInstance(PersonClient.class);
   }
 }
