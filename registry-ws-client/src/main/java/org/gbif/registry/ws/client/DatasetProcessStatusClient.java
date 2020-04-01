0a867e6b8393896aafe6a29a1e8cc8a4a6803f33
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-01 21:47:11 +0200

    Reformat registry clients

--- registry-ws-client/src/main/java/org/gbif/registry/ws/client/DatasetProcessStatusClient.java
+++ registry-ws-client/src/main/java/org/gbif/registry/ws/client/DatasetProcessStatusClient.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Global Biodiversity Information Facility (GBIF)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gbif.registry.ws.client;
 
 import org.gbif.api.model.common.paging.Pageable;
@@ -20,49 +35,47 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public interface DatasetProcessStatusClient extends DatasetProcessStatusService {
 
   @Override
-  default void createDatasetProcessStatus(
-    @NotNull DatasetProcessStatus datasetProcessStatus) {
+  default void createDatasetProcessStatus(@NotNull DatasetProcessStatus datasetProcessStatus) {
     throw new IllegalStateException("Dataset process status create not supported");
   }
 
   @Override
-  default void updateDatasetProcessStatus(
-    @NotNull DatasetProcessStatus datasetProcessStatus) {
+  default void updateDatasetProcessStatus(@NotNull DatasetProcessStatus datasetProcessStatus) {
     throw new IllegalStateException("Dataset process status update not supported");
   }
 
   @RequestMapping(
-    method = RequestMethod.GET,
-    value = "dataset/{datasetKey}/process/{attempt}",
-    produces = MediaType.APPLICATION_JSON_VALUE)
+      method = RequestMethod.GET,
+      value = "dataset/{datasetKey}/process/{attempt}",
+      produces = MediaType.APPLICATION_JSON_VALUE)
   @Nullable
   @ResponseBody
   @Override
   DatasetProcessStatus getDatasetProcessStatus(
-    @PathVariable("datasetKey") UUID datasetKey, @PathVariable("attempt") int attempt);
+      @PathVariable("datasetKey") UUID datasetKey, @PathVariable("attempt") int attempt);
 
   @RequestMapping(
-    method = RequestMethod.GET,
-    value = "dataset/process",
-    produces = MediaType.APPLICATION_JSON_VALUE)
+      method = RequestMethod.GET,
+      value = "dataset/process",
+      produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
   @Override
   PagingResponse<DatasetProcessStatus> listDatasetProcessStatus(@SpringQueryMap Pageable page);
 
   @RequestMapping(
-    method = RequestMethod.GET,
-    value = "dataset/process/aborted",
-    produces = MediaType.APPLICATION_JSON_VALUE)
+      method = RequestMethod.GET,
+      value = "dataset/process/aborted",
+      produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
   @Override
   PagingResponse<DatasetProcessStatus> listAbortedDatasetProcesses(@SpringQueryMap Pageable page);
 
   @RequestMapping(
-    method = RequestMethod.GET,
-    value = "dataset/{datasetKey}/process",
-    produces = MediaType.APPLICATION_JSON_VALUE)
+      method = RequestMethod.GET,
+      value = "dataset/{datasetKey}/process",
+      produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
   @Override
   PagingResponse<DatasetProcessStatus> listDatasetProcessStatus(
-    @PathVariable("datasetKey") UUID datasetKey, @SpringQueryMap Pageable page);
+      @PathVariable("datasetKey") UUID datasetKey, @SpringQueryMap Pageable page);
 }
