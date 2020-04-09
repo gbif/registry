44c8f864d0a7964bf744b032c4c4e07bcdd52097
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-09 21:47:06 +0200

    MetasyncHistoryIT

--- registry-ws-it-tests/src/test/java/org/gbif/registry/ws/MetasyncHistoryIT.java
+++ registry-ws-it-tests/src/test/java/org/gbif/registry/ws/MetasyncHistoryIT.java
@@ -15,6 +15,7 @@
  */
 package org.gbif.registry.ws;
 
+import org.gbif.api.model.common.paging.Pageable;
 import org.gbif.api.model.common.paging.PagingRequest;
 import org.gbif.api.model.common.paging.PagingResponse;
 import org.gbif.api.model.registry.Installation;
@@ -25,24 +26,40 @@ import org.gbif.api.service.registry.InstallationService;
 import org.gbif.api.service.registry.MetasyncHistoryService;
 import org.gbif.api.service.registry.NodeService;
 import org.gbif.api.service.registry.OrganizationService;
+import org.gbif.api.vocabulary.UserRole;
 import org.gbif.registry.database.DatabaseInitializer;
 import org.gbif.registry.test.TestDataFactory;
+import org.gbif.registry.ws.fixtures.TestConstants;
 import org.gbif.ws.client.filter.SimplePrincipalProvider;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.UUID;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import io.zonky.test.db.postgres.embedded.LiquibasePreparer;
 import io.zonky.test.db.postgres.junit5.EmbeddedPostgresExtension;
 import io.zonky.test.db.postgres.junit5.PreparedDbExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Runs tests for the {@link MetasyncHistoryService} implementations. This is parameterized to run
@@ -53,7 +70,11 @@ import io.zonky.test.db.postgres.junit5.PreparedDbExtension;
  *   <li>The WS service client layer
  * </ol>
  */
-@RunWith(Parameterized.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = RegistryIntegrationTestsConfiguration.class)
+@ContextConfiguration(initializers = {MetasyncHistoryIT.ContextInitializer.class})
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 public class MetasyncHistoryIT {
 
   @RegisterExtension
@@ -65,11 +86,34 @@ public class MetasyncHistoryIT {
   public final DatabaseInitializer databaseRule =
       new DatabaseInitializer(database.getTestDatabase());
 
-  // Tests user
-  private static String TEST_USER = "admin";
+  static class ContextInitializer
+      implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+      TestPropertyValues.of(dbTestPropertyPairs())
+          .applyTo(configurableApplicationContext.getEnvironment());
+      withSearchEnabled(false, configurableApplicationContext.getEnvironment());
+    }
+
+    protected static void withSearchEnabled(
+        boolean enabled, ConfigurableEnvironment configurableEnvironment) {
+      TestPropertyValues.of("searchEnabled=" + enabled).applyTo(configurableEnvironment);
+    }
+
+    protected String[] dbTestPropertyPairs() {
+      return new String[] {
+        "registry.datasource.url=jdbc:postgresql://localhost:"
+            + database.getConnectionInfo().getPort()
+            + "/"
+            + database.getConnectionInfo().getDbName(),
+        "registry.datasource.username=" + database.getConnectionInfo().getUser(),
+        "registry.datasource.password="
+      };
+    }
+  }
 
   private final MetasyncHistoryService metasyncHistoryService;
-
   private final SimplePrincipalProvider simplePrincipalProvider;
 
   private final OrganizationService organizationService;
@@ -94,11 +138,18 @@ public class MetasyncHistoryIT {
     this.testDataFactory = testDataFactory;
   }
 
-  @Before
+  @BeforeEach
   public void setup() {
     // reset SimplePrincipleProvider, configured for web service client tests only
     if (simplePrincipalProvider != null) {
-      simplePrincipalProvider.setPrincipal(TEST_USER);
+      simplePrincipalProvider.setPrincipal(TestConstants.TEST_ADMIN);
+      SecurityContext ctx = SecurityContextHolder.createEmptyContext();
+      SecurityContextHolder.setContext(ctx);
+      ctx.setAuthentication(
+          new UsernamePasswordAuthenticationToken(
+              simplePrincipalProvider.get().getName(),
+              "",
+              Collections.singleton(new SimpleGrantedAuthority(UserRole.REGISTRY_ADMIN.name()))));
     }
   }
 
@@ -109,14 +160,11 @@ public class MetasyncHistoryIT {
     metasyncHistoryService.createMetasync(metasyncHistory);
     PagingResponse<MetasyncHistory> response =
         metasyncHistoryService.listMetasync(new PagingRequest());
-    Assert.assertTrue(
-        "The list operation should return at least 1 record", response.getResults().size() > 0);
+    assertTrue(
+        response.getResults().size() > 0, "The list operation should return at least 1 record");
   }
 
-  /**
-   * Tests the {@link MetasyncHistoryService#listByInstallation(UUID,
-   * org.gbif.api.model.common.paging.Pageable)} operation.
-   */
+  /** Tests the {@link MetasyncHistoryService#listMetasync(UUID, Pageable)} operation. */
   @Test
   public void testListAndListByInstallation() {
     MetasyncHistory metasyncHistory = getTestInstance();
@@ -124,8 +172,8 @@ public class MetasyncHistoryIT {
     PagingResponse<MetasyncHistory> response =
         metasyncHistoryService.listMetasync(
             metasyncHistory.getInstallationKey(), new PagingRequest());
-    Assert.assertTrue(
-        "The list operation should return at least 1 record", response.getResults().size() > 0);
+    assertTrue(
+        response.getResults().size() > 0, "The list operation should return at least 1 record");
   }
 
   /**
