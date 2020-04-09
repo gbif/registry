001407767b3804f7ff847353ba4fa8c1c0529509
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-09 13:47:03 +0200

    DatasetSearchClient#search

--- registry-ws-client/src/main/java/org/gbif/registry/ws/client/DatasetSearchClient.java
+++ registry-ws-client/src/main/java/org/gbif/registry/ws/client/DatasetSearchClient.java
@@ -19,18 +19,17 @@ import org.gbif.api.model.common.search.SearchResponse;
 import org.gbif.api.model.registry.search.DatasetSearchParameter;
 import org.gbif.api.model.registry.search.DatasetSearchRequest;
 import org.gbif.api.model.registry.search.DatasetSearchResult;
-import org.gbif.api.model.registry.search.DatasetSuggestRequest;
-import org.gbif.api.model.registry.search.DatasetSuggestResult;
 import org.gbif.api.service.registry.DatasetSearchService;
 
-import java.util.List;
+import java.util.Set;
 
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// TODO: 03/04/2020 find a way how to process object request params
 public interface DatasetSearchClient extends DatasetSearchService {
 
   @RequestMapping(
@@ -38,15 +37,49 @@ public interface DatasetSearchClient extends DatasetSearchService {
       value = "dataset/search",
       produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
-  @Override
   SearchResponse<DatasetSearchResult, DatasetSearchParameter> search(
-      DatasetSearchRequest datasetSearchRequest);
+      @SpringQueryMap DatasetSearchRequest datasetSearchRequest,
+      @RequestParam(value = "type", required = false) Set<String> type,
+      @RequestParam(value = "subtype", required = false) Set<String> subtype,
+      @RequestParam(value = "publishingOrg", required = false) Set<String> publishingOrg,
+      @RequestParam(value = "hostingOrg", required = false) Set<String> hostingOrg,
+      @RequestParam(value = "keyword", required = false) Set<String> keyword,
+      @RequestParam(value = "decade", required = false) Set<String> decade,
+      @RequestParam(value = "publishingCountry", required = false) Set<String> publishingCountry,
+      @RequestParam(value = "country", required = false) Set<String> country,
+      @RequestParam(value = "continent", required = false) Set<String> continent,
+      @RequestParam(value = "license", required = false) Set<String> license,
+      @RequestParam(value = "projectId", required = false) Set<String> projectId,
+      @RequestParam(value = "taxonKey", required = false) Set<String> taxonKey,
+      @RequestParam(value = "recordCount", required = false) Set<String> recordCount,
+      @RequestParam(value = "year", required = false) Set<String> year,
+      @RequestParam(value = "modifiedDate", required = false) Set<String> modifiedDate,
+      @RequestParam(value = "datasetTitle", required = false) Set<String> datasetTitle,
+      @RequestParam(value = "collectionKey", required = false) Set<String> collectionKey,
+      @RequestParam(value = "institutionKey", required = false) Set<String> institutionKey);
 
-  @RequestMapping(
-      method = RequestMethod.GET,
-      value = "dataset/suggest",
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  @ResponseBody
   @Override
-  List<DatasetSuggestResult> suggest(DatasetSuggestRequest datasetSuggestRequest);
+  default SearchResponse<DatasetSearchResult, DatasetSearchParameter> search(
+      DatasetSearchRequest datasetSearchRequest) {
+    return search(
+        datasetSearchRequest,
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.TYPE),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.SUBTYPE),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.PUBLISHING_ORG),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.HOSTING_ORG),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.KEYWORD),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.DECADE),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.PUBLISHING_COUNTRY),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.COUNTRY),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.CONTINENT),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.LICENSE),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.PROJECT_ID),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.TAXON_KEY),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.RECORD_COUNT),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.YEAR),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.MODIFIED_DATE),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.DATASET_TITLE),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.COLLECTION_KEY),
+        datasetSearchRequest.getParameters().get(DatasetSearchParameter.INSTITUTION_KEY));
+  }
 }
