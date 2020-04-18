f722762845077bec7cc6ba4ff878a1525eb1b17a
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-18 22:05:21 +0200

    Replace junit 4 functionality

--- registry-ws-it-tests/src/test/java/org/gbif/registry/ws/util/LegacyResourceUtilsTest.java
+++ registry-ws-it-tests/src/test/java/org/gbif/registry/ws/util/LegacyResourceUtilsTest.java
@@ -17,9 +17,9 @@ package org.gbif.registry.ws.util;
 
 import org.gbif.registry.domain.ws.LegacyDataset;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LegacyResourceUtilsTest {
 
