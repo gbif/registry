c29476cb262bfabdd042ad9bcb2b047a1f3789a2
fmendezh <federicomh@gmail.com>
2020-04-20 15:11:19 +0200

    removing @Import

--- registry-ws-it-tests/src/test/java/org/gbif/registry/ws/it/EnumerationResourceIT.java
+++ registry-ws-it-tests/src/test/java/org/gbif/registry/ws/it/EnumerationResourceIT.java
@@ -27,7 +27,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
