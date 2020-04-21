16de6ce78cf4a582059f9c938b3121eb5ea776b2
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-21 22:43:35 +0200

    Implement Runner in metasync module

--- registry-metasync/src/main/java/org/gbif/registry/metasync/Runner.java
+++ registry-metasync/src/main/java/org/gbif/registry/metasync/Runner.java
@@ -15,44 +15,59 @@
  */
 package org.gbif.registry.metasync;
 
+import org.gbif.api.service.registry.DatasetService;
+import org.gbif.api.service.registry.InstallationService;
+import org.gbif.api.service.registry.MetasyncHistoryService;
+import org.gbif.registry.metasync.api.SyncResult;
+import org.gbif.registry.metasync.protocols.biocase.BiocaseMetadataSynchroniser;
+import org.gbif.registry.metasync.protocols.digir.DigirMetadataSynchroniser;
+import org.gbif.registry.metasync.protocols.tapir.TapirMetadataSynchroniser;
+import org.gbif.registry.metasync.resulthandler.DebugHandler;
+import org.gbif.registry.metasync.resulthandler.RegistryUpdater;
+import org.gbif.registry.metasync.util.HttpClientFactory;
+import org.gbif.registry.ws.client.DatasetClient;
+import org.gbif.registry.ws.client.InstallationClient;
+import org.gbif.ws.client.ClientFactory;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public final class Runner {
 
   private static final Logger LOG = LoggerFactory.getLogger(Runner.class);
 
   public static void main(String[] args) {
-    //    SLF4JBridgeHandler.removeHandlersForRootLogger();
-    //    SLF4JBridgeHandler.install();
-    //
-    //    ClientFactory clientFactory = new ClientFactory("username", "http://localhost:8080",
-    // "username",
-    //      "password");
-    //    InstallationService installationService =
-    // clientFactory.newInstance(InstallationClient.class);
-    //
-    //    MetadataSynchroniserImpl synchroniser = new MetadataSynchroniserImpl(installationService);
-    //
-    //    synchroniser
-    //      .registerProtocolHandler(new
-    // DigirMetadataSynchroniser(clientFactory.provideHttpClient()));
-    //    synchroniser
-    //      .registerProtocolHandler(new
-    // TapirMetadataSynchroniser(clientFactory.provideHttpClient()));
-    //    synchroniser
-    //      .registerProtocolHandler(new
-    // BiocaseMetadataSynchroniser(clientFactory.provideHttpClient()));
-    //
-    //    DatasetService datasetService = clientFactory.newInstance(DatasetClient.class);
-    //
-    //    List<SyncResult> syncResults = synchroniser.synchroniseAllInstallations(100);
-    //    LOG.info("Done syncing. Processing results");
-    //    DebugHandler.processResults(syncResults);
-    //
-    //    RegistryUpdater updater = new RegistryUpdater(datasetService,
-    //      ((MetasyncHistoryService) installationService));
-    //    updater.saveSyncResultsToRegistry(syncResults);
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
+
+    HttpClientFactory clientFactory = new HttpClientFactory(10, TimeUnit.SECONDS);
+
+    ClientFactory wsClientFactory =
+        new ClientFactory("username", "http://localhost:8080", "username", "password");
+    InstallationService installationService = wsClientFactory.newInstance(InstallationClient.class);
+
+    MetadataSynchroniserImpl synchroniser = new MetadataSynchroniserImpl(installationService);
+
+    synchroniser.registerProtocolHandler(
+        new DigirMetadataSynchroniser(clientFactory.provideHttpClient()));
+    synchroniser.registerProtocolHandler(
+        new TapirMetadataSynchroniser(clientFactory.provideHttpClient()));
+    synchroniser.registerProtocolHandler(
+        new BiocaseMetadataSynchroniser(clientFactory.provideHttpClient()));
+
+    DatasetService datasetService = wsClientFactory.newInstance(DatasetClient.class);
+
+    List<SyncResult> syncResults = synchroniser.synchroniseAllInstallations(100);
+    LOG.info("Done syncing. Processing results");
+    DebugHandler.processResults(syncResults);
+
+    RegistryUpdater updater =
+        new RegistryUpdater(datasetService, ((MetasyncHistoryService) installationService));
+    updater.saveSyncResultsToRegistry(syncResults);
   }
 
   private Runner() {
