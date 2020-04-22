4cc926b66dcc04d2dce35bb2d8150631c662f388
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-23 01:26:33 +0200

    Move ITs to registry-integration-tests module

--- registry-integration-tests/src/test/resources/scripts/pipelines/pipelines_cleanup.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM public.download_statistics;
-DELETE FROM public.dataset;
-DELETE FROM public.installation;
-DELETE FROM public.organization;
-DELETE FROM public.node;
-
-DELETE FROM public.pipeline_step;
-DELETE FROM public.pipeline_execution;
-DELETE FROM public.pipeline_process;
--- registry-integration-tests/src/test/resources/scripts/user/user_cleanup.sql
+++
@@ -1,6 +0,0 @@
-DELETE
-FROM public."user"
-WHERE key = 4;
-DELETE
-FROM public."user"
-WHERE key = 5;
