ae22ba097a3a17c1911b5296eeeb2f4f48da2497
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-04-24 13:04:44 +0200

    Move AlternativeCodesTypeHandler to auxhandler package

--- registry-persistence/src/main/java/org/gbif/registry/persistence/mapper/auxhandler/AlternativeCodesTypeHandler.java
+++ registry-persistence/src/main/java/org/gbif/registry/persistence/mapper/auxhandler/AlternativeCodesTypeHandler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.gbif.registry.persistence.mapper.handler;
+package org.gbif.registry.persistence.mapper.auxhandler;
 
 import org.gbif.api.model.collections.Institution;
 
