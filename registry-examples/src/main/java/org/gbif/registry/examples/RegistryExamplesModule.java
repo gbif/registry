42ba2d83da0b7854afffd303ce4744b4c7287580
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-07-03 00:30:40 +0200

    Add empty class to registry-examples to make it build, small fixes

---
+++ registry-examples/src/main/java/org/gbif/registry/examples/RegistryExamplesModule.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020 Global Biodiversity Information Facility (GBIF)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.registry.examples;
+
+// This is a temporal fix for the following problem:
+// JAR will be empty - no content was marked for inclusion!
+public interface RegistryExamplesModule {}
