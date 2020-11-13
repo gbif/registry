5fb2b00b22a43c7da4137a5a9fbd3a4237ade681
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2020-11-13 14:31:29 +0100

    modified search to do prefix matching while keeping exact code matching

--- registry-search/src/main/java/org/gbif/registry/search/dataset/service/collections/CollectionsSearchService.java
+++ registry-search/src/main/java/org/gbif/registry/search/dataset/service/collections/CollectionsSearchService.java
@@ -19,8 +19,15 @@ import org.gbif.registry.persistence.mapper.collections.CollectionsSearchMapper;
 import org.gbif.registry.persistence.mapper.collections.dto.SearchDto;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.elasticsearch.common.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,8 +49,25 @@ public class CollectionsSearchService {
   public List<SearchResponse> search(String query, boolean highlight) {
     List<SearchDto> dtos = searchMapper.search(query, highlight);
 
-    List<SearchResponse> responses = new ArrayList<>();
+    Map<UUID, SearchDto> dtosMap = new HashMap<>();
     dtos.forEach(
+        dto -> {
+          if (dtosMap.containsKey(dto.getKey())) {
+            SearchDto existing = dtosMap.get(dto.getKey());
+            mergeDtos(existing, dto);
+          } else {
+            dtosMap.put(dto.getKey(), dto);
+          }
+        });
+
+    List<SearchDto> sortedDtos =
+        dtosMap.values().stream()
+            .sorted(Comparator.comparing(SearchDto::getScore).reversed())
+            .collect(Collectors.toList());
+
+    // the query can return duplicates
+    List<SearchResponse> responses = new ArrayList<>();
+    sortedDtos.forEach(
         dto -> {
           SearchResponse response = new SearchResponse();
           response.setType(dto.getType());
@@ -57,32 +81,74 @@ public class CollectionsSearchService {
             response.setInstitutionName(dto.getInstitutionName());
           }
 
-          List<SearchResponse.Match> matches = new ArrayList<>();
-          createHighlightMatch(dto.getCodeHighlight(), "code").ifPresent(matches::add);
-          createHighlightMatch(dto.getNameHighlight(), "name").ifPresent(matches::add);
-          createHighlightMatch(dto.getDescriptionHighlight(), "description")
-              .ifPresent(matches::add);
-          createHighlightMatch(dto.getAlternativeCodesHighlight(), "alternativeCode")
-              .ifPresent(matches::add);
-          createHighlightMatch(dto.getAddressHighlight(), "address").ifPresent(matches::add);
-          createHighlightMatch(dto.getCityHighlight(), "city").ifPresent(matches::add);
-          createHighlightMatch(dto.getProvinceHighlight(), "province").ifPresent(matches::add);
-          createHighlightMatch(dto.getCountryHighlight(), "country").ifPresent(matches::add);
-          createHighlightMatch(dto.getMailAddressHighlight(), "mailingAddress")
-              .ifPresent(matches::add);
-          createHighlightMatch(dto.getMailCityHighlight(), "mailingCity").ifPresent(matches::add);
-          createHighlightMatch(dto.getMailProvinceHighlight(), "mailingProvince")
-              .ifPresent(matches::add);
-          createHighlightMatch(dto.getMailCountryHighlight(), "mailingCountry")
-              .ifPresent(matches::add);
-          if (!matches.isEmpty()) {
-            response.setMatches(matches);
+          if (highlight) {
+            Set<SearchResponse.Match> matches = createMatches(dto);
+            if (!matches.isEmpty()) {
+              response.setMatches(matches);
+            }
           }
 
           responses.add(response);
         });
 
     return responses;
+  }
+
+  private void mergeDtos(SearchDto existing, SearchDto newDto) {
+    if (!Strings.isNullOrEmpty(newDto.getCodeHighlight())) {
+      existing.setCodeHighlight(newDto.getCodeHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getNameHighlight())) {
+      existing.setNameHighlight(newDto.getNameHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getDescriptionHighlight())) {
+      existing.setDescriptionHighlight(newDto.getDescriptionHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getAlternativeCodesHighlight())) {
+      existing.setAlternativeCodesHighlight(newDto.getAlternativeCodesHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getAddressHighlight())) {
+      existing.setAddressHighlight(newDto.getAddressHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getCityHighlight())) {
+      existing.setCityHighlight(newDto.getCityHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getProvinceHighlight())) {
+      existing.setProvinceHighlight(newDto.getProvinceHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getCountryHighlight())) {
+      existing.setCountryHighlight(newDto.getCountryHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getMailAddressHighlight())) {
+      existing.setMailAddressHighlight(newDto.getMailAddressHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getMailCityHighlight())) {
+      existing.setMailCityHighlight(newDto.getMailCityHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getMailProvinceHighlight())) {
+      existing.setMailProvinceHighlight(newDto.getMailProvinceHighlight());
+    }
+    if (!Strings.isNullOrEmpty(newDto.getMailCountryHighlight())) {
+      existing.setMailCountryHighlight(newDto.getMailCountryHighlight());
+    }
+  }
+
+  private Set<SearchResponse.Match> createMatches(SearchDto dto) {
+    Set<SearchResponse.Match> matches = new HashSet<>();
+    createHighlightMatch(dto.getCodeHighlight(), "code").ifPresent(matches::add);
+    createHighlightMatch(dto.getNameHighlight(), "name").ifPresent(matches::add);
+    createHighlightMatch(dto.getDescriptionHighlight(), "description").ifPresent(matches::add);
+    createHighlightMatch(dto.getAlternativeCodesHighlight(), "alternativeCode")
+        .ifPresent(matches::add);
+    createHighlightMatch(dto.getAddressHighlight(), "address").ifPresent(matches::add);
+    createHighlightMatch(dto.getCityHighlight(), "city").ifPresent(matches::add);
+    createHighlightMatch(dto.getProvinceHighlight(), "province").ifPresent(matches::add);
+    createHighlightMatch(dto.getCountryHighlight(), "country").ifPresent(matches::add);
+    createHighlightMatch(dto.getMailAddressHighlight(), "mailingAddress").ifPresent(matches::add);
+    createHighlightMatch(dto.getMailCityHighlight(), "mailingCity").ifPresent(matches::add);
+    createHighlightMatch(dto.getMailProvinceHighlight(), "mailingProvince").ifPresent(matches::add);
+    createHighlightMatch(dto.getMailCountryHighlight(), "mailingCountry").ifPresent(matches::add);
+    return matches;
   }
 
   private static Optional<SearchResponse.Match> createHighlightMatch(
