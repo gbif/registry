2619e4e9ffaf599f122e4f4d8a59dfb26e8b1846
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2020-12-10 17:10:39 +0100

    #255 keeping identifiers, machine tags and occurrence mappings in the replaced entity

--- registry-persistence/src/main/java/org/gbif/registry/persistence/mapper/collections/MergeableMapper.java
+++ registry-persistence/src/main/java/org/gbif/registry/persistence/mapper/collections/MergeableMapper.java
@@ -23,19 +23,4 @@ public interface MergeableMapper {
 
   void replace(
       @Param("targetEntityKey") UUID targetEntityKey, @Param("replacementKey") UUID replacementKey);
-
-  void moveIdentifier(
-      @Param("sourceEntityKey") UUID sourceEntityKey,
-      @Param("targetEntityKey") UUID targetEntityKey,
-      @Param("identifierKey") int identifierKey);
-
-  void moveMachineTag(
-      @Param("sourceEntityKey") UUID sourceEntityKey,
-      @Param("targetEntityKey") UUID targetEntityKey,
-      @Param("machineTagKey") int machineTagKey);
-
-  void moveOccurrenceMapping(
-      @Param("sourceEntityKey") UUID sourceEntityKey,
-      @Param("targetEntityKey") UUID targetEntityKey,
-      @Param("occurrenceMappingKey") int occurrenceMappingKey);
 }
