bd1d5693897bcdb8492a26487aaaf148ba6e91e1
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-12-11 10:32:23 +0100

    #257 Dataset ES index improvements

--- registry-search/src/main/java/org/gbif/registry/search/dataset/indexing/ws/JacksonObjectMapper.java
+++ registry-search/src/main/java/org/gbif/registry/search/dataset/indexing/ws/JacksonObjectMapper.java
@@ -18,12 +18,15 @@ package org.gbif.registry.search.dataset.indexing.ws;
 import org.gbif.api.jackson.LicenseSerde;
 import org.gbif.api.vocabulary.License;
 
+import java.text.SimpleDateFormat;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 @Component
@@ -37,6 +40,8 @@ public class JacksonObjectMapper {
     simpleModule.addDeserializer(License.class, new LicenseSerde.LicenseJsonDeserializer());
     OBJECT_MAPPER.registerModule(simpleModule);
     OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    OBJECT_MAPPER.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    OBJECT_MAPPER.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS"));
   }
 
   public static ObjectMapper get() {
