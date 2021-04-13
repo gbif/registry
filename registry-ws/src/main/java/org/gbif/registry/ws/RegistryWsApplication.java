e8d92b7e0105b49cbfa90e1e7d04ab8451a2f52d
fmendezh <federicomh@gmail.com>
2021-04-13 14:25:12 +0200

    allowing encoded back slashes in URIs

--- registry-ws/src/main/java/org/gbif/registry/ws/RegistryWsApplication.java
+++ registry-ws/src/main/java/org/gbif/registry/ws/RegistryWsApplication.java
@@ -61,6 +61,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableFeignClients
 public class RegistryWsApplication {
   public static void main(String[] args) {
+    System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
     SpringApplication.run(RegistryWsApplication.class, args);
   }
 }
