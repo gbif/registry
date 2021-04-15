eb2ed97126fb25a026163d3c4a9f17b990e91f97
fmendezh <federicomh@gmail.com>
2021-04-15 16:57:27 +0200

    Mocking  Elasticsearch for all non NetworkEntity tests

--- registry-integration-tests/src/test/java/org/gbif/registry/search/test/DatasetElasticsearchConfiguration.java
+++ registry-integration-tests/src/test/java/org/gbif/registry/search/test/DatasetElasticsearchConfiguration.java
@@ -17,7 +17,10 @@ package org.gbif.registry.search.test;
 
 import org.gbif.registry.search.dataset.indexing.es.EsClient;
 
+import org.elasticsearch.client.RestHighLevelClient;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,12 +33,13 @@ public class DatasetElasticsearchConfiguration {
   @Autowired private ResourceLoader resourceLoader;
 
   @Bean("datasetElasticCluster")
-  public EsManageServer esManageServer(@Value("${elasticsearch.registry.index}") String indexName) {
+  public EsManageServer esManageServer(@Value("${elasticsearch.registry.index}") String indexName,
+                                       @Value("${elasticsearch.mock}") boolean mockEs) {
     try {
-      return new EsManageServer(
-          resourceLoader.getResource("classpath:dataset-es-mapping.json"),
-          resourceLoader.getResource("classpath:dataset-es-settings.json"),
-          indexName);
+      return mockEs? Mockito.mock(EsManageServer.class):
+        new EsManageServer(resourceLoader.getResource("classpath:dataset-es-mapping.json"),
+                           resourceLoader.getResource("classpath:dataset-es-settings.json"),
+                           indexName);
     } catch (Exception ex) {
       throw new RuntimeException(ex);
     }
@@ -56,5 +60,22 @@ public class DatasetElasticsearchConfiguration {
   @Primary
   public EsClient.EsClientConfiguration registryRestHighLevelClient(EsManageServer esManageServer) {
     return occurrenceRestHighLevelClient(esManageServer);
+  }
+
+  @Bean
+  @Primary
+  public RestHighLevelClient restHighLevelClient(
+    @Qualifier("registryEsClientConfig") EsClient.EsClientConfiguration esClientConfiguration,
+    @Value("${elasticsearch.mock}") boolean mockEs) {
+    return mockEs ? Mockito.mock(RestHighLevelClient.class) :
+      EsClient.provideEsClient(esClientConfiguration);
+  }
+
+  @Bean(name = "occurrenceEsClient")
+  public RestHighLevelClient occurrenceRestHighLevelClient(
+    @Qualifier("esOccurrenceClientConfig") EsClient.EsClientConfiguration esClientConfiguration,
+    @Value("${elasticsearch.mock}") boolean mockEs) {
+    return mockEs ? Mockito.mock(RestHighLevelClient.class) :
+      EsClient.provideEsClient(esClientConfiguration);
   }
 }
