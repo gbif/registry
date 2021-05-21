5e028792c886d20f837a95ee0d59c3ab45f7ce56
fmendezh <federicomh@gmail.com>
2021-05-21 14:22:24 +0200

    implementing the export to csv/tsv of dataset search results

--- registry-ws/src/main/java/org/gbif/registry/ws/export/CsvWriter.java
+++ registry-ws/src/main/java/org/gbif/registry/ws/export/CsvWriter.java
@@ -1,20 +1,22 @@
 package org.gbif.registry.ws.export;
 
 import org.gbif.api.model.common.export.ExportFormat;
-import org.gbif.api.model.common.paging.Pageable;
 import org.gbif.api.model.occurrence.DownloadStatistics;
-import org.gbif.api.service.registry.OccurrenceDownloadService;
-import org.gbif.api.vocabulary.Country;
+import org.gbif.api.model.registry.search.DatasetSearchResult;
+import org.gbif.api.vocabulary.DatasetSubtype;
+import org.gbif.api.vocabulary.DatasetType;
+import org.gbif.api.vocabulary.License;
 
 import java.io.Writer;
-import java.util.Date;
+import java.util.List;
 import java.util.UUID;
-
+import java.util.stream.Collectors;
 
 import lombok.Builder;
 import lombok.Data;
 import lombok.SneakyThrows;
 import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.ParseEnum;
 import org.supercsv.cellprocessor.ParseInt;
 import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.io.CsvBeanWriter;
@@ -25,6 +27,9 @@ import org.supercsv.util.CsvContext;
 @Data
 @Builder
 public class CsvWriter<T> {
+
+  //Delimiter used for list/array of elements
+  public static final String ARRAY_DELIMITER = ";";
 
   private final String[] header;
 
@@ -58,22 +63,52 @@ public class CsvWriter<T> {
   }
 
   /**
-   * Creates and CsvWriter/exporter DownloadStatistics.
+   * Creates an CsvWriter/exporter of DownloadStatistics.
    */
-  public static CsvWriter<DownloadStatistics> downloadStatisticsTsvWriter(Iterable<DownloadStatistics> pager,
+  public static CsvWriter<DownloadStatistics> downloadStatisticsCsvWriter(Iterable<DownloadStatistics> pager,
                                                                           ExportFormat preference) {
 
     return CsvWriter.<DownloadStatistics>builder()
               .fields(new String[]{"datasetKey", "totalRecords", "numberDownloads", "year", "month"})
               .header(new String[]{"dataset_key", "total_records", "number_downloads", "year", "month"})
-              .processors(new CellProcessor[]{new UUIDProcessor(),
-                                              new Optional(new ParseInt()),
-                                              new Optional(new ParseInt()),
-                                              new Optional(new ParseInt()),
-                                              new Optional(new ParseInt())})
+              .processors(new CellProcessor[]{new UUIDProcessor(),           //datasetKey
+                                              new Optional(new ParseInt()),  //totalRecords
+                                              new Optional(new ParseInt()),  //numberDowloads
+                                              new Optional(new ParseInt()),  //year
+                                              new Optional(new ParseInt())   //month
+                                              })
               .preference(preference)
               .pager(pager)
               .build();
+  }
+
+  /**
+   * Creates an CsvWriter/exporter of DatasetSearchResult.
+   */
+  public static CsvWriter<DatasetSearchResult> datasetSearchResultCsvWriter(Iterable<DatasetSearchResult> pager,
+                                                                           ExportFormat preference) {
+    return CsvWriter.<DatasetSearchResult>builder()
+      .fields(new String[]{"key", "title", "doi", "license", "type", "subType", "hostingOrganizationKey", "hostingOrganizationTitle", "publishingOrganizationKey", "publishingOrganizationTitle","networkKeys", "projectIdentifier", "recordCount", "nameUsagesCount"})
+      .header(new String[]{"dataset_key", "title", "doi", "license", "type", "sub_type", "hosting_organization_Key", "hosting_organization_title", "publishing_organization_key", "publishing_organization_title","network_keys", "project_identifier", "occurrence_records_count", "name_usages_count"})
+      //  "recordCount", "nameUsagesCount"
+      .processors(new CellProcessor[]{new UUIDProcessor(), //key
+                                      null,                                             //title
+                                      new DOIProcessor(),                               //doi
+                                      new Optional(new ParseEnum(License.class)),       //license
+                                      new Optional(new ParseEnum(DatasetType.class)),   //type
+                                      new Optional(new ParseEnum(DatasetSubtype.class)),//subType
+                                      new UUIDProcessor(),                              //hostingOrganizationKey
+                                      null,                                             //hostingOrganizationTitle
+                                      new UUIDProcessor(),                              //publishingOrganizationKey
+                                      null,                                             //publishingOrganizationTitle
+                                      new ListUUIDProcessor(),                          //networkKeys
+                                      null,                                             //projectIdentifier
+                                      new ParseInt(),                                   //recordCount
+                                      new ParseInt()                                    //nameUsagesCount
+                                      })
+      .preference(preference)
+      .pager(pager)
+      .build();
   }
 
   /**
@@ -86,4 +121,24 @@ public class CsvWriter<T> {
     }
   }
 
+  /**
+   * Null aware List of UUIDs processor.
+   */
+  private static class ListUUIDProcessor implements CellProcessor {
+    @Override
+    public String execute(Object value, CsvContext csvContext) {
+      return value != null ?
+        ((List<UUID>) value).stream().map(UUID::toString).collect(Collectors.joining(ARRAY_DELIMITER)) : "";
+    }
+  }
+
+  /**
+   * Null aware UUID processor.
+   */
+  private static class DOIProcessor implements CellProcessor {
+    @Override
+    public String execute(Object value, CsvContext csvContext) {
+      return value != null ? value.toString() : "";
+    }
+  }
 }
