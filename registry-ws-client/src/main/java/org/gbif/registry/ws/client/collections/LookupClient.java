2fc3e367d9579a5386726f3b0713d219b417ab34
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2021-06-09 14:29:27 +0200

    motherpom version + spotless applied

--- registry-ws-client/src/main/java/org/gbif/registry/ws/client/collections/LookupClient.java
+++ registry-ws-client/src/main/java/org/gbif/registry/ws/client/collections/LookupClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Global Biodiversity Information Facility (GBIF)
+ * Copyright 2020-2021 Global Biodiversity Information Facility (GBIF)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
