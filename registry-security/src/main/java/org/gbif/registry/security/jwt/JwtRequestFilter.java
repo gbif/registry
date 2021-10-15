4390066b8c842d64645993e5c6188f24080e46a9
fmendezh <federicomh@gmail.com>
2021-10-15 13:48:10 +0200

    adding an endpoint to validate JWT

--- registry-security/src/main/java/org/gbif/registry/security/jwt/JwtRequestFilter.java
+++ registry-security/src/main/java/org/gbif/registry/security/jwt/JwtRequestFilter.java
@@ -1,6 +1,4 @@
 /*
- * Copyright 2020 Global Biodiversity Information Facility (GBIF)
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -21,7 +19,6 @@ import org.gbif.ws.security.GbifAuthenticationToken;
 
 import java.io.IOException;
 import java.util.Optional;
-import java.util.regex.Pattern;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -31,7 +28,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -51,9 +47,6 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
   private static final Logger LOG = LoggerFactory.getLogger(JwtRequestFilter.class);
 
-  // Patterns that catches case insensitive versions of word 'bearer'
-  private static final Pattern BEARER_PATTERN = Pattern.compile("(?i)bearer");
-
   private final UserDetailsService userDetailsService;
   private final JwtAuthenticateService jwtAuthenticateService;
   private final JwtIssuanceService jwtIssuanceService;
@@ -71,7 +64,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
   protected void doFilterInternal(
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    final Optional<String> token = findTokenInRequest(request);
+    final Optional<String> token = JwtUtils.findTokenInRequest(request);
 
     if (!token.isPresent()) {
       // if there is no token in the request we ignore this authentication
@@ -101,25 +94,4 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     filterChain.doFilter(request, response);
   }
 
-  /** Tries to find the token in the {@link HttpHeaders#AUTHORIZATION} header. */
-  public Optional<String> findTokenInRequest(HttpServletRequest request) {
-    // check header first
-    return Optional.ofNullable(request.getHeader(HttpHeaders.AUTHORIZATION))
-        .filter(this::containsBearer)
-        .map(this::removeBearer);
-  }
-
-  /**
-   * Removes 'bearer' token, leading an trailing whitespaces.
-   *
-   * @param token to be clean
-   * @return a token without whitespaces and the word 'bearer'
-   */
-  private String removeBearer(String token) {
-    return BEARER_PATTERN.matcher(token).replaceAll("").trim();
-  }
-
-  private boolean containsBearer(String header) {
-    return BEARER_PATTERN.matcher(header).find();
-  }
 }
