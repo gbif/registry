770dfc389670833fd5642ae02572448fc178e9f6
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2022-09-27 16:20:01 +0200

    #442 using only accepted institution if exists for collection mismatch

--- registry-service/src/main/java/org/gbif/registry/service/collections/lookup/DefaultLookupService.java
+++ registry-service/src/main/java/org/gbif/registry/service/collections/lookup/DefaultLookupService.java
@@ -13,15 +13,11 @@
  */
 package org.gbif.registry.service.collections.lookup;
 
-import org.gbif.api.model.collections.lookup.AlternativeMatches;
-import org.gbif.api.model.collections.lookup.CollectionMatched;
-import org.gbif.api.model.collections.lookup.InstitutionMatched;
-import org.gbif.api.model.collections.lookup.LookupParams;
-import org.gbif.api.model.collections.lookup.LookupResult;
-import org.gbif.api.model.collections.lookup.Match;
+import org.gbif.api.model.collections.lookup.*;
 import org.gbif.registry.service.collections.lookup.matchers.CollectionMatcher;
 import org.gbif.registry.service.collections.lookup.matchers.InstitutionMatcher;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Set;
 import java.util.UUID;
@@ -76,6 +72,13 @@ public class DefaultLookupService implements LookupService {
   }
 
   private Set<UUID> getInstitutionsMatched(Matches<InstitutionMatched> institutionMatches) {
+    if (institutionMatches.getAcceptedMatch() != null
+        && institutionMatches.getAcceptedMatch().getMatchType() != Match.MatchType.NONE) {
+      // if there is an accepted match we only use this one
+      return Collections.singleton(
+          institutionMatches.getAcceptedMatch().getEntityMatched().getKey());
+    }
+
     return institutionMatches.getAllMatches().stream()
         .map(m -> m.getEntityMatched().getKey())
         .collect(Collectors.toSet());
