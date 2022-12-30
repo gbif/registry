28fee0881a8a5b98c3bdabf0870438b47c334bcb
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2022-12-30 12:17:19 +0100

    concurrent tests registry-cli

--- registry-cli/src/test/java/org/gbif/registry/cli/directoryupdate/DirectoryUpdateServiceTest.java
+++ registry-cli/src/test/java/org/gbif/registry/cli/directoryupdate/DirectoryUpdateServiceTest.java
@@ -21,6 +21,8 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationContext;
@@ -31,6 +33,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@Execution(ExecutionMode.CONCURRENT)
 @ExtendWith(MockitoExtension.class)
 public class DirectoryUpdateServiceTest {
 
