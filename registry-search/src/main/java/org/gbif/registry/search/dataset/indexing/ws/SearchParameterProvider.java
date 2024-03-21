642caaeaf8ffadbcb0a538d186684cb784f08a5d
fmendezh <federicomh@gmail.com>
2024-03-21 16:01:17 +0100

    accessing the method to avoid generics issue

--- registry-search/src/main/java/org/gbif/registry/search/dataset/indexing/ws/SearchParameterProvider.java
+++ registry-search/src/main/java/org/gbif/registry/search/dataset/indexing/ws/SearchParameterProvider.java
@@ -13,6 +13,7 @@
  */
 package org.gbif.registry.search.dataset.indexing.ws;
 
+import org.gbif.api.model.checklistbank.search.NameUsageSearchRequest;
 import org.gbif.api.model.common.paging.Pageable;
 import org.gbif.api.model.common.search.FacetedSearchRequest;
 import org.gbif.api.model.common.search.SearchParameter;
@@ -33,6 +34,8 @@ import com.google.common.collect.Lists;
 
 import lombok.experimental.UtilityClass;
 
+import org.gbif.api.model.occurrence.search.OccurrenceSearchRequest;
+
 import static org.gbif.registry.search.dataset.indexing.ws.WebserviceParameter.DEFAULT_SEARCH_PARAM_VALUE;
 import static org.gbif.registry.search.dataset.indexing.ws.WebserviceParameter.PARAM_FACET;
 import static org.gbif.registry.search.dataset.indexing.ws.WebserviceParameter.PARAM_FACET_LIMIT;
@@ -47,7 +50,15 @@ import static org.gbif.registry.search.dataset.indexing.ws.WebserviceParameter.P
 @UtilityClass
 public class SearchParameterProvider {
 
-  public static <P extends SearchParameter, R extends FacetedSearchRequest<P>>
+  public static ProxyRetrofitQueryMap getParameterFromFacetedRequest(NameUsageSearchRequest nameUsageSearchRequest) {
+    return getParameterFromSearchRequest(nameUsageSearchRequest);
+  }
+
+  public static ProxyRetrofitQueryMap getParameterFromFacetedRequest(OccurrenceSearchRequest occurrenceSearchRequest) {
+    return getParameterFromSearchRequest(occurrenceSearchRequest);
+  }
+
+  private static <P extends SearchParameter, R extends FacetedSearchRequest<P>>
       ProxyRetrofitQueryMap getParameterFromFacetedRequest(@Nullable R searchRequest) {
     // The searchRequest is transformed in a parameter map
     ProxyRetrofitQueryMap parameters = getParameterFromSearchRequest(searchRequest);
@@ -85,7 +96,7 @@ public class SearchParameterProvider {
     return parameters;
   }
 
-  public static <P extends SearchParameter> ProxyRetrofitQueryMap getParameterFromSearchRequest(
+  private static <P extends SearchParameter> ProxyRetrofitQueryMap getParameterFromSearchRequest(
       @Nullable SearchRequest<P> searchRequest) {
 
     // The searchRequest is transformed in a parameter map
