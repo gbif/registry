6d2d5fa831036bd752156267e7faab0e19ac16f8
fmendezh <federicomh@gmail.com>
2025-06-18 09:57:59 +0200

    adding cardinality to dwcaExtensions

--- registry-search/src/main/java/org/gbif/registry/search/dataset/DatasetEsFieldMapper.java
+++ registry-search/src/main/java/org/gbif/registry/search/dataset/DatasetEsFieldMapper.java
@@ -19,6 +19,7 @@ import org.gbif.api.vocabulary.Country;
 import org.gbif.api.vocabulary.DatasetSubtype;
 import org.gbif.api.vocabulary.DatasetType;
 import org.gbif.api.vocabulary.EndpointType;
+import org.gbif.api.vocabulary.Extension;
 import org.gbif.api.vocabulary.License;
 import org.gbif.registry.search.dataset.common.EsFieldMapper;
 
@@ -79,6 +80,7 @@ public class DatasetEsFieldMapper implements EsFieldMapper<DatasetSearchParamete
           .put("type", DatasetType.values().length)
           .put("subtype", DatasetSubtype.values().length)
           .put("endpoints.type", EndpointType.values().length)
+          .put("dwcaExtensions", Extension.values().length)
           .build();
 
   private static final String[] EXCLUDE_FIELDS = new String[] {"all"};
