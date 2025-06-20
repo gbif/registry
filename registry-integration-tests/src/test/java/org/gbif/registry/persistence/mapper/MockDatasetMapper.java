770b73d8ab4b44ecd6b2182667a5ca8f0ce29300
fmendezh <federicomh@gmail.com>
2025-06-20 11:53:33 +0200

    https://github.com/gbif/registry/issues/681 this branch is proposal to index dwca data at indexing and store basic information in the registry database

--- registry-integration-tests/src/test/java/org/gbif/registry/persistence/mapper/MockDatasetMapper.java
+++ registry-integration-tests/src/test/java/org/gbif/registry/persistence/mapper/MockDatasetMapper.java
@@ -305,4 +305,10 @@ public class MockDatasetMapper implements DatasetMapper {
   public Dataset getLightweight(UUID key) {
     return null;
   }
+
+  @Override
+  public void createDwcaDataset(UUID datasetKey, Dataset.DwcA dwcaDataset) {}
+
+  @Override
+  public void updateDwcaDataset(UUID datasetKey, Dataset.DwcA dwcaDataset) {}
 }
