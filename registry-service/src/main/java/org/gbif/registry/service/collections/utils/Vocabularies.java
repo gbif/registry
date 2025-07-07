37c78b563da0df57145639357ce5b6e9d8fb661e
ahakanzn <a.hakan.zn@gmail.com>
2025-07-07 12:48:43 +0200

    Revert "make vocabulary lookup case-insensitive https://github.com/gbif/registry/issues/703"
    This reverts commit 8223345938cecf5501e07c855fa3fdc628d2146f.

--- registry-service/src/main/java/org/gbif/registry/service/collections/utils/Vocabularies.java
+++ registry-service/src/main/java/org/gbif/registry/service/collections/utils/Vocabularies.java
@@ -205,16 +205,6 @@ public class Vocabularies {
 
   private static Set<String> findChildren(
       ConceptClient conceptClient, String vocabName, String conceptName, Set<String> allChildren) {
-
-    List<LookupResult> lookupResults = lookupLatestRelease(vocabName, conceptName, conceptClient);
-    final String actualConceptName;
-
-    if (lookupResults != null && !lookupResults.isEmpty()) {
-      actualConceptName = lookupResults.get(0).getConceptName();
-    } else {
-      actualConceptName = conceptName;
-    }
-
     PagingResponse<ConceptView> result =
         Retry.decorateSupplier(
                 RETRY,
@@ -222,7 +212,7 @@ public class Vocabularies {
                     conceptClient.listConceptsLatestRelease(
                         vocabName,
                         ConceptListParams.builder()
-                            .name(actualConceptName)
+                            .name(conceptName)
                             .includeChildren(true)
                             .build()))
             .get();
