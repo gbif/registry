bc4005590c947cf0956f6adfdf4bf76739092c11
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2025-08-27 12:27:32 +0200

    added option to rerun event hdfs view thru api

--- registry-pipelines/src/main/java/org/gbif/registry/pipelines/DefaultRegistryPipelinesHistoryTrackingService.java
+++ registry-pipelines/src/main/java/org/gbif/registry/pipelines/DefaultRegistryPipelinesHistoryTrackingService.java
@@ -473,6 +473,7 @@ public class DefaultRegistryPipelinesHistoryTrackingService
       case EVENTS_VERBATIM_TO_INTERPRETED:
         return deserializeMessage(jsonMessage, PipelinesEventsMessage.class);
       case EVENTS_INTERPRETED_TO_INDEX:
+      case EVENTS_HDFS_VIEW:
         return deserializeMessage(jsonMessage, PipelinesEventsInterpretedMessage.class);
       default:
         return Optional.empty();
